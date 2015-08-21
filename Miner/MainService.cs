8790d330aad71c44b7d8f62dae8dde4c97164805
erikzhang <erik@vcage.com>
2015-08-21 18:15:26 +0800

    将挖矿工作包装成任务；

--- Miner/MainService.cs
+++ Miner/MainService.cs
@@ -2,7 +2,9 @@
 using AntShares.Data;
 using AntShares.Network;
 using AntShares.Services;
+using AntShares.Threading;
 using System;
+using System.Threading;
 
 namespace AntShares
 {
@@ -10,16 +12,26 @@ namespace AntShares
     {
         private LevelDBBlockchain blockchain;
         private LocalNode localnode;
+        private CancellableTask task;
 
         protected override string Prompt => "ant";
         public override string ServiceName => "AntSharesMiner";
 
         private void Blockchain_PersistCompleted(object sender, EventArgs e)
         {
+            task.Cancel();
+            task.Run();
+        }
+
+        private void Mine(CancellationToken token)
+        {
             //TODO: 挖矿
-            //1. 首先，统计当前的矿工列表，看自己是否在名单中；
-            //2. 基于本地区块链的当前区块开始挖；
-            //3. 如果收到新的区块广播，则停止当前的挖矿工作，重新从第一步开始；
+            //1. 首先，统计当前的矿工列表，看自己是否在名单中；如果不在名单中，则直接放弃本轮挖矿动作；
+            //2. 基于本地区块链的当前区块开始构造共识数据；
+            //3. 将共识数据广播到矿工网络；
+            //4. 组合所有其它矿工的共识数据；
+            //5. 签名并广播；
+            //6. 广播最终共识后的区块；
         }
 
         protected internal override void OnStart()
@@ -29,11 +41,15 @@ namespace AntShares
             Blockchain.RegisterBlockchain(blockchain);
             localnode = new LocalNode();
             localnode.Start();
+            task = new CancellableTask(Mine);
+            task.Run();
         }
 
         protected internal override void OnStop()
         {
             blockchain.PersistCompleted -= Blockchain_PersistCompleted;
+            task.Cancel();
+            task.Wait();
             localnode.Dispose();
             blockchain.Dispose();
         }
---
+++ Miner/Threading/CancellableTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AntShares.Threading
+{
+    internal class CancellableTask
+    {
+        private Action<CancellationToken> action;
+        private Task task_current;
+        private CancellationTokenSource cancel_current;
+
+        public CancellableTask(Action<CancellationToken> action)
+        {
+            this.action = action;
+        }
+
+        public void Cancel()
+        {
+            if (task_current == null || task_current.IsCompleted)
+                return;
+            cancel_current.Cancel();
+        }
+
+        public void Run()
+        {
+            if (task_current?.IsCompleted == false)
+                throw new InvalidOperationException();
+            cancel_current = new CancellationTokenSource();
+            task_current = Task.Run(() => action(cancel_current.Token), cancel_current.Token);
+        }
+
+        public void Wait()
+        {
+            if (task_current == null)
+                throw new InvalidOperationException();
+            task_current.Wait();
+        }
+    }
+}
