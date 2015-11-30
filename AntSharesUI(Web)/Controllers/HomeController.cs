150ab247a5b874e356a3422aa88d1865009d1f84
chenzhitong <chenzhitong12@qq.com>
2015-11-30 13:41:15 +0800

    调整菜单结构

--- AntSharesUI(Web)/Controllers/HomeController.cs
+++ AntSharesUI(Web)/Controllers/HomeController.cs
@@ -8,15 +8,22 @@ namespace AntSharesUI_Web_.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult textaction(string name)
+        {
+            string a = name;
+            return null;
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
