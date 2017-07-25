946760dde6be118f2f1cccb34fca29e41d49f9d4
AshRolls <devchan@neoblockchain.slack>
2017-07-25 11:25:52 +0100

    ClaimTransaction.cs ToJson() test

--- neo.UnitTests/UT_ClaimTransaction.cs
+++ neo.UnitTests/UT_ClaimTransaction.cs
@@ -5,6 +5,7 @@ using System.Text;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.Core;
+using Neo.IO.Json;
 
 namespace Neo.UnitTests
 {
@@ -124,6 +125,35 @@ namespace Neo.UnitTests
 
             UInt160[] res = uut.GetScriptHashesForVerifying();
             res.Length.Should().Be(2);
+        }
+
+        [TestMethod]
+        public void ToJson()
+        {
+            CoinReference[] refs = new[] { TestUtils.GetCoinReference(new UInt256(TestUtils.GetByteArray(32, 0x42))) };
+            uut.Claims = refs;
+            uut.Attributes = new TransactionAttribute[0];
+            uut.Inputs = new CoinReference[0];
+            uut.Outputs = new TransactionOutput[0];
+            uut.Scripts = new Witness[0];
+
+            JObject jObj = uut.ToJson();
+            jObj.Should().NotBeNull();
+            jObj["txid"].AsString().Should().Be("45a5c537ca95d62add6b331cad0fd742ce29ada02c50ea7e4d709f83563972b9");
+            jObj["size"].AsNumber().Should().Be(41);
+            jObj["type"].AsString().Should().Be("ClaimTransaction");
+            jObj["version"].AsNumber().Should().Be(0);
+            ((JArray)jObj["attributes"]).Count.Should().Be(0);
+            ((JArray)jObj["vin"]).Count.Should().Be(0);
+            ((JArray)jObj["vout"]).Count.Should().Be(0);
+            jObj["sys_fee"].AsString().Should().Be("0");
+            jObj["net_fee"].AsString().Should().Be("0");
+            ((JArray)jObj["scripts"]).Count.Should().Be(0);
+
+            JArray claims = (JArray) jObj["claims"];
+            claims.Count.Should().Be(1);
+            claims[0]["txid"].AsString().Should().Be("2020202020202020202020202020202020202020202020202020202020202042");
+            claims[0]["vout"].AsNumber().Should().Be(0);
         }
 
     }
