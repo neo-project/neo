8d423a8e9421c55ba1f0caab9c8ae393a9611aa0
dps <dps@localhost>
2017-07-31 21:56:05 -0500

    added "listpeers" RPC service

--- neo/Network/RPC/RpcServer.cs
+++ neo/Network/RPC/RpcServer.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace Neo.Network.RPC
 {
@@ -203,6 +204,49 @@ namespace Neo.Network.RPC
                         }
                         json["address"] = _params[0];
                         json["isvalid"] = scriptHash != null;
+                        return json;
+					}
+				case "listpeers":
+					{
+						JObject json = new JObject();
+
+                        {
+                            JArray unconnectedPeers = new JArray();
+                            foreach (IPEndPoint peer in LocalNode.GetUnconnectedPeers())
+                            {
+                                JObject peerJson = new JObject();
+                                peerJson["address"] = peer.Address.ToString();
+                                peerJson["port"] = peer.Port;
+                                unconnectedPeers.Add(peerJson);
+                            }
+                            json["unconnected"] = unconnectedPeers;
+                        }
+
+                        {
+                            JArray badPeers = new JArray();
+                            foreach (IPEndPoint peer in LocalNode.GetBadPeers())
+                            {
+                                JObject peerJson = new JObject();
+                                peerJson["address"] = peer.Address.ToString();
+                                peerJson["port"] = peer.Port;
+                                badPeers.Add(peerJson);
+                            }
+                            json["bad"] = badPeers;
+                        }
+
+                        {
+                            JArray connectedPeers = new JArray();
+                            foreach (RemoteNode node in LocalNode.GetRemoteNodes())
+                            {
+                                IPEndPoint peer = node.RemoteEndpoint;
+                                JObject peerJson = new JObject();
+                                peerJson["address"] = peer.Address.ToString();
+                                peerJson["port"] = peer.Port;
+                                connectedPeers.Add(peerJson);
+                            }
+                            json["connected"] = connectedPeers;
+                        }
+
                         return json;
                     }
                 default:
