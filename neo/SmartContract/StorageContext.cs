8413a6de05c3666bcbbbf3fd1515b83ea0ded9df
shargon <shargon@gmail.com>
2017-10-28 22:10:11 +0200

    Internal to public

--- neo/SmartContract/StorageContext.cs
+++ neo/SmartContract/StorageContext.cs
@@ -2,7 +2,7 @@
 
 namespace Neo.SmartContract
 {
-    internal class StorageContext : IInteropInterface
+    public class StorageContext : IInteropInterface
     {
         public UInt160 ScriptHash;
 
