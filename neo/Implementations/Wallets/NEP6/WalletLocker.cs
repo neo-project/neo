23582f78dca25d9532dbc56dd58c373c80beffbd
Erik Zhang <erik@vcage.com>
2017-12-07 21:15:09 +0800

    implement NEP-6 wallet

---
+++ neo/Implementations/Wallets/NEP6/WalletLocker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Neo.Implementations.Wallets.NEP6
+{
+    internal class WalletLocker : IDisposable
+    {
+        private NEP6Wallet wallet;
+
+        public WalletLocker(NEP6Wallet wallet)
+        {
+            this.wallet = wallet;
+        }
+
+        public void Dispose()
+        {
+            wallet.Lock();
+        }
+    }
+}
