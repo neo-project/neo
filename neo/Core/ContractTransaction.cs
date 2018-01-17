3309764b61e63d2160c4d97ce01f10a3c02ef90a
swdevMPaul <swdevMPaul@outlook.com>
2018-01-17 12:02:11 +0200

    Update ContractTransaction.cs

--- neo/Core/ContractTransaction.cs
+++ neo/Core/ContractTransaction.cs
@@ -5,6 +5,7 @@ namespace Neo.Core
 {
     /// <summary>
     /// 合约交易，这是最常用的一种交易
+    ///Contract trading, which is the most commonly used type of transaction
     /// </summary>
     public class ContractTransaction : Transaction
     {
