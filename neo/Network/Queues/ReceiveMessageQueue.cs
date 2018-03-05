fe7e9ce5ce1101401bce09668524ec74d35faa97
Shargon <shargon@gmail.com>
2018-03-05 09:57:24 +0100

    Priority changes

--- neo/Network/Queues/ReceiveMessageQueue.cs
+++ neo/Network/Queues/ReceiveMessageQueue.cs
@@ -10,6 +10,8 @@ namespace Neo.Network.Queues
         {
             switch (command)
             {
+                case "block":
+                case "consensus":
                 case "getheaders":
                 case "getblocks":
                     {
