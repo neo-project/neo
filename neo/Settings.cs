3a3b94c64ac8b23f552bda4bd6d316e56b0644c3
DaveOnBlocks <crypto@solidhouse.com>
2018-03-26 13:44:34 -0600

    Extracting hardcoded seconds per block to the protocol.json file

--- neo/Settings.cs
+++ neo/Settings.cs
@@ -8,12 +8,15 @@ namespace Neo
 {
     internal class Settings
     {
+       
         public uint Magic { get; private set; }
         public byte AddressVersion { get; private set; }
         public int MaxTransactionsPerBlock { get; private set; }
         public string[] StandbyValidators { get; private set; }
         public string[] SeedList { get; private set; }
         public IReadOnlyDictionary<TransactionType, Fixed8> SystemFee { get; private set; }
+        public uint SecondsPerBlock { get; private set; }
+        public IConfigurationSection ConfigurationSection { get; set; }
 
         public static Settings Default { get; private set; }
 
@@ -31,6 +34,8 @@ namespace Neo
             this.StandbyValidators = section.GetSection("StandbyValidators").GetChildren().Select(p => p.Value).ToArray();
             this.SeedList = section.GetSection("SeedList").GetChildren().Select(p => p.Value).ToArray();
             this.SystemFee = section.GetSection("SystemFee").GetChildren().ToDictionary(p => (TransactionType)Enum.Parse(typeof(TransactionType), p.Key, true), p => Fixed8.Parse(p.Value));
+            var secondsPerBlockSection = section.GetSection("SecondsPerBlock");
+            this.SecondsPerBlock = secondsPerBlockSection.Value == null ? 15 : uint.Parse(secondsPerBlockSection.Value);
         }
 
         public T GetValueOrDefault<T>(IConfigurationSection section, T defaultValue, Func<string, T> selector)
