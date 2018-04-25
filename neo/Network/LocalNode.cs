06288c5264408e0af8e7830ffe959155e0a95616
Jeff Solinsky <jeff@aphelion.org>
2018-04-24 18:32:17 -0700

    Remove transactions for complete blocks from the mem_pool atomically.

--- neo/Network/LocalNode.cs
+++ neo/Network/LocalNode.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Neo.Core;
-using Neo.IO;
 using Neo.IO.Caching;
 using Neo.Network.Payloads;
 using System;
@@ -49,12 +48,14 @@ namespace Neo.Network
         private IWebHost ws_host;
         private Thread connectThread;
         private Thread poolThread;
+        private Thread memPoolUpdaterThread;
         private readonly AutoResetEvent new_tx_event = new AutoResetEvent(false);
+        private readonly AutoResetEvent persisted_block_event = new AutoResetEvent(false);
         private int started = 0;
         private int disposed = 0;
         private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
 
-        private ConcurrentQueue<UInt256> persistedTxHashQueue = new ConcurrentQueue<UInt256>();
+        private Queue<UInt256> persistedTxHashQueue = new Queue<UInt256>();
 
         public bool GlobalMissionsEnabled { get; set; } = true;
         public int RemoteNodeCount => connectedPeers.Count;
@@ -83,6 +84,13 @@ namespace Neo.Network
                     IsBackground = true,
                     Name = "LocalNode.AddTransactionLoop",
                     Priority = ThreadPriority.BelowNormal
+                };
+
+                this.memPoolUpdaterThread = new Thread(MemPoolUpdateLoop)
+                {
+                    IsBackground = true,
+                    Name = "LocalNode.MemPoolUpdateLoop",
+                    Priority = ThreadPriority.Highest
                 };
             }
             this.UserAgent = string.Format("/NEO:{0}/", GetType().GetTypeInfo().Assembly.GetName().Version.ToString(3));
@@ -138,9 +146,6 @@ namespace Neo.Network
                 ConcurrentBag<Transaction> verified = new ConcurrentBag<Transaction>();
                 lock (mem_pool)
                 {
-                    while (persistedTxHashQueue.TryDequeue(out UInt256 persistedTxHash)) 
-                        mem_pool.Remove(persistedTxHash);
-                    
                     Transaction[] transactions;
                     lock (temp_pool)
                     {
@@ -173,6 +178,30 @@ namespace Neo.Network
             }
         }
 
+        private void MemPoolUpdateLoop()
+        {
+            while (!cancellationTokenSource.IsCancellationRequested)
+            {
+                persisted_block_event.WaitOne();
+
+                List<UInt256> persistedTxHashes;
+                lock (persistedTxHashQueue)
+                {
+                    persistedTxHashes = new List<UInt256>(persistedTxHashQueue.Count);
+                    while (persistedTxHashQueue.Count > 0)
+                        persistedTxHashes.Add(persistedTxHashQueue.Dequeue());
+                }
+
+                lock (mem_pool)
+                {
+                    foreach (var persistedTxHash in persistedTxHashes)
+                        mem_pool.Remove(persistedTxHash);
+                }
+                
+                new_tx_event.Set();
+            }
+        }
+
         public static void AllowHashes(IEnumerable<UInt256> hashes)
         {
             lock (KnownHashes)
@@ -184,10 +213,13 @@ namespace Neo.Network
 
         private void Blockchain_PersistCompleted(object sender, Block block)
         {
-            foreach (Transaction tx in block.Transactions)
-                persistedTxHashQueue.Enqueue(tx.Hash);
+            lock (persistedTxHashQueue)
+            {
+                foreach (Transaction tx in block.Transactions)
+                    persistedTxHashQueue.Enqueue(tx.Hash);                
+            }
 
-            new_tx_event.Set();
+            persisted_block_event.Set();
         }
 
         private static bool CheckKnownHashes(UInt256 hash)
@@ -350,9 +382,14 @@ namespace Neo.Network
                     }
                     Task.WaitAll(nodes.Select(p => Task.Run(() => p.Disconnect(false))).ToArray());
 
+                    persisted_block_event.Set();
+                    if (memPoolUpdaterThread?.ThreadState.HasFlag(ThreadState.Unstarted) == false)
+                        memPoolUpdaterThread.Join();
+                    
                     new_tx_event.Set();
                     if (poolThread?.ThreadState.HasFlag(ThreadState.Unstarted) == false)
                         poolThread.Join();
+                    
                     // Need to ensure any outstanding calls to Blockchain_PersistCompleted are not in progress.
                     // TODO: could add locking instead of using an arbitrarily long sleep here.
                     Thread.Sleep(3000);
