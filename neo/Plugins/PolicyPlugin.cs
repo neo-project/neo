e8bb7ebf81fe91fd0f1a829eaf805c52db587229
erikzhang <erik@vcage.com>
2018-05-16 15:44:05 +0800

    add `PolicyPlugin`

---
+++ neo/Plugins/PolicyPlugin.cs
@@ -0,0 +1,19 @@
+﻿using Neo.Core;
+using System.Collections.Generic;
+
+namespace Neo.Plugins
+{
+    public abstract class PolicyPlugin : Plugin
+    {
+        private static readonly List<PolicyPlugin> instances = new List<PolicyPlugin>();
+
+        public new static IEnumerable<PolicyPlugin> Instances => instances;
+
+        protected PolicyPlugin()
+        {
+            instances.Add(this);
+        }
+
+        internal protected abstract bool CheckPolicy(Transaction tx);
+    }
+}
