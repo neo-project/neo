cce3eb1cbb90ac7fd6446f37263249f66f3814ff
erikzhang <erik@vcage.com>
2018-05-23 18:22:24 +0800

    add Enumerator APIs

---
+++ neo/SmartContract/Enumerators/IteratorKeysWrapper.cs
@@ -0,0 +1,30 @@
+﻿using Neo.SmartContract.Iterators;
+using Neo.VM;
+
+namespace Neo.SmartContract.Enumerators
+{
+    internal class IteratorKeysWrapper : IEnumerator
+    {
+        private readonly IIterator iterator;
+
+        public IteratorKeysWrapper(IIterator iterator)
+        {
+            this.iterator = iterator;
+        }
+
+        public void Dispose()
+        {
+            iterator.Dispose();
+        }
+
+        public bool Next()
+        {
+            return iterator.Next();
+        }
+
+        public StackItem Value()
+        {
+            return iterator.Key();
+        }
+    }
+}
