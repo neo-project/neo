1788d0113f41df9c410253152e84dbf2b3b2cb17
Shargon <shargon@gmail.com>
2018-07-17 02:58:37 +0200

    Remove enter

--- neo/Consensus/ConsensusMessageType.cs
+++ neo/Consensus/ConsensusMessageType.cs
@@ -10,8 +10,7 @@ namespace Neo.Consensus
         PrepareRequest = 0x20,
         [ReflectionCache(typeof(PrepareResponse))]
         PrepareResponse = 0x21,
-
         [ReflectionCache(typeof(CommitAgreement))]
-        CommitAgreement = 0x030,
+        CommitAgreement = 0x022,
     }
 }
