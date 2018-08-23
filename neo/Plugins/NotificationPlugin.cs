ab3b1af439b3f3628f42762f82ddf8c814e571ed
localhuman <tasaunders@gmail.com>
2018-08-22 21:09:42 -0500

    Add NotificationPlugin interface

---
+++ neo/Plugins/NotificationPlugin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Neo.Plugins
+{
+    public abstract class NotificationPlugin : Plugin
+    {
+        private static readonly List<NotificationPlugin> instances = new List<NotificationPlugin>();
+
+        public new static IEnumerable<NotificationPlugin> Instances => instances;
+
+        public string UserAgent { get; set; }
+
+        protected NotificationPlugin()
+        {
+            instances.Add(this);
+        }
+
+        public abstract bool StartPersistingNotifications();
+        public abstract bool StartRESTApi();
+
+    }
+}
