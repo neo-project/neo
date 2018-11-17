135bc8be9a737e5713711c726f7902a16abf317f
Igor Machado <igor.machado@gmail.com>
2018-11-17 19:41:17 -0200

    get root

--- neo/Persistence/MPT.cs
+++ neo/Persistence/MPT.cs
@@ -13,13 +13,28 @@ namespace Neo.Persistence
     // Merkle Patricia Tree
     public class MPT
     {
+        private static MPTKey GetMPTRootKey(Snapshot snapshot, UInt160 contract)
+        {
+            UInt256 rootHash = snapshot[contract].MPTHashRoot;
+            return new MPTKey  {
+                            ScriptHash = contract,
+                            HashKey = rootHash
+                        };
+        }
+
         public static void AddToStorage(Snapshot snapshot, UInt160 contract, StorageKey key, StorageItem item)
         {
+            MPTKey rootKey = GetMPTRootKey(snapshot, contract);
+            MPTItem rootItem = snapshot.MPTStorages[rootKey];
+
             // TODO: include on MPT or update!
         }
 
         public static void DeleteFromStorage(Snapshot snapshot, UInt160 contract, StorageKey key)
         {
+            MPTKey rootKey = GetMPTRootKey(snapshot, contract);
+            MPTItem rootItem = snapshot.MPTStorages[rootKey];
+
             // TODO: include on MPT
         }
     }
