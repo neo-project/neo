3e4763bc3075c0a2a3638582b73b255f8b1f0c69
Igor Machado Coelho <igor.machado@gmail.com>
2018-11-19 19:44:49 -0200

    cleaning dependencies

--- neo/Consensus/ConsensusTimer.cs
+++ neo/Consensus/ConsensusTimer.cs
@@ -1,18 +1,3 @@
-using Akka.Actor;
-using Akka.Configuration;
-using Neo.Cryptography;
-using Neo.IO;
-using Neo.IO.Actors;
-using Neo.Ledger;
-using Neo.Network.P2P;
-using Neo.Network.P2P.Payloads;
-using Neo.Persistence;
-using Neo.Plugins;
-using Neo.Wallets;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace Neo.Consensus
 {
     internal class ConsensusTimer
