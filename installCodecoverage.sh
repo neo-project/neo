989d7e66befef4e8e4cba3d50a1f671f16981746
Rodolfo Araujo <rodoufu@gmail.com>
2018-11-25 16:06:46 -0200

    Code coverage features.

---
+++ installCodecoverage.sh
@@ -0,0 +1,5 @@
+echo Installing coverlet https://github.com/tonerdo/coverlet
+dotnet tool install --global coverlet.console
+echo Installing reportgenerator https://github.com/danielpalme/ReportGenerator
+dotnet tool install -g dotnet-reportgenerator-globaltool
+dotnet tool install dotnet-reportgenerator-globaltool --tool-path tools
