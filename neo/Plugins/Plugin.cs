d2f22670b5a6e978bf990d0e78c09739af8ae8d6
Shargon <shargon@gmail.com>
2018-11-28 11:01:02 +0100

    New plugin feature : hot config change
    Now is possible to change the config of plugins and refresh the plugin itself

--- neo/Plugins/Plugin.cs
+++ neo/Plugins/Plugin.cs
@@ -1,4 +1,5 @@
-﻿using Neo.Network.P2P.Payloads;
+﻿using Microsoft.Extensions.Configuration;
+using Neo.Network.P2P.Payloads;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,6 +9,8 @@ namespace Neo.Plugins
 {
     public abstract class Plugin
     {
+        private static FileSystemWatcher _configWatcher;
+
         public static readonly List<Plugin> Plugins = new List<Plugin>();
         private static readonly List<ILogPlugin> Loggers = new List<ILogPlugin>();
         internal static readonly List<IPolicyPlugin> Policies = new List<IPolicyPlugin>();
@@ -17,12 +20,14 @@ namespace Neo.Plugins
         protected static NeoSystem System { get; private set; }
         public virtual string Name => GetType().Name;
         public virtual Version Version => GetType().Assembly.GetName().Version;
+        public virtual string ConfigFile => Path.GetFullPath(Path.Combine("Plugins", GetType().Assembly.GetName().Name, "config.json"));
 
         protected virtual bool OnMessage(object message) => false;
 
         protected Plugin()
         {
             Plugins.Add(this);
+
             if (this is ILogPlugin logger) Loggers.Add(logger);
             if (this is IPolicyPlugin policy) Policies.Add(policy);
             if (this is IRpcPlugin rpc) RpcPlugins.Add(rpc);
@@ -39,9 +44,26 @@ namespace Neo.Plugins
 
         internal static void LoadPlugins(NeoSystem system)
         {
+            if (_configWatcher != null)
+            {
+                _configWatcher?.Dispose();
+                _configWatcher = null;
+            }
+
             System = system;
             string path = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Plugins");
             if (!Directory.Exists(path)) return;
+
+            _configWatcher = new FileSystemWatcher(path, "*.json")
+            {
+                EnableRaisingEvents = true,
+                IncludeSubdirectories = true,
+                NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.LastWrite | NotifyFilters.Size,
+            };
+
+            _configWatcher.Changed += _configWatcher_Changed;
+            _configWatcher.Created += _configWatcher_Changed;
+
             foreach (string filename in Directory.EnumerateFiles(path, "*.dll", SearchOption.TopDirectoryOnly))
             {
                 Assembly assembly = Assembly.LoadFile(filename);
@@ -49,11 +71,31 @@ namespace Neo.Plugins
                 {
                     if (!type.IsSubclassOf(typeof(Plugin))) continue;
                     if (type.IsAbstract) continue;
+
                     ConstructorInfo constructor = type.GetConstructor(Type.EmptyTypes);
                     if (constructor == null) continue;
-                    constructor.Invoke(null);
+
+                    var plugin = (Plugin)constructor.Invoke(null);
+                    plugin?.Configure(assembly.GetConfiguration());
                 }
             }
+        }
+
+        private static void _configWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            foreach (var plugin in Plugins)
+            {
+                if (plugin.ConfigFile == e.FullPath)
+                {
+                    plugin.Configure(plugin.GetType().Assembly.GetConfiguration());
+                    break;
+                }
+            }
+        }
+
+        public virtual void Configure(IConfigurationSection config)
+        {
+            Log(Name, LogLevel.Info, $"Loaded config for {Name}");
         }
 
         public static void Log(string source, LogLevel level, string message)
