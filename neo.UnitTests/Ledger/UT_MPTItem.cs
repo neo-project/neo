58f7f579acadba346dcb6c290095f176f1be10bc
Rodolfo Araujo <rodoufu@gmail.com>
2018-12-08 13:17:46 -0200

    Refactoring.

--- neo.UnitTests/Ledger/UT_MPTItem.cs
+++ neo.UnitTests/Ledger/UT_MPTItem.cs
@@ -9,38 +9,38 @@ namespace Neo.UnitTests.Ledger
     [TestClass]
     public class UT_MPTItem
     {
-        private MPTItem mptItem;
+        private MPTItem _mptItem;
 
         [TestInitialize]
         public void TestSetup()
         {
-            mptItem = new MPTItem();
+            _mptItem = new MPTItem();
         }
 
         [TestMethod]
         public void Serialize_Leaf()
         {
-            mptItem.NodeType = MPTItem.MPTNodeType.Leaf;
-            mptItem.Path = "2";
-            mptItem.Value = "abc";
-            mptItem.KeyHash = new UInt256(System.Text.Encoding.UTF8.GetBytes("oi").Sha256());
-            mptItem.Hashes = new UInt256[0];
+            _mptItem.NodeType = MPTItem.MPTNodeType.Leaf;
+            _mptItem.Path = "2";
+            _mptItem.Value = "abc";
+            _mptItem.KeyHash = new UInt256(System.Text.Encoding.UTF8.GetBytes("oi").Sha256());
+            _mptItem.Hashes = new UInt256[0];
             
             using (var ms = new MemoryStream())
             {
                 using (var bw = new BinaryWriter(ms))
                 {
-                    mptItem.Serialize(bw);
+                    _mptItem.Serialize(bw);
                     using (var br = new BinaryReader(bw.BaseStream))
                     {
                         br.BaseStream.Position = 0;
                         var mptResposta = new MPTItem();
                         mptResposta.Deserialize(br);
 
-                        mptResposta.NodeType.Should().Be(mptItem.NodeType);
-                        mptResposta.Path.Should().Be(mptItem.Path);
-                        mptResposta.Value.Should().Be(mptItem.Value);
-                        mptResposta.KeyHash.Should().Be(mptItem.KeyHash);
+                        mptResposta.NodeType.Should().Be(_mptItem.NodeType);
+                        mptResposta.Path.Should().Be(_mptItem.Path);
+                        mptResposta.Value.Should().Be(_mptItem.Value);
+                        mptResposta.KeyHash.Should().Be(_mptItem.KeyHash);
                     }
                 }
             }
