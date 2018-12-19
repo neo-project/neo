a8fd76b715e675bc7982f26481ecfb9b6ea68811
Erik Zhang <erik@neo.org>
2018-12-19 20:05:07 +0800

    Rename `Settings` to `ProtocolSettings` and make it be `public` (#526)

--- neo/Network/P2P/Payloads/Transaction.cs
+++ neo/Network/P2P/Payloads/Transaction.cs
@@ -64,7 +64,7 @@ namespace Neo.Network.P2P.Payloads
 
         InventoryType IInventory.InventoryType => InventoryType.TX;
 
-        public bool IsLowPriority => NetworkFee < Settings.Default.LowPriorityThreshold;
+        public bool IsLowPriority => NetworkFee < ProtocolSettings.Default.LowPriorityThreshold;
 
         private Fixed8 _network_fee = -Fixed8.Satoshi;
         public virtual Fixed8 NetworkFee
@@ -110,7 +110,7 @@ namespace Neo.Network.P2P.Payloads
 
         public virtual int Size => sizeof(TransactionType) + sizeof(byte) + Attributes.GetVarSize() + Inputs.GetVarSize() + Outputs.GetVarSize() + Witnesses.GetVarSize();
 
-        public virtual Fixed8 SystemFee => Settings.Default.SystemFee.TryGetValue(Type, out Fixed8 fee) ? fee : Fixed8.Zero;
+        public virtual Fixed8 SystemFee => ProtocolSettings.Default.SystemFee.TryGetValue(Type, out Fixed8 fee) ? fee : Fixed8.Zero;
 
         protected Transaction(TransactionType type)
         {
