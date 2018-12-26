1da4ff6af57bb549ad11249ba16ef8f3d2c2a8ed
switcheo <henry.chua@switcheo.network>
2018-12-26 12:15:13 +0800

    Pass ApplicationExecutionResult to IPersistencePlugin

--- neo/Plugins/IPersistencePlugin.cs
+++ neo/Plugins/IPersistencePlugin.cs
@@ -1,9 +1,11 @@
-﻿using Neo.Persistence;
+﻿using Neo.Ledger;
+using Neo.Persistence;
+using System.Collections.Generic;
 
 namespace Neo.Plugins
 {
     public interface IPersistencePlugin
     {
-        void OnPersist(Snapshot snapshot);
+        void OnPersist(Snapshot snapshot, List<ApplicationExecutionResult> application);
     }
 }
