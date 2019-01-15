1e92cbef7dc6b1a7bdc9e6ceecd42d61c4542a22
Igor Machado Coelho <igor.machado@gmail.com>
2019-01-15 13:19:30 -0200

    Removed enum fixed values

--- neo/Ledger/RelayResultReason.cs
+++ neo/Ledger/RelayResultReason.cs
@@ -2,12 +2,12 @@
 {
     public enum RelayResultReason : byte
     {
-        Succeed = 0x00,
-        AlreadyExists = 0x01,
-        OutOfMemory = 0x02,
-        UnableToVerify = 0x03,
-        Invalid = 0x04,
-        PolicyFail = 0x05,
-        Unknown = 0xff
+        Succeed,
+        AlreadyExists,
+        OutOfMemory,
+        UnableToVerify,
+        Invalid,
+        PolicyFail,
+        Unknown
     }
 }
