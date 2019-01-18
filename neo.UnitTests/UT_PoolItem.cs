31c91d1790f7aeab1ae556f6859fa69fd48baf5c
Igor Machado <igor.machado@gmail.com>
2019-01-18 16:31:31 -0200

    basic test

--- neo.UnitTests/UT_PoolItem.cs
+++ neo.UnitTests/UT_PoolItem.cs
@@ -25,6 +25,30 @@ namespace Neo.UnitTests
         [TestMethod]
         public void PoolItem_CompareTo_Fee()
         {
+            int timeIndex = 0;
+            var timeValues = new[] {
+                new DateTime(1968, 06, 01, 0, 0, 1, DateTimeKind.Utc),
+                new DateTime(1968, 06, 01, 0, 0, 2, DateTimeKind.Utc),
+                new DateTime(1968, 06, 01, 0, 0, 3, DateTimeKind.Utc),
+                new DateTime(1968, 06, 01, 0, 0, 4, DateTimeKind.Utc),
+                new DateTime(1968, 06, 01, 0, 0, 5, DateTimeKind.Utc)
+            };
+            var timeMock = new Mock<TimeProvider>();
+            timeMock.SetupGet(tp => tp.UtcNow).Returns(() => timeValues[timeIndex])
+                                              .Callback(() => timeIndex++);
+            TimeProvider.Current = timeMock.Object;
+
+            int size1 = 50;
+            int netFeeSatoshi1 = 1;
+            var tx1 = MockGenerateInvocationTx(new Fixed8(netFeeSatoshi1), size1, UInt256.Zero);
+            int size2 = 50;
+            int netFeeSatoshi2 = 2;
+            var tx2 = MockGenerateInvocationTx(new Fixed8(netFeeSatoshi2), size2, UInt256.Zero);
+
+            PoolItem pitem1 = new PoolItem(tx1.Object);
+            PoolItem pitem2 = new PoolItem(tx2.Object);
+            // pitem1 < pitem2 (fee) => -1
+            pitem1.CompareTo(pitem2).Should().Be(-1);
         }
 
         [TestMethod]
@@ -35,6 +59,16 @@ namespace Neo.UnitTests
         [TestMethod]
         public void PoolItem_CompareTo_Equals()
         {
+        }
+
+        // Generate Mock InvocationTransaction with different sizes and prices
+        public static Mock<Transaction> MockGenerateInvocationTx(Fixed8 networkFee, int size, UInt256 hash)
+        {
+            var mockTx = new Mock<Transaction>(TransactionType.InvocationTransaction);
+            mockTx.SetupGet(mr => mr.NetworkFee).Returns(networkFee);
+            mockTx.SetupGet(mr => mr.Size).Returns(size);
+            //mockTx.SetupGet(mr => mr.Hash).Returns(hash); // cannot overwrite this method, will see GetHashData
+            return mockTx;
         }
     }
 }
