a9b596205b21ec96928a965e6e5f2cff7afbfdfd
jsolman <jeff@aphelion.org>
2019-02-05 15:49:30 -0800

    Remove unused return values.

--- neo/Plugins/IMemoryPoolTxObserverPlugin.cs
+++ neo/Plugins/IMemoryPoolTxObserverPlugin.cs
@@ -5,7 +5,7 @@ namespace Neo.Plugins
 {
     public interface IMemoryPoolTxObserverPlugin
     {
-        bool AddedTransaction(Transaction tx);
-        bool RemovedTransactions(MemoryPoolTxRemovalReason reason, IEnumerable<Transaction> transactions);
+        void AddedTransaction(Transaction tx);
+        void RemovedTransactions(MemoryPoolTxRemovalReason reason, IEnumerable<Transaction> transactions);
     }
 }
