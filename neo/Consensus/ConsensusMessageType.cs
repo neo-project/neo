862691790c9509a01b9457b8cfb4a5a35745eb00
jsolman <jeff@aphelion.org>
2019-02-11 01:31:48 -0800

    Change RecoveryMessage enum definition value according to PR comment.

--- neo/Consensus/ConsensusMessageType.cs
+++ neo/Consensus/ConsensusMessageType.cs
@@ -10,10 +10,11 @@ namespace Neo.Consensus
         PrepareRequest = 0x20,
         [ReflectionCache(typeof(PrepareResponse))]
         PrepareResponse = 0x21,
-        [ReflectionCache(typeof(RecoveryMessage))]
-        RecoveryMessage = 0x22,
 
         [ReflectionCache(typeof(Commit))]
         Commit = 0x30,
+
+        [ReflectionCache(typeof(RecoveryMessage))]
+        RecoveryMessage = 0x41,
     }
 }
