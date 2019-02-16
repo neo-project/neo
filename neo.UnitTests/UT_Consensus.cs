700a0aa11d75530704232dd9aaea8b4ba483a8ed
jsolman <jeff@aphelion.org>
2019-02-16 00:36:29 -0800

    Fix test compile.

--- neo.UnitTests/UT_Consensus.cs
+++ neo.UnitTests/UT_Consensus.cs
@@ -15,6 +15,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Security.Cryptography;
+using Neo.Persistence.LevelDB;
 using ECPoint = Neo.Cryptography.ECC.ECPoint;
 
 namespace Neo.UnitTests
