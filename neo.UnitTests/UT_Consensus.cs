3dd2928fea7c841a006ab30242a232876625d46e
jsolman <jeff@aphelion.org>
2019-02-19 12:05:18 -0800

    Remove unused using statement.

--- neo.UnitTests/UT_Consensus.cs
+++ neo.UnitTests/UT_Consensus.cs
@@ -15,7 +15,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Security.Cryptography;
-using Neo.Persistence.LevelDB;
 using ECPoint = Neo.Cryptography.ECC.ECPoint;
 
 namespace Neo.UnitTests
