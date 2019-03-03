f88c427987791e11387ca23eadb619a5e9fdfff9
Vitor Nazário Coelho <vncoelho@gmail.com>
2019-03-03 05:53:14 -0500

    dBFT 2.0 (#547)
    * Add commit phase to consensus algorithm (#534)
    
    * Add commit phase to consensus algorithm
    
    * fix tests
    
    * Prevent repeated sending of `Commit` messages
    
    * RPC call gettransactionheight (#541)
    
    * getrawtransactionheight
    
    Nowadays two calls are need to get a transaction height, `getrawtransaction` with `verbose` and then use the `blockhash`.
    Other option is to use `confirmations`, but it can be misleading.
    
    * Minnor fix
    
    * Shargon's tip
    
    * modified
    
    * Allow to use the wallet inside a RPC plugin (#536)
    
    * Clean code
    
    * Clean code
    
    * Minor fix on mempoolVerified
    
    * Add MemoryPool Unit tests. Fix bug on initital start of Persisting the Genesis block.
    
    * Prevent `ConsensusService` from receiving messages before starting (#573)
    
    * Prevent `ConsensusService` from receiving messages before starting
    
    * fixed tests - calling OnStart now
    
    * Consensus recovery log (#572)
    
    * Pass store to `ConsensusService`
    
    * Implement `ISerializable` in `ConsensusContext`
    
    * Start from recovery log
    
    * Fix unit tests due to constructor taking the store.
    
    * Add unit tests for serializing and deserializing the consensus context.
    
    * Combine `ConsensusContext.ChangeView()` and `ConsensusContext.Reset()`
    
    * Add `PreparationHash` field to `PrepareResponse` to prevent replay attacks from malicious primary (#576)
    
    * Fixed a problem where `PrepareResponse.PreparationHash` was not assigned.
    
    * Load context from store only when height matches
    
    * Recover nodes requesting ChangeView when possible (#579)
    
    * Fixes bug in `OnPrepareRequestReceived()`
    
    * Send `RecoveryMessage` only when `message.NewViewNumber <= context.ViewNumber`
    
    * Fix and optimize view changing (#590)
    
    * Allow to ignore the recovery logs
    
    * Add `isRecovering` (#594)
    
    * Fix accepting own prepare request (#596)
    
    * Pick some changes from #575.
    
    * Fixes `Prefixes`
    
    * Restore transactions from saved consensus context (#598)
    
    * Refactoring
    
    * AggressiveInlining (#606)
    
    * Reset Block reference when consensus context is initialized after block persist. (#608)
    
    * Change `ConsensusPayload` for compatibility (#609)

--- neo/Persistence/LevelDB/Prefixes.cs
+++ neo/Persistence/LevelDB/Prefixes.cs
@@ -19,5 +19,10 @@
         public const byte IX_CurrentHeader = 0xc1;
 
         public const byte SYS_Version = 0xf0;
+
+        /* Prefixes 0xf1 to 0xff are reserved for external use.
+         *
+         * Note: The saved consensus state uses the Prefix 0xf4
+         */
     }
 }
