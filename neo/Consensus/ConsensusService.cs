84507bd1ba10842c2423a509438c7632f115e14b
Vitor Nazário Coelho <vncoelho@gmail.com>
2019-03-08 13:40:16 -0300

    Removing primary timeout adjust

--- neo/Consensus/ConsensusService.cs
+++ neo/Consensus/ConsensusService.cs
@@ -529,7 +529,7 @@ namespace Neo.Consensus
                 foreach (InvPayload payload in InvPayload.CreateGroup(InventoryType.TX, context.TransactionHashes.Skip(1).ToArray()))
                     localNode.Tell(Message.Create("inv", payload));
             }
-            ChangeTimer(TimeSpan.FromSeconds((Blockchain.SecondsPerBlock << (context.ViewNumber + 1)) - Blockchain.SecondsPerBlock));
+            ChangeTimer(TimeSpan.FromSeconds(Blockchain.SecondsPerBlock << (context.ViewNumber + 1)));
         }
 
         private bool VerifyRequest()
