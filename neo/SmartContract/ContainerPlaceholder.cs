0a06049479889da9af1804521455b06668bc7ce1
Shargon <shargon@gmail.com>
2019-03-21 10:10:59 +0100

    Fix new abstract method

--- neo/SmartContract/ContainerPlaceholder.cs
+++ neo/SmartContract/ContainerPlaceholder.cs
@@ -13,6 +13,11 @@ namespace Neo.SmartContract
             throw new NotSupportedException();
         }
 
+        public override bool GetBoolean()
+        {
+            throw new NotImplementedException();
+        }
+
         public override byte[] GetByteArray()
         {
             throw new NotSupportedException();
