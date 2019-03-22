cc4536a3cb7c6ae9253c12324b768b4bbd51f9b8
Shargon <shargon@gmail.com>
2019-03-22 16:10:21 +0100

    Update Settings.cs

--- neo/Settings.cs
+++ neo/Settings.cs
@@ -34,7 +34,7 @@ namespace Neo
             this.SystemFee = section.GetSection("SystemFee").GetChildren().ToDictionary(p => (TransactionType)Enum.Parse(typeof(TransactionType), p.Key, true), p => Fixed8.Parse(p.Value));
             this.SecondsPerBlock = GetValueOrDefault(section.GetSection("SecondsPerBlock"), 15u, p => uint.Parse(p));
             this.LowPriorityThreshold = GetValueOrDefault(section.GetSection("LowPriorityThreshold"), Fixed8.FromDecimal(0.001m), p => Fixed8.Parse(p));
-            this.HashCacheSize = GetValueOrDefault(section.GetSection("HashCacheSize"), 100, Int32.Parse);
+            this.HashCacheSize = GetValueOrDefault(section.GetSection("HashCacheSize"), 32 * 1024, Int32.Parse);
         }
 
         internal T GetValueOrDefault<T>(IConfigurationSection section, T defaultValue, Func<string, T> selector)
