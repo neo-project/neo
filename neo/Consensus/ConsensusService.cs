96d12a663bd3dc635a733408562fbee1b213cd2b
Vitor <vncoelho@gmail.com>
2019-03-27 12:24:09 -0300

    Ensuring continous ping between CN

--- neo/Consensus/ConsensusService.cs
+++ neo/Consensus/ConsensusService.cs
@@ -541,7 +541,10 @@ namespace Neo.Consensus
             ChangeTimer(TimeSpan.FromSeconds(Blockchain.SecondsPerBlock << (expectedView + 1)));
             if ((context.CountCommitted() + context.CountFailed()) > context.F())
             {
-                Log($"Node will not request change view to nv={expectedView} because nc={context.CountCommitted()}+nf={context.CountFailed()} > {context.F()}");
+                Log($"Node will not request change view to nv={expectedView} because nc={context.CountCommitted()} nf={context.CountFailed()}");
+                // asking for recovery (ping)
+                if (context.BlockIndex == Blockchain.Singleton.HeaderHeight + 1 && !context.WatchOnly())
+                    localNode.Tell(new LocalNode.SendDirectly { Inventory = context.MakeChangeView(0) });
                 return;
             }
             Log($"request change view: height={context.BlockIndex} view={context.ViewNumber} nv={expectedView} nc={context.CountCommitted()} nf={context.CountFailed()}");
