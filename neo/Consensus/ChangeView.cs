67f93530f0753520005454220337ff2fd8a8ff1f
jsolman <jsolinsky@gmail.com>
2019-03-27 22:16:29 -0700

    Fix broken comment format.

--- neo/Consensus/ChangeView.cs
+++ neo/Consensus/ChangeView.cs
@@ -7,8 +7,8 @@ namespace Neo.Consensus
         public byte NewViewNumber;
         /// <summary>
         /// Timestamp of when the ChangeView message was created. This allows receiving nodes to ensure
-        // they only respond once to a specific ChangeView request (it thus prevents replay of the ChangeView
-        // message from repeatedly broadcasting RecoveryMessages).
+        /// they only respond once to a specific ChangeView request (it thus prevents replay of the ChangeView
+        /// message from repeatedly broadcasting RecoveryMessages).
         /// </summary>
         public uint Timestamp;
 
