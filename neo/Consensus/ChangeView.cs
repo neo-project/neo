6065e626e08ae6207fadb73bca7043e17419a721
Shargon <shargon@gmail.com>
2019-03-29 11:22:18 +0100

    Update ChangeView.cs

--- neo/Consensus/ChangeView.cs
+++ neo/Consensus/ChangeView.cs
@@ -7,7 +7,7 @@ namespace Neo.Consensus
         /// <summary>
         /// NewViewNumber is always set to the current ViewNumber asking changeview + 1
         /// </summary>
-        public byte NewViewNumber => ViewNumber + 1;
+        public byte NewViewNumber => (byte)(ViewNumber + 1);
         /// <summary>
         /// Timestamp of when the ChangeView message was created. This allows receiving nodes to ensure
         /// they only respond once to a specific ChangeView request (it thus prevents replay of the ChangeView
@@ -19,10 +19,7 @@ namespace Neo.Consensus
             + sizeof(byte)  //NewViewNumber
             + sizeof(uint); //Timestamp
 
-        public ChangeView()
-            : base(ConsensusMessageType.ChangeView)
-        {
-        }
+        public ChangeView() : base(ConsensusMessageType.ChangeView) { }
 
         public override void Deserialize(BinaryReader reader)
         {
