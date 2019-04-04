14ae77834a3fdb3a2e2ca5191c48771f89737d4b
Shargon <shargon@gmail.com>
2019-04-04 10:56:51 +0200

    Update NeoSystem.cs

--- neo/NeoSystem.cs
+++ neo/NeoSystem.cs
@@ -21,7 +21,7 @@ namespace Neo
             $"remote-node-mailbox {{ mailbox-type: \"{typeof(RemoteNodeMailbox).AssemblyQualifiedName}\" }}" +
             $"protocol-handler-mailbox {{ mailbox-type: \"{typeof(ProtocolHandlerMailbox).AssemblyQualifiedName}\" }}" +
             $"consensus-service-mailbox {{ mailbox-type: \"{typeof(ConsensusServiceMailbox).AssemblyQualifiedName}\" }}" +
-                $"blockchain-dispatcher {{ type = PinnedDispatcher }}");
+            $"blockchain-dispatcher {{ type = PinnedDispatcher }}");
         public IActorRef Blockchain { get; }
         public IActorRef LocalNode { get; }
         internal IActorRef TaskManager { get; }
