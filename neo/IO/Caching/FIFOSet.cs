ef4cfcdf47702c67b7755a08f83c88f51bb90365
Yongjie Ma <20391402+yongjiema@users.noreply.github.com>
2019-04-08 04:24:20 +0800

    Fix memory leak as transactions are continuously received. (#644)
    * Fix memory leak if connections keep connecting
    
    * Make protocol hash size configurable
    
    * Clean and optimize code
    
    * Ensure that fifoset works fine
    
    * Change to HashSet
    
    * Revert to use OrderedDictionary
    
    * Clean code
    
    - Add readonly
    
    * Update FIFOSet.cs
    
    * Update Settings.cs
    
    * Updates
    
    - Rename Settings as the class name
    - Remove Settings and hardoce a good value
    
    * Update FIFOSet.cs
    
    * format
    
    * format
    
    * Not allow `removeCount` to be `0`
    
    * rename

---
+++ neo/IO/Caching/FIFOSet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace Neo.IO.Caching
+{
+    internal class FIFOSet<T> where T : IEquatable<T>
+    {
+        private readonly int maxCapacity;
+        private readonly int removeCount;
+        private readonly OrderedDictionary dictionary;
+
+        public FIFOSet(int maxCapacity, decimal batchSize = 0.1m)
+        {
+            if (maxCapacity <= 0) throw new ArgumentOutOfRangeException(nameof(maxCapacity));
+            if (batchSize <= 0 || batchSize > 1) throw new ArgumentOutOfRangeException(nameof(batchSize));
+
+            this.maxCapacity = maxCapacity;
+            this.removeCount = Math.Max((int)(maxCapacity * batchSize), 1);
+            this.dictionary = new OrderedDictionary(maxCapacity);
+        }
+
+        public bool Add(T item)
+        {
+            if (dictionary.Contains(item)) return false;
+            if (dictionary.Count >= maxCapacity)
+            {
+                if (removeCount == maxCapacity)
+                {
+                    dictionary.Clear();
+                }
+                else
+                {
+                    for (int i = 0; i < removeCount; i++)
+                        dictionary.RemoveAt(0);
+                }
+            }
+            dictionary.Add(item, null);
+            return true;
+        }
+    }
+}
