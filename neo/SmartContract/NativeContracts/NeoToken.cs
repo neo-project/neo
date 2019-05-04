4c7774c63f90fe4b2b7fad8dc9987f8a2dbe8de0
erikzhang <erik@neo.org>
2019-05-04 23:55:29 +0800

    Prevent being called directly from SYSCALL.

--- neo/SmartContract/NativeContracts/NeoToken.cs
+++ neo/SmartContract/NativeContracts/NeoToken.cs
@@ -19,6 +19,8 @@ namespace Neo.SmartContract
 
         private bool NeoToken_Main(ExecutionEngine engine)
         {
+            if (!new UInt160(engine.CurrentContext.ScriptHash).Equals(Blockchain.NeoToken.ScriptHash))
+                return false;
             string operation = engine.CurrentContext.EvaluationStack.Pop().GetString();
             VMArray args = (VMArray)engine.CurrentContext.EvaluationStack.Pop();
             StackItem result;
