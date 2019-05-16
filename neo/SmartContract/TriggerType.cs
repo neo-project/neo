2813c10f95571b350d6642d6ee47aaa891490b84
erikzhang <erik@neo.org>
2019-05-16 14:01:37 +0800

    Assign values to `TriggerType`

--- neo/SmartContract/TriggerType.cs
+++ neo/SmartContract/TriggerType.cs
@@ -2,7 +2,7 @@ namespace Neo.SmartContract
 {
     public enum TriggerType : byte
     {
-        System,
+        System = 0x00,
         /// <summary>
         /// The verification trigger indicates that the contract is being invoked as a verification function.
         /// The verification function can accept multiple parameters, and should return a boolean value that indicates the validity of the transaction or block.
@@ -10,7 +10,7 @@ namespace Neo.SmartContract
         ///     main(...);
         /// The entry point of the contract must be able to handle this type of invocation.
         /// </summary>
-        Verification,
+        Verification = 0x20,
         /// <summary>
         /// The application trigger indicates that the contract is being invoked as an application function.
         /// The application function can accept multiple parameters, change the states of the blockchain, and return any type of value.
@@ -18,6 +18,6 @@ namespace Neo.SmartContract
         ///     public byte[] main(string operation, params object[] args)
         /// The functions can be invoked by creating an InvocationTransaction.
         /// </summary>
-        Application,
+        Application = 0x40
     }
 }
