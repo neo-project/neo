cefc6ae8ca18c8ed6018732a9a0eac51b0c290f5
Shargon <shargon@gmail.com>
2019-05-17 10:42:28 +0200

    Update NodeCapabilityType.cs

--- neo/Network/P2P/Capabilities/NodeCapabilityType.cs
+++ neo/Network/P2P/Capabilities/NodeCapabilityType.cs
@@ -2,9 +2,13 @@
 {
     public enum NodeCapabilityType : byte
     {
+        #region Servers
+        
         TcpServer = 0x01,
         UdpServer = 0x02,
         WsServer = 0x03,
+        
+        #endregion
 
         FullNode = 0x10
     }
