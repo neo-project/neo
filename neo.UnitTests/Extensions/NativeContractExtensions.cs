c4b16dc9bb5eea150cd50a34c6112d99a7018701
Shargon <shargon@gmail.com>
2019-05-19 23:12:01 +0200

    Unit test (#760)

---
+++ neo.UnitTests/Extensions/NativeContractExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Neo.SmartContract;
+using Neo.SmartContract.Native;
+using Neo.VM;
+using System;
+
+namespace Neo.UnitTests.Extensions
+{
+    public static class NativeContractExtensions
+    {
+        public static StackItem Call(this NativeContract contract, Persistence.Snapshot snapshot, string method)
+        {
+            var engine = new ApplicationEngine(TriggerType.Application, null, snapshot, 0, true);
+
+            engine.LoadScript(contract.Script);
+
+            var script = new ScriptBuilder();
+            script.EmitPush(0);
+            script.Emit(OpCode.PACK);
+            script.EmitPush(method);
+            engine.LoadScript(script.ToArray());
+
+            if (engine.Execute() != VMState.HALT)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return engine.ResultStack.Pop();
+        }
+    }
+}
---
+++ neo.UnitTests/UT_Policy.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Persistence;
+using Neo.SmartContract;
+using Neo.SmartContract.Native;
+using Neo.UnitTests.Extensions;
+using System.Linq;
+
+namespace Neo.UnitTests
+{
+    [TestClass]
+    public class UT_Policy
+    {
+        Store Store;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            TestBlockchain.InitializeMockNeoSystem();
+            Store = TestBlockchain.GetStore();
+        }
+
+        [TestMethod]
+        public void Initialize()
+        {
+            var snapshot = Store.GetSnapshot().Clone();
+            var keyCount = snapshot.Storages.GetChangeSet().Count();
+
+            NativeContract.Policy.Initialize(new ApplicationEngine(TriggerType.Application, null, snapshot, 0)).Should().BeTrue();
+
+            (keyCount + 5).Should().Be(snapshot.Storages.GetChangeSet().Count());
+
+            var ret = NativeContract.Policy.Call(snapshot, "getMaxTransactionsPerBlock");
+            ret.Should().BeOfType<VM.Types.Integer>();
+            ret.GetBigInteger().Should().Be(512);
+
+            ret = NativeContract.Policy.Call(snapshot, "getMaxLowPriorityTransactionsPerBlock");
+            ret.Should().BeOfType<VM.Types.Integer>();
+            ret.GetBigInteger().Should().Be(20);
+
+            ret = NativeContract.Policy.Call(snapshot, "getMaxLowPriorityTransactionSize");
+            ret.Should().BeOfType<VM.Types.Integer>();
+            ret.GetBigInteger().Should().Be(256);
+
+            ret = NativeContract.Policy.Call(snapshot, "getFeePerByte");
+            ret.Should().BeOfType<VM.Types.Integer>();
+            ret.GetBigInteger().Should().Be(1000);
+
+            ret = NativeContract.Policy.Call(snapshot, "getBlockedAccounts");
+            ret.Should().BeOfType<VM.Types.Array>();
+            ((VM.Types.Array)ret).Count.Should().Be(0);
+        }
+    }
+}
