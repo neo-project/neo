f33b0e88f375fe7ba87ed24807c431ea47a232f6
Shargon <shargon@gmail.com>
2019-05-21 17:22:32 +0200

    Clean code

--- neo/SmartContract/Native/NativeContract.cs
+++ neo/SmartContract/Native/NativeContract.cs
@@ -22,7 +22,7 @@ namespace Neo.SmartContract.Native
         public uint ServiceHash { get; }
         public byte[] Script { get; }
         public UInt160 Hash { get; }
-        public virtual ContractManifest Manifest { get; }
+        public ContractManifest Manifest { get; }
         public virtual string[] SupportedStandards { get; } = { "NEP-10" };
 
         protected NativeContract()
