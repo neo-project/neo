55fd3850d223cb01f4dec494114ccf18b0253f48
Shargon <shargon@gmail.com>
2019-05-21 16:54:11 +0200

    Default value in constructor

--- neo/SmartContract/Native/NativeContract.cs
+++ neo/SmartContract/Native/NativeContract.cs
@@ -22,7 +22,7 @@ namespace Neo.SmartContract.Native
         public uint ServiceHash { get; }
         public byte[] Script { get; }
         public UInt160 Hash { get; }
-        public virtual ContractManifest Manifest => ContractManifest.CreateDefault(Hash);
+        public virtual ContractManifest Manifest { get; }
         public virtual string[] SupportedStandards { get; } = { "NEP-10" };
 
         protected NativeContract()
@@ -33,7 +33,10 @@ namespace Neo.SmartContract.Native
                 sb.EmitSysCall(ServiceHash);
                 this.Script = sb.ToArray();
             }
+
             this.Hash = Script.ToScriptHash();
+            this.Manifest = ContractManifest.CreateDefault(this.Hash);
+
             contracts.Add(this);
         }
 
