f10c2307c5336928366ab2585fab36685a07454a
erikzhang <erik@neo.org>
2019-05-24 16:12:12 +0800

    Modify the price of `ConsensusPayload.Verify()`

--- neo/Network/P2P/Payloads/ConsensusPayload.cs
+++ neo/Network/P2P/Payloads/ConsensusPayload.cs
@@ -118,7 +118,7 @@ namespace Neo.Network.P2P.Payloads
         {
             if (BlockIndex <= snapshot.Height)
                 return false;
-            return this.VerifyWitnesses(snapshot, 1_00000000);
+            return this.VerifyWitnesses(snapshot, 0_02000000);
         }
     }
 }
