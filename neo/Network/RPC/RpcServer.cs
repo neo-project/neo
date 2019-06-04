2cbdb14e3d7fd3792623649f086602fe51f95128
Shargon <shargon@gmail.com>
2019-06-04 17:58:25 +0200

    Update RpcServer.cs

--- neo/Network/RPC/RpcServer.cs
+++ neo/Network/RPC/RpcServer.cs
@@ -97,7 +97,6 @@ namespace Neo.Network.RPC
                 case RelayResultReason.Succeed:
                     {
                         var ret = new JObject();
-                        ret["relayed"] = true;
                         ret["hash"] = hash.ToString();
                         return ret;
                     }
