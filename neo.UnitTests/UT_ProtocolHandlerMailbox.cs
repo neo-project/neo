5b0e0451a6e31477bb1c34a743c46df4c59cfaf3
Igor Machado <igor.machado@gmail.com>
2019-06-10 02:33:05 -0300

    testing ShallDrop

--- neo.UnitTests/UT_ProtocolHandlerMailbox.cs
+++ neo.UnitTests/UT_ProtocolHandlerMailbox.cs
@@ -9,6 +9,8 @@ using Neo.Network.P2P.Payloads;
 using Neo.Network.P2P;
 using Akka.Configuration;
 using Neo.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Neo.UnitTests
 {
@@ -50,6 +52,30 @@ namespace Neo.UnitTests
             // any random object (non Message) should not have priority
             object obj = null;
             uut.IsHighPriority(obj).Should().Be(false);
+        }
+
+
+        [TestMethod]
+        public void ProtocolHandlerMailbox_Test_ShallDrop()
+        {
+            // using this for messages
+            ISerializable s = null;
+            // empty queue
+            IEnumerable<object> queue = Enumerable.Empty<object>();
+
+            // any random object (non Message) should be dropped
+            object obj = null;
+            uut.ShallDrop(obj, queue).Should().Be(true);
+
+            // test drop for specific commands (empty queue)
+            uut.ShallDrop(Message.Create("getaddr", s), queue).Should().Be(false);
+            uut.ShallDrop(Message.Create("getblocks", s), queue).Should().Be(false);
+            uut.ShallDrop(Message.Create("getdata", s), queue).Should().Be(false);
+            uut.ShallDrop(Message.Create("getheaders", s), queue).Should().Be(false);
+            uut.ShallDrop(Message.Create("version", s), queue).Should().Be(false);
+            uut.ShallDrop(Message.Create("mempool", s), queue).Should().Be(false);
+            // any random command should not be dropped
+            uut.ShallDrop(Message.Create("_", s), queue).Should().Be(false);
         }
     }
 }
