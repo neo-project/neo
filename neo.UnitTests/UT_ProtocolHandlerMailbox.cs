f91ad9e803025d6854a7a5f76e3581c9d697696d
Igor Machado <igor.machado@gmail.com>
2019-06-12 10:59:39 -0300

    testing all cases

--- neo.UnitTests/UT_ProtocolHandlerMailbox.cs
+++ neo.UnitTests/UT_ProtocolHandlerMailbox.cs
@@ -40,16 +40,39 @@ namespace Neo.UnitTests
         public void ProtocolHandlerMailbox_Test_IsHighPriority()
         {
             ISerializable s = null;
-            // test priority commands
+
+            //handshaking
+            uut.IsHighPriority(Message.Create(MessageCommand.Version, s)).Should().Be(true);
+            uut.IsHighPriority(Message.Create(MessageCommand.Verack, s)).Should().Be(true);
+
+            //connectivity
+            uut.IsHighPriority(Message.Create(MessageCommand.GetAddr, s)).Should().Be(false);
+            uut.IsHighPriority(Message.Create(MessageCommand.Addr, s)).Should().Be(false);
+            uut.IsHighPriority(Message.Create(MessageCommand.Ping, s)).Should().Be(false);
+            uut.IsHighPriority(Message.Create(MessageCommand.Pong, s)).Should().Be(false);
+
+            //synchronization
+            uut.IsHighPriority(Message.Create(MessageCommand.GetHeaders, s)).Should().Be(false);
+            uut.IsHighPriority(Message.Create(MessageCommand.Headers, s)).Should().Be(false);
+            uut.IsHighPriority(Message.Create(MessageCommand.GetBlocks, s)).Should().Be(false);
+            uut.IsHighPriority(Message.Create(MessageCommand.Mempool, s)).Should().Be(false);
+            uut.IsHighPriority(Message.Create(MessageCommand.Inv, s)).Should().Be(false);
+            uut.IsHighPriority(Message.Create(MessageCommand.GetData, s)).Should().Be(false);
+            uut.IsHighPriority(Message.Create(MessageCommand.NotFound, s)).Should().Be(false);
+            uut.IsHighPriority(Message.Create(MessageCommand.Transaction, s)).Should().Be(false);
+            uut.IsHighPriority(Message.Create(MessageCommand.Block, s)).Should().Be(false);
             uut.IsHighPriority(Message.Create(MessageCommand.Consensus, s)).Should().Be(true);
+            uut.IsHighPriority(Message.Create(MessageCommand.Reject, s)).Should().Be(false);
+
+            //SPV protocol
+            uut.IsHighPriority(Message.Create(MessageCommand.FilterLoad, s)).Should().Be(true);
             uut.IsHighPriority(Message.Create(MessageCommand.FilterAdd, s)).Should().Be(true);
             uut.IsHighPriority(Message.Create(MessageCommand.FilterClear, s)).Should().Be(true);
-            uut.IsHighPriority(Message.Create(MessageCommand.FilterLoad, s)).Should().Be(true);
-            uut.IsHighPriority(Message.Create(MessageCommand.Verack, s)).Should().Be(true);
-            uut.IsHighPriority(Message.Create(MessageCommand.Version, s)).Should().Be(true);
+            uut.IsHighPriority(Message.Create(MessageCommand.MerkleBlock, s)).Should().Be(false);
+
+            //others
             uut.IsHighPriority(Message.Create(MessageCommand.Alert, s)).Should().Be(true);
-            // any random command should not have priority
-            uut.IsHighPriority(Message.Create((MessageCommand)'0xff', s)).Should().Be(false);
+
             // any random object (non Message) should not have priority
             object obj = null;
             uut.IsHighPriority(obj).Should().Be(false);
@@ -61,22 +84,111 @@ namespace Neo.UnitTests
         {
             // using this for messages
             ISerializable s = null;
+            Message msg = null; // multiple uses
             // empty queue
-            IEnumerable<object> queue = Enumerable.Empty<object>();
+            IEnumerable<object> emptyQueue = Enumerable.Empty<object>();
 
             // any random object (non Message) should be dropped
             object obj = null;
-            uut.ShallDrop(obj, queue).Should().Be(true);
+            uut.ShallDrop(obj, emptyQueue).Should().Be(true);
 
-            // test drop for specific commands (empty queue)
-            uut.ShallDrop(Message.Create(MessageCommand.GetAddr, s), queue).Should().Be(false);
-            uut.ShallDrop(Message.Create(MessageCommand.GetBlocks, s), queue).Should().Be(false);
-            uut.ShallDrop(Message.Create(MessageCommand.GetData, s), queue).Should().Be(false);
-            uut.ShallDrop(Message.Create(MessageCommand.GetHeaders, s), queue).Should().Be(false);
-            uut.ShallDrop(Message.Create(MessageCommand.Version, s), queue).Should().Be(false);
-            uut.ShallDrop(Message.Create(MessageCommand.Mempool, s), queue).Should().Be(false);
-            // any random command should not be dropped
-            uut.ShallDrop(Message.Create((MessageCommand)'0xff', s), queue).Should().Be(false);
+            //handshaking
+            // Version (no drop)
+            msg = Message.Create(MessageCommand.Version, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+            // Verack (no drop)
+            msg = Message.Create(MessageCommand.Verack, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+
+            //connectivity
+            // GetAddr (drop)
+            msg = Message.Create(MessageCommand.GetAddr, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(true);
+            // Addr (no drop)
+            msg = Message.Create(MessageCommand.Addr, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+            // Ping (no drop)
+            msg = Message.Create(MessageCommand.Ping, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+            // Pong (no drop)
+            msg = Message.Create(MessageCommand.Pong, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+
+            //synchronization
+            // GetHeaders (drop)
+            msg = Message.Create(MessageCommand.GetHeaders, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(true);
+            // Headers (no drop)
+            msg = Message.Create(MessageCommand.Headers, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+            // GetBlocks (drop)
+            msg = Message.Create(MessageCommand.GetBlocks, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(true);
+            // Mempool (drop)
+            msg = Message.Create(MessageCommand.Mempool, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(true);
+            // Inv (no drop)
+            msg = Message.Create(MessageCommand.Inv, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+            // GetData (drop)
+            msg = Message.Create(MessageCommand.GetData, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(true);
+            // NotFound (no drop)
+            msg = Message.Create(MessageCommand.NotFound, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+            // Transaction (no drop)
+            msg = Message.Create(MessageCommand.Transaction, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+            // Block (no drop)
+            msg = Message.Create(MessageCommand.Block, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+            // Consensus (no drop)
+            msg = Message.Create(MessageCommand.Consensus, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+            // Reject (no drop)
+            msg = Message.Create(MessageCommand.Reject, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+
+            //SPV protocol
+            // FilterLoad (no drop)
+            msg = Message.Create(MessageCommand.FilterLoad, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+            // FilterAdd (no drop)
+            msg = Message.Create(MessageCommand.FilterAdd, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+            // FilterClear (no drop)
+            msg = Message.Create(MessageCommand.FilterClear, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+            // MerkleBlock (no drop)
+            msg = Message.Create(MessageCommand.MerkleBlock, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
+
+            //others
+            // Alert (no drop)
+            msg = Message.Create(MessageCommand.Alert, s);
+            uut.ShallDrop(msg, emptyQueue).Should().Be(false);
+            uut.ShallDrop(msg, new object[]{ msg }).Should().Be(false);
         }
     }
 }
