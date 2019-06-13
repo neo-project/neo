f179678a644064fdc4e001b0201a60636dcf7f87
Igor Machado Coelho <igor.machado@gmail.com>
2019-06-12 23:44:02 -0300

    Tested all akka mailboxes (Neo 3) (#823)
    * tested all akka mailboxes
    
    * better names

--- neo.UnitTests/UT_RemoteNodeMailbox.cs
+++ neo.UnitTests/UT_RemoteNodeMailbox.cs
@@ -8,6 +8,9 @@ using Neo.Ledger;
 using Neo.Network.P2P.Payloads;
 using Neo.Network.P2P;
 using Akka.Configuration;
+using System.Net;
+using Akka.Actor;
+using Akka.IO;
 
 namespace Neo.UnitTests
 {
@@ -31,6 +34,19 @@ namespace Neo.UnitTests
             var config = TestKit.DefaultConfig;
             var akkaSettings = new Akka.Actor.Settings(system, config);
             uut = new RemoteNodeMailbox(akkaSettings, config);
+        }
+
+        [TestMethod]
+        public void RemoteNode_Test_IsHighPriority()
+        {
+            // high priority commands
+            uut.IsHighPriority(new Tcp.ConnectionClosed()).Should().Be(true);
+            uut.IsHighPriority(new Connection.Timer()).Should().Be(true);
+            uut.IsHighPriority(new Connection.Ack()).Should().Be(true);
+
+            // any random object should not have priority
+            object obj = null;
+            uut.IsHighPriority(obj).Should().Be(false);
         }
     }
 }
