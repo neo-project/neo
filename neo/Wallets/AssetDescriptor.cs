058865d0f860d1c07e4d6d38f27ea99daa4c7023
Shargon <shargon@gmail.com>
2019-06-19 19:05:13 +0200

    Restore asset descriptor

---
+++ neo/Wallets/AssetDescriptor.cs
@@ -0,0 +1,34 @@
+﻿using Neo.SmartContract;
+using Neo.VM;
+using System;
+
+namespace Neo.Wallets
+{
+    public class AssetDescriptor
+    {
+        public UInt160 AssetId;
+        public string AssetName;
+        public byte Decimals;
+
+        public AssetDescriptor(UInt160 asset_id)
+        {
+            byte[] script;
+            using (ScriptBuilder sb = new ScriptBuilder())
+            {
+                sb.EmitAppCall(asset_id, "decimals");
+                sb.EmitAppCall(asset_id, "name");
+                script = sb.ToArray();
+            }
+            ApplicationEngine engine = ApplicationEngine.Run(script);
+            if (engine.State.HasFlag(VMState.FAULT)) throw new ArgumentException();
+            this.AssetId = asset_id;
+            this.AssetName = engine.ResultStack.Pop().GetString();
+            this.Decimals = (byte)engine.ResultStack.Pop().GetBigInteger();
+        }
+
+        public override string ToString()
+        {
+            return AssetName;
+        }
+    }
+}
