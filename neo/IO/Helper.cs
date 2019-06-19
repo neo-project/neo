8389c62f83cf5854a764901e0a6f55e443d65b40
Shargon <shargon@gmail.com>
2019-06-19 16:26:28 +0200

    Remove unused code

--- neo/IO/Helper.cs
+++ neo/IO/Helper.cs
@@ -119,12 +119,6 @@ namespace Neo.IO
             }
         }
 
-        public static string ReadFixedString(this BinaryReader reader, int length)
-        {
-            byte[] data = reader.ReadBytes(length);
-            return Encoding.UTF8.GetString(data.TakeWhile(p => p != 0).ToArray());
-        }
-
         public static T ReadSerializable<T>(this BinaryReader reader) where T : ISerializable, new()
         {
             T obj = new T();
@@ -222,20 +216,6 @@ namespace Neo.IO
             for (int i = 0; i < padding; i++)
                 writer.Write((byte)0);
             writer.Write((byte)padding);
-        }
-
-        public static void WriteFixedString(this BinaryWriter writer, string value, int length)
-        {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
-            if (value.Length > length)
-                throw new ArgumentException();
-            byte[] bytes = Encoding.UTF8.GetBytes(value);
-            if (bytes.Length > length)
-                throw new ArgumentException();
-            writer.Write(bytes);
-            if (bytes.Length < length)
-                writer.Write(new byte[length - bytes.Length]);
         }
 
         public static void WriteVarBytes(this BinaryWriter writer, byte[] value)
