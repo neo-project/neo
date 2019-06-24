7ed0ff32fd1188450ee403e115930fb3af976e11
Igor Machado <igor.machado@gmail.com>
2019-06-24 16:41:23 -0300

    fixes SA1311

--- neo.UnitTests/TestWalletAccount.cs
+++ neo.UnitTests/TestWalletAccount.cs
@@ -7,17 +7,17 @@ namespace Neo.UnitTests
 {
     class TestWalletAccount : WalletAccount
     {
-        private static readonly KeyPair key;
+        private static readonly KeyPair Key;
 
         public override bool HasKey => true;
-        public override KeyPair GetKey() => key;
+        public override KeyPair GetKey() => Key;
 
         public TestWalletAccount(UInt160 hash)
             : base(hash)
         {
             var mock = new Mock<Contract>();
             mock.SetupGet(p => p.ScriptHash).Returns(hash);
-            mock.Object.Script = Contract.CreateSignatureRedeemScript(key.PublicKey);
+            mock.Object.Script = Contract.CreateSignatureRedeemScript(Key.PublicKey);
             mock.Object.ParameterList = new[] { ContractParameterType.Signature };
             Contract = mock.Object;
         }
@@ -27,7 +27,7 @@ namespace Neo.UnitTests
             Random random = new Random();
             byte[] prikey = new byte[32];
             random.NextBytes(prikey);
-            key = new KeyPair(prikey);
+            Key = new KeyPair(prikey);
         }
     }
 }
