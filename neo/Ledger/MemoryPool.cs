0e732be68bcbd417920ac3b291ffe9ee6d5be326
Shargon <shargon@gmail.com>
2019-06-25 12:03:56 +0200

    Clear 2x

--- neo/Ledger/MemoryPool.cs
+++ neo/Ledger/MemoryPool.cs
@@ -19,12 +19,12 @@ namespace Neo.Ledger
         private const int BlocksTillRebroadcastHighPriorityPoolTx = 10;
         private int RebroadcastMultiplierThreshold => Capacity / 10;
 
-        private static readonly double MaxSecondsToReverifyHighPrioTx = (double) Blockchain.SecondsPerBlock / 3;
-        private static readonly double MaxSecondsToReverifyLowPrioTx = (double) Blockchain.SecondsPerBlock / 5;
+        private static readonly double MaxSecondsToReverifyHighPrioTx = (double)Blockchain.SecondsPerBlock / 3;
+        private static readonly double MaxSecondsToReverifyLowPrioTx = (double)Blockchain.SecondsPerBlock / 5;
 
         // These two are not expected to be hit, they are just safegaurds.
-        private static readonly double MaxSecondsToReverifyHighPrioTxPerIdle = (double) Blockchain.SecondsPerBlock / 15;
-        private static readonly double MaxSecondsToReverifyLowPrioTxPerIdle = (double) Blockchain.SecondsPerBlock / 30;
+        private static readonly double MaxSecondsToReverifyHighPrioTxPerIdle = (double)Blockchain.SecondsPerBlock / 15;
+        private static readonly double MaxSecondsToReverifyLowPrioTxPerIdle = (double)Blockchain.SecondsPerBlock / 30;
 
         private readonly NeoSystem _system;
 
@@ -206,14 +206,31 @@ namespace Neo.Ledger
             }
         }
 
+        /// <summary>
+        /// Clear mem pool
+        /// </summary>
+        public void Clear()
+        {
+            _txRwLock.EnterReadLock();
+
+            _sortedHighPrioTransactions.Clear();
+            _sortedLowPrioTransactions.Clear();
+            _unsortedTransactions.Clear();
+            _unverifiedSortedHighPriorityTransactions.Clear();
+            _unverifiedSortedLowPriorityTransactions.Clear();
+            _unverifiedTransactions.Clear();
+
+            _txRwLock.ExitReadLock();
+        }
+
         public IEnumerable<Transaction> GetSortedVerifiedTransactions()
         {
             _txRwLock.EnterReadLock();
             try
             {
-               return _sortedHighPrioTransactions.Reverse().Select(p => p.Tx)
-                        .Concat(_sortedLowPrioTransactions.Reverse().Select(p => p.Tx))
-                        .ToArray();
+                return _sortedHighPrioTransactions.Reverse().Select(p => p.Tx)
+                         .Concat(_sortedLowPrioTransactions.Reverse().Select(p => p.Tx))
+                         .ToArray();
             }
             finally
             {
