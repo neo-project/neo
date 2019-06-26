c3a2c25f90628af2926a8b50374f90a6c10276c1
Vitor Nazário Coelho <vncoelho@gmail.com>
2019-06-26 14:55:12 -0300

    Simplifying ProtocolHandler constructor

--- neo/Network/P2P/ProtocolHandler.cs
+++ neo/Network/P2P/ProtocolHandler.cs
@@ -21,8 +21,9 @@ namespace Neo.Network.P2P
         public class SetFilter { public BloomFilter Filter; }
 
         private readonly NeoSystem system;
-        private readonly FIFOSet<UInt256> knownHashes;
-        private readonly FIFOSet<UInt256> sentHashes;
+        private static readonly int MaxCachedHashes = Blockchain.Singleton.MemPool.Capacity * 2;
+        private readonly FIFOSet<UInt256> knownHashes = new FIFOSet<UInt256>(MaxCachedHashes);
+        private readonly FIFOSet<UInt256> sentHashes = new FIFOSet<UInt256>(MaxCachedHashes);
         private VersionPayload version;
         private bool verack = false;
         private BloomFilter bloom_filter;
@@ -30,8 +31,6 @@ namespace Neo.Network.P2P
         public ProtocolHandler(NeoSystem system)
         {
             this.system = system;
-            this.knownHashes = new FIFOSet<UInt256>(Blockchain.Singleton.MemPool.Capacity * 2);
-            this.sentHashes = new FIFOSet<UInt256>(Blockchain.Singleton.MemPool.Capacity * 2);
         }
 
         protected override void OnReceive(object message)
