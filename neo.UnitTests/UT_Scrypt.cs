29fb4ed794d21e5f8015b6e00e158901ec2942e1
Shargon <shargon@gmail.com>
2019-07-08 09:14:44 +0200

    DeriveKey Scrypt unit test (#886)
    * Strengthen PBKDF2
    
    * Update SCrypt.cs
    
    * Scrypt DeriveKey ut
    
    * Deterministic behaviour

---
+++ neo.UnitTests/UT_Scrypt.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Cryptography;
+using System;
+
+namespace Neo.UnitTests
+{
+    [TestClass]
+    public class UT_Scrypt
+    {
+        [TestMethod]
+        public void DeriveKeyTest()
+        {
+            int N = 16384, r = 8, p = 8;
+
+            var derivedkey = SCrypt.DeriveKey(new byte[] { 0x01, 0x02, 0x03 }, new byte[] { 0x04, 0x05, 0x06 }, N, r, p, 64).ToHexString();
+            Assert.AreEqual("2bb9c7bb9c392f0dd37821b76e42b01944902520f48d00946a51e72c960fba0a3c62a87d835c9df10a8ad66a04cdf02fbb10b9d7396c20959f28d6cb3ddfdffb", derivedkey);
+        }
+    }
+}
