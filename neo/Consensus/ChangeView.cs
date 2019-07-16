788c9bf00f7f75ceb50b589e2db3e897ab52e4e2
Shargon <shargon@gmail.com>
2019-07-16 18:06:17 +0200

    Change view reason

--- neo/Consensus/ChangeView.cs
+++ neo/Consensus/ChangeView.cs
@@ -4,10 +4,25 @@ namespace Neo.Consensus
 {
     public class ChangeView : ConsensusMessage
     {
+        public enum ChangeViewReason : byte
+        {
+            Timeout = 0x0,
+            WrongExpectedView = 0x1,
+            TxNotFound = 0x2,
+            TxRejectedByPolicy = 0x3,
+            TxInvalid = 0x4,
+        }
+
         /// <summary>
         /// NewViewNumber is always set to the current ViewNumber asking changeview + 1
         /// </summary>
         public byte NewViewNumber => (byte)(ViewNumber + 1);
+
+        /// <summary>
+        /// Reason
+        /// </summary>
+        public ChangeViewReason Reason;
+
         /// <summary>
         /// Timestamp of when the ChangeView message was created. This allows receiving nodes to ensure
         /// they only respond once to a specific ChangeView request (it thus prevents replay of the ChangeView
@@ -15,20 +30,23 @@ namespace Neo.Consensus
         /// </summary>
         public uint Timestamp;
 
-        public override int Size => base.Size
-            + sizeof(uint); //Timestamp
+        public override int Size => base.Size +
+            sizeof(ChangeViewReason) +  // Reason
+            sizeof(uint);               // Timestamp
 
         public ChangeView() : base(ConsensusMessageType.ChangeView) { }
 
         public override void Deserialize(BinaryReader reader)
         {
             base.Deserialize(reader);
+            Reason = (ChangeViewReason)reader.ReadByte();
             Timestamp = reader.ReadUInt32();
         }
 
         public override void Serialize(BinaryWriter writer)
         {
             base.Serialize(writer);
+            writer.Write((byte)Reason);
             writer.Write(Timestamp);
         }
     }
