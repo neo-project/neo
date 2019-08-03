b8b8ffffb12c3751203046bb62d109f8566ca53c
Igor Machado <igor.machado@gmail.com>
2019-08-02 21:15:12 -0300

    creating PublicKey and Signature classes

---
+++ neo/Cryptography/PublicKey.cs
@@ -0,0 +1,39 @@
+using Neo.IO;
+using Neo.IO.Json;
+using System.IO;
+using Neo.Cryptography.ECC;
+
+namespace Neo.Cryptography
+{
+    public class PublicKey : ISerializable
+    {
+        // standard elliptic curve: secp256r1
+        public ECPoint Data;
+
+        public int Size => Data.Length;
+
+        void ISerializable.Deserialize(BinaryReader reader)
+        {
+            Data = reader.ReadBytes(64);
+        }
+
+        void ISerializable.Serialize(BinaryWriter writer)
+        {
+            writer.Write(Data);
+        }
+
+        public JObject ToJson()
+        {
+            JObject json = new JObject();
+            json["data"] = Data.ToHexString();
+            return json;
+        }
+
+        public static Signature FromJson(JObject json)
+        {
+            Signature signature = new Signature();
+            signature.Data = json["data"].AsString().HexToBytes();
+            return signature;
+        }
+    }
+}
