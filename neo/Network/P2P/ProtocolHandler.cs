85bf8b9a48f22f683e7687b30137a1e60ab17e4e
Vitor Nazário Coelho <vncoelho@gmail.com>
2019-08-08 20:32:30 -0300

    Shargon's suggestion II

--- neo/Network/P2P/ProtocolHandler.cs
+++ neo/Network/P2P/ProtocolHandler.cs
@@ -248,9 +248,8 @@ namespace Neo.Network.P2P
 
         private void OnInvMessageReceived(InvPayload payload)
         {
-            UInt256[] hashes = payload.Hashes.Where(p => knownHashes.Add(p)).ToArray();
-            bool sentByMe = payload.Hashes.Any(p => sentHashes.HasItem(p));
-            if (hashes.Length == 0 || sentByMe) return;
+            UInt256[] hashes = payload.Hashes.Where(p => knownHashes.Add(p) && !sentHashes.HasItem(p)).ToArray();
+            if (hashes.Length == 0) return;
             switch (payload.Type)
             {
                 case InventoryType.Block:
