601bb4790143447b1b85e81e1ebb6afa9ff1a4b8
Shargon <shargon@gmail.com>
2019-08-08 13:46:39 +0200

    Fix for 1005 (#1006)

--- neo/SmartContract/Native/Tokens/Nep5AccountState.cs
+++ neo/SmartContract/Native/Tokens/Nep5AccountState.cs
@@ -19,7 +19,7 @@ namespace Neo.SmartContract.Native.Tokens
 
         public void FromByteArray(byte[] data)
         {
-            FromStruct((Struct)data.DeserializeStackItem(16, 32));
+            FromStruct((Struct)data.DeserializeStackItem(16, 34));
         }
 
         protected virtual void FromStruct(Struct @struct)
