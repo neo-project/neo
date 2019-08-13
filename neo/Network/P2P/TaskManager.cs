b44e1eb910ce1e7b5acd8e20bda5daaad6e48599
Yongjie Ma <20391402+yongjiema@users.noreply.github.com>
2019-08-13 02:54:46 +0000

    Try to get headers from remote nodes even the current header height is higher than them if it is not updated for a while

--- neo/Network/P2P/TaskManager.cs
+++ neo/Network/P2P/TaskManager.cs
@@ -224,8 +224,8 @@ namespace Neo.Network.P2P
             }
             if ((!HasHeaderTask || globalTasks[HeaderTaskHash] < MaxConncurrentTasks)
                 && (Blockchain.Singleton.HeaderHeight < session.StartHeight
-                    || (Blockchain.Singleton.HeaderHeight == session.StartHeight
-                        && Blockchain.Singleton.HeaderHeight == sessions.Select(x => x.Value.StartHeight).Max()
+                    || (Blockchain.Singleton.Height == Blockchain.Singleton.HeaderHeight
+                        && Blockchain.Singleton.HeaderHeight >= sessions.Select(x => x.Value.StartHeight).Max()
                         && TimeProvider.Current.UtcNow.ToTimestamp() - 60 >= Blockchain.Singleton.GetBlock(Blockchain.Singleton.CurrentHeaderHash)?.Timestamp)))
             {
                 session.Tasks[HeaderTaskHash] = DateTime.UtcNow;
