d77ebce8f50507fe8bdd03cc3ed0e7aef2664f48
Erik Zhang <erik@neo.org>
2019-08-21 05:05:41 +0800

    Optimize `GetHeader()` and `GetBlock()` (#1039)

--- neo/Persistence/Helper.cs
+++ neo/Persistence/Helper.cs
@@ -20,6 +20,7 @@ namespace Neo.Persistence
 
         public static Block GetBlock(this IPersistence persistence, uint index)
         {
+            if (index == 0) return Blockchain.GenesisBlock;
             UInt256 hash = Blockchain.Singleton.GetBlockHash(index);
             if (hash == null) return null;
             return persistence.GetBlock(hash);
@@ -35,6 +36,7 @@ namespace Neo.Persistence
 
         public static Header GetHeader(this IPersistence persistence, uint index)
         {
+            if (index == 0) return Blockchain.GenesisBlock.Header;
             UInt256 hash = Blockchain.Singleton.GetBlockHash(index);
             if (hash == null) return null;
             return persistence.GetHeader(hash);
