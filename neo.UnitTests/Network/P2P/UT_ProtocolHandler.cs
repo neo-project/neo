1309fcc28d9807644aaf5ce1d5be18536372f5a9
Shargon <shargon@gmail.com>
2019-09-09 13:01:00 +0200

    Full UT

--- neo.UnitTests/Network/P2P/UT_ProtocolHandler.cs
+++ neo.UnitTests/Network/P2P/UT_ProtocolHandler.cs
@@ -6,6 +6,7 @@ using Neo.Ledger;
 using Neo.Network.P2P;
 using Neo.Network.P2P.Capabilities;
 using Neo.Network.P2P.Payloads;
+using Neo.Persistence;
 using System.Linq;
 
 namespace Neo.UnitTests.Network.P2P
@@ -13,6 +14,7 @@ namespace Neo.UnitTests.Network.P2P
     [TestClass]
     public class UT_ProtocolHandler : TestKit
     {
+        private Store testStore;
         private NeoSystem testBlockchain;
         private TestProbe senderProbe;
         private TestProbe parent;
@@ -28,6 +30,7 @@ namespace Neo.UnitTests.Network.P2P
         public void TestSetup()
         {
             testBlockchain = TestBlockchain.InitializeMockNeoSystem();
+            testStore = TestBlockchain.GetStore();
 
             senderProbe = CreateTestProbe();
             parent = CreateTestProbe();
@@ -56,27 +59,104 @@ namespace Neo.UnitTests.Network.P2P
         }
 
         [TestMethod]
-        public void Test_OnGetDataMessageReceived()
+        public void Test_OnGetDataMessageReceived_Consensus()
         {
-            var consensus = new ConsensusPayload[2]
+            var entries = new ConsensusPayload[]
             {
                 TestUtils.CreateConsensusPayload(),
                 TestUtils.CreateConsensusPayload()
             };
-            Blockchain.Singleton.ConsensusRelayCache.AddRange(consensus);
 
+            // Fill cache
+            Blockchain.Singleton.ConsensusRelayCache.AddRange(entries);
+
+            // Test
             senderProbe.Send(protocolActor, Message.Create(MessageCommand.GetData, new InvPayload()
             {
-                Hashes = consensus.Select(u => u.Hash).ToArray(),
+                Hashes = entries.Select(u => u.Hash).ToArray(),
                 Type = InventoryType.Consensus
             }));
 
             var msg = parent.ExpectMsg<Message>();
             Assert.AreEqual(MessageCommand.BulkInv, msg.Command);
             Assert.IsInstanceOfType(msg.Payload, typeof(BulkInvPayload));
+            Assert.AreEqual(InventoryType.Consensus, ((BulkInvPayload)msg.Payload).Type);
+            Assert.AreEqual(2, ((BulkInvPayload)msg.Payload).Values.Length);
 
-            // Clean singleton after the use
+            // Clean cache after the use
             Blockchain.Singleton.ConsensusRelayCache.Clear();
+        }
+
+        [TestMethod]
+        public void Test_OnGetDataMessageReceived_Transaction()
+        {
+            var entries = new Transaction[]
+            {
+                TestUtils.CreateTransaction(),
+                TestUtils.CreateTransaction()
+            };
+
+            // Fill mempool
+            foreach (var entry in entries)
+            {
+                Blockchain.Singleton.MemPool.TryAdd(entry.Hash, entry);
+            }
+
+            // Test
+            senderProbe.Send(protocolActor, Message.Create(MessageCommand.GetData, new InvPayload()
+            {
+                Hashes = entries.Select(u => u.Hash).ToArray(),
+                Type = InventoryType.TX
+            }));
+
+            var msg = parent.ExpectMsg<Message>();
+            Assert.AreEqual(MessageCommand.BulkInv, msg.Command);
+            Assert.IsInstanceOfType(msg.Payload, typeof(BulkInvPayload));
+            Assert.AreEqual(InventoryType.TX, ((BulkInvPayload)msg.Payload).Type);
+            Assert.AreEqual(2, ((BulkInvPayload)msg.Payload).Values.Length);
+
+            // Clean mempool
+            foreach (var entry in entries)
+            {
+                Blockchain.Singleton.MemPool.TryRemoveUnVerified(entry.Hash, out var item);
+            }
+        }
+
+        [TestMethod]
+        public void Test_OnGetDataMessageReceived_Block()
+        {
+            var entries = new Block[]
+            {
+                TestUtils.CreateBlock(1),
+                TestUtils.CreateBlock(2),
+                TestUtils.CreateBlock(3)
+            };
+
+            // Fake blocks
+            TestDataCache<UInt256, TrimmedBlock> blocks = (TestDataCache<UInt256, TrimmedBlock>)testStore.GetBlocks();
+            foreach (var entry in entries)
+            {
+                blocks.Add(entry.Hash, entry.Trim());
+            }
+
+            // Test
+            senderProbe.Send(protocolActor, Message.Create(MessageCommand.GetData, new InvPayload()
+            {
+                Hashes = entries.Select(u => u.Hash).ToArray(),
+                Type = InventoryType.Block
+            }));
+
+            var msg = parent.ExpectMsg<Message>();
+            Assert.AreEqual(MessageCommand.BulkInv, msg.Command);
+            Assert.IsInstanceOfType(msg.Payload, typeof(BulkInvPayload));
+            Assert.AreEqual(InventoryType.Block, ((BulkInvPayload)msg.Payload).Type);
+            Assert.AreEqual(3, ((BulkInvPayload)msg.Payload).Values.Length);
+
+            // Clean fake blocks
+            foreach (var entry in entries)
+            {
+                blocks.Delete(entry.Hash);
+            }
         }
     }
 }
