8196dff9396cd16dc50bf408595be15ff8587809
Yongjie Ma <20391402+yongjiema@users.noreply.github.com>
2019-09-29 06:46:47 +0000

    Improve syncing

--- neo/Network/P2P/TaskManager.cs
+++ neo/Network/P2P/TaskManager.cs
@@ -15,6 +15,7 @@ namespace Neo.Network.P2P
     internal class TaskManager : UntypedActor
     {
         public class Register { public VersionPayload Version; }
+        public class Update { public uint LastBlockIndex; }
         public class NewTasks { public InvPayload Payload; }
         public class TaskCompleted { public UInt256 Hash; }
         public class HeaderTaskCompleted { }
@@ -26,6 +27,7 @@ namespace Neo.Network.P2P
 
         private readonly NeoSystem system;
         private const int MaxConncurrentTasks = 3;
+        private const int PingCoolingOffPeriod = 60; // in secconds.
         private readonly FIFOSet<UInt256> knownHashes;
         private readonly Dictionary<UInt256, int> globalTasks = new Dictionary<UInt256, int>();
         private readonly Dictionary<IActorRef, TaskSession> sessions = new Dictionary<IActorRef, TaskSession>();
@@ -87,6 +89,9 @@ namespace Neo.Network.P2P
                 case Register register:
                     OnRegister(register.Version);
                     break;
+                case Update update:
+                    OnUpdate(update.LastBlockIndex);
+                    break;
                 case NewTasks tasks:
                     OnNewTasks(tasks.Payload);
                     break;
@@ -114,6 +119,13 @@ namespace Neo.Network.P2P
             TaskSession session = new TaskSession(Sender, version);
             sessions.Add(Sender, session);
             RequestTasks(session);
+        }
+
+        private void OnUpdate(uint lastBlockIndex)
+        {
+            if (!sessions.TryGetValue(Sender, out TaskSession session))
+                return;
+            session.LastBlockIndex = lastBlockIndex;
         }
 
         private void OnRestartTasks(InvPayload payload)
@@ -222,17 +234,13 @@ namespace Neo.Network.P2P
                     return;
                 }
             }
-            if ((!HasHeaderTask || globalTasks[HeaderTaskHash] < MaxConncurrentTasks)
-                && (Blockchain.Singleton.HeaderHeight < session.StartHeight
-                    || (Blockchain.Singleton.Height == Blockchain.Singleton.HeaderHeight
-                        && Blockchain.Singleton.HeaderHeight >= sessions.Select(x => x.Value.StartHeight).Max()
-                        && TimeProvider.Current.UtcNow.ToTimestampMS() - 60000 >= Blockchain.Singleton.GetBlock(Blockchain.Singleton.CurrentHeaderHash)?.Timestamp)))
+            if ((!HasHeaderTask || globalTasks[HeaderTaskHash] < MaxConncurrentTasks) && Blockchain.Singleton.HeaderHeight < session.LastBlockIndex)
             {
                 session.Tasks[HeaderTaskHash] = DateTime.UtcNow;
                 IncrementGlobalTask(HeaderTaskHash);
                 session.RemoteNode.Tell(Message.Create(MessageCommand.GetHeaders, GetBlocksPayload.Create(Blockchain.Singleton.CurrentHeaderHash)));
             }
-            else if (Blockchain.Singleton.Height < session.StartHeight)
+            else if (Blockchain.Singleton.Height < session.LastBlockIndex)
             {
                 UInt256 hash = Blockchain.Singleton.CurrentBlockHash;
                 for (uint i = Blockchain.Singleton.Height + 1; i <= Blockchain.Singleton.HeaderHeight; i++)
@@ -245,6 +253,11 @@ namespace Neo.Network.P2P
                     }
                 }
                 session.RemoteNode.Tell(Message.Create(MessageCommand.GetBlocks, GetBlocksPayload.Create(hash)));
+            }
+            else if (Blockchain.Singleton.HeaderHeight >= session.LastBlockIndex
+                    && TimeProvider.Current.UtcNow.ToTimestamp() - PingCoolingOffPeriod >= Blockchain.Singleton.GetBlock(Blockchain.Singleton.CurrentHeaderHash)?.Timestamp)
+            {
+                session.RemoteNode.Tell(Message.Create(MessageCommand.Ping, PingPayload.Create(Blockchain.Singleton.Height)));
             }
         }
     }
@@ -261,6 +274,7 @@ namespace Neo.Network.P2P
             switch (message)
             {
                 case TaskManager.Register _:
+                case TaskManager.Update _:
                 case TaskManager.RestartTasks _:
                     return true;
                 case TaskManager.NewTasks tasks:
