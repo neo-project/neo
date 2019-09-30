3a43a1f1383204d6a4f5aac327ae52322342778a
Shargon <shargon@gmail.com>
2019-09-30 21:09:41 +0200

    Clean using

--- neo/Network/P2P/LocalNode.cs
+++ neo/Network/P2P/LocalNode.cs
@@ -7,7 +7,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Sockets;
 using System.Reflection;
 using System.Threading;
 
