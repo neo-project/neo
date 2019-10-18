0ddc2c4c1fa028b064f8e76c496d2dc6c5e1ba70
Vitor Nazário Coelho <vncoelho@gmail.com>
2019-10-18 08:18:13 -0300

    removing wrong modification on protocolhandler

--- neo/Network/P2P/ProtocolHandler.cs
+++ neo/Network/P2P/ProtocolHandler.cs
@@ -27,8 +27,6 @@ namespace Neo.Network.P2P
         private bool verack = false;
         private BloomFilter bloom_filter;
 
-        private static readonly Random random = new Random();
-
         public ProtocolHandler(NeoSystem system)
         {
             this.system = system;
@@ -180,8 +178,7 @@ namespace Neo.Network.P2P
         private void OnGetDataMessageReceived(InvPayload payload)
         {
             UInt256[] hashes = payload.Hashes.Where(p => sentHashes.Add(p)).ToArray();
-            var randomHashes = hashes.OrderBy(x => random.Next()).ToArray();
-            foreach (UInt256 hash in randomHashes)
+            foreach (UInt256 hash in hashes)
             {
                 switch (payload.Type)
                 {
