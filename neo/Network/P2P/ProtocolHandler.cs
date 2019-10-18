a2199626242da2113b1f4f75610bede17176742a
Vitor Nazário Coelho <vncoelho@gmail.com>
2019-10-18 03:39:04 -0300

    Adding random hashes for OnGetDataMessageReceived

--- neo/Network/P2P/ProtocolHandler.cs
+++ neo/Network/P2P/ProtocolHandler.cs
@@ -178,7 +178,8 @@ namespace Neo.Network.P2P
         private void OnGetDataMessageReceived(InvPayload payload)
         {
             UInt256[] hashes = payload.Hashes.Where(p => sentHashes.Add(p)).ToArray();
-            foreach (UInt256 hash in hashes)
+            var randomHashes = hashes.OrderBy(x => random.Next()).ToArray();
+            foreach (UInt256 hash in randomHashes)
             {
                 switch (payload.Type)
                 {
