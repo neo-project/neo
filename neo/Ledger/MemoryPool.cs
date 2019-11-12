ef36f255b8f0e328298757638a5a587d03cbb3a1
Charis <eryeer@163.com>
2019-11-12 17:27:08 +0800

    remove numerics

--- neo/Ledger/MemoryPool.cs
+++ neo/Ledger/MemoryPool.cs
@@ -9,7 +9,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Threading;
 
