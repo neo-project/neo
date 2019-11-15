5b9646a6dc3a5aa482c57ada222938bc73f9ed3a
Shargon <shargon@gmail.com>
2019-11-15 18:40:39 +0100

    Some UT

---
+++ neo.UnitTests/Oracle/Protocols/HTTP1/UT_OracleHTTP1Request.cs
@@ -0,0 +1,42 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Oracle.Protocols.HTTP1;
+
+namespace Neo.UnitTests.Oracle.Protocols.HTTP1
+{
+    [TestClass]
+    public class UT_OracleHTTP1Request
+    {
+        [TestMethod]
+        public void TestHash()
+        {
+            var requestA = CreateDefault();
+            var requestB = CreateDefault();
+
+            requestB.Body = new byte[1];
+            Assert.AreNotEqual(requestA.Hash, requestB.Hash);
+
+            requestB = CreateDefault();
+            requestB.Filter = "X";
+            Assert.AreNotEqual(requestA.Hash, requestB.Hash);
+
+            requestB = CreateDefault();
+            requestB.URL = "X";
+            Assert.AreNotEqual(requestA.Hash, requestB.Hash);
+
+            requestB = CreateDefault();
+            requestB.Method = OracleHTTP1Method.GET;
+            Assert.AreNotEqual(requestA.Hash, requestB.Hash);
+        }
+
+        private OracleHTTP1Request CreateDefault()
+        {
+            return new OracleHTTP1Request()
+            {
+                Body = new byte[0],
+                Filter = "",
+                Method = OracleHTTP1Method.DELETE,
+                URL = ""
+            };
+        }
+    }
+}
---
+++ neo.UnitTests/Oracle/UT_OracleResultsCache.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Oracle;
+using Neo.Oracle.Protocols.HTTP1;
+
+namespace Neo.UnitTests.Oracle
+{
+    [TestClass]
+    public class UT_OracleResultsCache
+    {
+        class CounterRequest : OracleHTTP1Request
+        {
+            public int Counter = 0;
+        }
+
+        UInt256 _txHash;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var rand = new Random();
+            var data = new byte[32];
+            rand.NextBytes(data);
+
+            _txHash = new UInt256(data);
+        }
+
+        [TestMethod]
+        public void TestWithOracle()
+        {
+            var cache = new OracleResultsCache(OracleLogic);
+
+            Assert.AreEqual(0, cache.Count);
+            Assert.IsFalse(cache.GetEnumerator().MoveNext());
+
+            // Test without cache
+
+            var req = new CounterRequest()
+            {
+                Counter = 0,
+                URL = "URL",
+                Filter = "Filter",
+                Method = OracleHTTP1Method.GET,
+                Body = new byte[] { 0x01 }
+            };
+            Assert.IsTrue(cache.TryGet(req, out var ret));
+
+            Assert.AreEqual(1, req.Counter);
+            Assert.AreEqual(1, cache.Count);
+            Assert.AreEqual(OracleResultError.None, ret.Error);
+            CollectionAssert.AreEqual(new byte[] { 0x01 }, ret.Result);
+
+            // Test cached
+
+            Assert.IsTrue(cache.TryGet(req, out ret));
+
+            Assert.AreEqual(1, req.Counter);
+            Assert.AreEqual(1, cache.Count);
+            Assert.AreEqual(OracleResultError.None, ret.Error);
+            CollectionAssert.AreEqual(new byte[] { 0x01 }, ret.Result);
+
+            // Check collection
+
+            var array = cache.ToArray();
+            Assert.AreEqual(1, array.Length);
+            Assert.AreEqual(req.Hash, array[0].Key);
+            Assert.AreEqual(_txHash, array[0].Value.TransactionHash);
+            Assert.AreEqual(OracleResultError.None, array[0].Value.Error);
+            CollectionAssert.AreEqual(new byte[] { 0x01 }, array[0].Value.Result);
+        }
+
+        private OracleResult OracleLogic(OracleRequest arg)
+        {
+            var http = (CounterRequest)arg;
+            http.Counter++;
+            return OracleResult.CreateResult(_txHash, arg.Hash, http.Body);
+        }
+
+        [TestMethod]
+        public void TestWithoutOracle()
+        {
+            var initReq = new OracleHTTP1Request()
+            {
+                URL = "URL",
+                Filter = "Filter",
+                Method = OracleHTTP1Method.GET,
+                Body = new byte[] { 0x01 }
+            };
+
+            var initRes = OracleResult.CreateError(_txHash, initReq.Hash, OracleResultError.ServerError);
+            var cache = new OracleResultsCache(initRes);
+
+            Assert.AreEqual(1, cache.Count);
+
+            // Check collection
+
+            var array = cache.ToArray();
+            Assert.AreEqual(1, array.Length);
+            Assert.AreEqual(initReq.Hash, array[0].Key);
+            Assert.AreEqual(_txHash, array[0].Value.TransactionHash);
+            Assert.AreEqual(OracleResultError.ServerError, array[0].Value.Error);
+            CollectionAssert.AreEqual(new byte[] { }, array[0].Value.Result);
+
+            // Test without cache
+
+            Assert.IsFalse(cache.TryGet(new OracleHTTP1Request()
+            {
+                URL = "URL",
+                Filter = "Filter",
+                Method = OracleHTTP1Method.POST,
+                Body = new byte[] { 0x01 }
+            }, out var ret));
+
+            Assert.IsNull(ret);
+
+            // Test cached
+
+            Assert.IsTrue(cache.TryGet(initReq, out ret));
+            Assert.IsNotNull(ret);
+            Assert.AreEqual(1, cache.Count);
+            Assert.IsTrue(ReferenceEquals(ret, initRes));
+        }
+    }
+}
