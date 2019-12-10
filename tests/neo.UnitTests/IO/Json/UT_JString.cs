5e1333b215518f48f1387abe288e3dd051899d3a
Erik Zhang <erik@neo.org>
2019-12-10 14:55:18 +0800

    Improve JSON (#1315)

--- tests/neo.UnitTests/IO/Json/UT_JString.cs
+++ tests/neo.UnitTests/IO/Json/UT_JString.cs
@@ -2,7 +2,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.IO.Json;
 using Neo.SmartContract;
 using System;
-using System.IO;
 
 namespace Neo.UnitTests.IO
 {
@@ -12,7 +11,7 @@ namespace Neo.UnitTests.IO
         [TestMethod]
         public void TestConstructor()
         {
-            String s = "hello world";
+            string s = "hello world";
             JString jstring = new JString(s);
             Assert.AreEqual(s, jstring.Value);
             Assert.ThrowsException<ArgumentNullException>(() => new JString(null));
@@ -21,8 +20,8 @@ namespace Neo.UnitTests.IO
         [TestMethod]
         public void TestAsBoolean()
         {
-            String s1 = "hello world";
-            String s2 = "";
+            string s1 = "hello world";
+            string s2 = "";
             JString jstring1 = new JString(s1);
             JString jstring2 = new JString(s2);
             Assert.AreEqual(true, jstring1.AsBoolean());
@@ -30,32 +29,17 @@ namespace Neo.UnitTests.IO
         }
 
         [TestMethod]
-        public void TestParse()
+        public void TestAsNumber()
         {
-            TextReader tr = new StringReader("\"hello world\"");
-            String s = JString.Parse(tr).Value;
-            Assert.AreEqual("hello world", s);
-
-            tr = new StringReader("hello world");
-            Assert.ThrowsException<FormatException>(() => JString.Parse(tr));
-
-            tr = new StringReader("\"\\s\"");
-            Assert.ThrowsException<FormatException>(() => JString.Parse(tr));
-
-            tr = new StringReader("\"\\\"\\\\\\/\\b\\f\\n\\r\\t\"");
-            s = JString.Parse(tr).Value;
-            Assert.AreEqual("\"\\/\b\f\n\r\t", s);
-
-            tr = new StringReader("\"\\u0030\"");
-            s = JString.Parse(tr).Value;
-            Assert.AreEqual("0", s);
-
-            tr = new StringReader("\"a");
-            Assert.ThrowsException<FormatException>(() => JString.Parse(tr));
-
-            byte[] byteArray = new byte[] { 0x22, 0x01, 0x22 };
-            tr = new StringReader(System.Text.Encoding.ASCII.GetString(byteArray));
-            Assert.ThrowsException<FormatException>(() => JString.Parse(tr));
+            string s1 = "hello world";
+            string s2 = "123";
+            string s3 = "";
+            JString jstring1 = new JString(s1);
+            JString jstring2 = new JString(s2);
+            JString jstring3 = new JString(s3);
+            Assert.AreEqual(double.NaN, jstring1.AsNumber());
+            Assert.AreEqual(123, jstring2.AsNumber());
+            Assert.AreEqual(0, jstring3.AsNumber());
         }
 
         [TestMethod]
