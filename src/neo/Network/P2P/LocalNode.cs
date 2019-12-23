7a5e54cd3d433def2c9d33b4f419556ed826ed2e
ShawnYun <yangxiaoyun@neo.org>
2019-12-23 16:45:09 +0800

    add relay-block-filter

--- src/neo/Network/P2P/LocalNode.cs
+++ src/neo/Network/P2P/LocalNode.cs
@@ -155,7 +155,7 @@ namespace Neo.Network.P2P
             else
             {
                 // Will call AddPeers with default SeedList set cached on <see cref="ProtocolSettings"/>.
-                // It will try to add those, sequentially, to the list of currently uncconected ones.
+                // It will try to add those, sequentially, to the list of currently unconnected ones.
 
                 Random rand = new Random();
                 AddPeers(SeedList.Where(u => u != null).OrderBy(p => rand.Next()).Take(count));
@@ -197,7 +197,24 @@ namespace Neo.Network.P2P
             system.Blockchain.Tell(inventory);
         }
 
-        private void OnRelayDirectly(IInventory inventory) => SendToRemoteNodes(new RemoteNode.Relay { Inventory = inventory });
+        private void OnRelayDirectly(IInventory inventory)
+        {
+            var message = new RemoteNode.Relay { Inventory = inventory };
+
+            // When relaying a block, if the block's index is higher than 'LastBlockIndex' of the RemoteNode, send the block;
+            // otherwise, don't send.
+            if (inventory is Block block)
+            {
+                foreach (KeyValuePair<IActorRef, RemoteNode> kvp in RemoteNodes)
+                {
+                    if (block.Index > kvp.Value.LastBlockIndex)
+                        kvp.Key.Tell(message);
+                }
+            }
+            else
+                SendToRemoteNodes(message);
+        }
+           
 
         private void OnSendDirectly(IInventory inventory) => SendToRemoteNodes(inventory);
 
