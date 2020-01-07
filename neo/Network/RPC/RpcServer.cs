ab43e2ed4764f590a4e54564afd0e7354a78c3fb
Shargon <Shargon@gmail.com>
2020-01-07 11:18:05 +0100

    Allow to set the max by config

--- neo/Network/RPC/RpcServer.cs
+++ neo/Network/RPC/RpcServer.cs
@@ -32,15 +32,17 @@ namespace Neo.Network.RPC
     {
         public Wallet Wallet { get; set; }
         public Fixed8 MaxGasInvoke { get; }
+        public int MaxConcurrentConnections { get; }
 
         private IWebHost host;
         private readonly NeoSystem system;
 
-        public RpcServer(NeoSystem system, Wallet wallet = null, Fixed8 maxGasInvoke = default(Fixed8))
+        public RpcServer(NeoSystem system, Wallet wallet = null, Fixed8 maxGasInvoke = default(Fixed8), int maxConcurrentConnections = Peer.DefaultMaxConnections)
         {
             this.system = system;
             this.Wallet = wallet;
             this.MaxGasInvoke = maxGasInvoke;
+            this.MaxConcurrentConnections = maxConcurrentConnections;
         }
 
         private static JObject CreateErrorResponse(JObject id, int code, string message, JObject data = null)
@@ -362,7 +364,7 @@ namespace Neo.Network.RPC
             host = new WebHostBuilder().UseKestrel(options => options.Listen(bindAddress, port, listenOptions =>
             {
                 // Default value is unlimited
-                options.Limits.MaxConcurrentConnections = Peer.DefaultMaxConnections;
+                options.Limits.MaxConcurrentConnections = MaxConcurrentConnections;
                 // Default value is 2 minutes
                 options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(1);
                 // Default value is 30 seconds
