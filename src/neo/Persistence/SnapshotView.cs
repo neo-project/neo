fe9f85cf174147fdedb4a3b300585f21b95d335b
erikzhang <erik@neo.org>
2020-01-17 22:19:25 +0800

    Rename

--- src/neo/Persistence/SnapshotView.cs
+++ src/neo/Persistence/SnapshotView.cs
@@ -31,7 +31,7 @@ namespace Neo.Persistence
             HeaderHashList = new StoreDataCache<SerializableWrapper<uint>, HeaderHashList>(snapshot, Prefixes.IX_HeaderHashList);
             BlockHashIndex = new StoreMetaDataCache<HashIndexState>(snapshot, Prefixes.IX_CurrentBlock);
             HeaderHashIndex = new StoreMetaDataCache<HashIndexState>(snapshot, Prefixes.IX_CurrentHeader);
-            ContractId = new StoreMetaDataCache<ContractIdState>(snapshot, Prefixes.IX_CurrentContractId);
+            ContractId = new StoreMetaDataCache<ContractIdState>(snapshot, Prefixes.IX_ContractId);
         }
 
         public override void Commit()
