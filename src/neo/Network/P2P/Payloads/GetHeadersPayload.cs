c54eb7e851129f524f86866fce16dd2bf7e4be9f
Shawn <yangxiaoyun@neo.org>
2020-02-17 15:20:27 +0800

    modify get headers

---
+++ src/neo/Network/P2P/Payloads/GetHeadersPayload.cs
@@ -0,0 +1,36 @@
+using Neo.IO;
+using System;
+using System.IO;
+
+namespace Neo.Network.P2P.Payloads
+{
+    public class GetHeadersPayload : ISerializable
+    {
+        public uint IndexStart;
+        public short Count;
+
+        public int Size => sizeof(uint) + sizeof(short);
+
+        public static GetHeadersPayload Create(uint index_start, short count = -1)
+        {
+            return new GetHeadersPayload
+            {
+                IndexStart = index_start,
+                Count = count
+            };
+        }
+
+        void ISerializable.Deserialize(BinaryReader reader)
+        {
+            IndexStart = reader.ReadUInt32();
+            Count = reader.ReadInt16();
+            if (Count < -1 || Count == 0) throw new FormatException();
+        }
+
+        void ISerializable.Serialize(BinaryWriter writer)
+        {
+            writer.Write(IndexStart);
+            writer.Write(Count);
+        }
+    }
+}
