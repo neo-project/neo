7c55b1283dc263f98de54c4dee817557c3e2bbcb
KickSeason <ztgoforit@163.com>
2020-03-23 16:55:24 +0800

    combine key and proof

--- neo/Network/RPC/RpcServer.cs
+++ neo/Network/RPC/RpcServer.cs
@@ -325,14 +325,8 @@ namespace Neo.Network.RPC
                 case "verifyproof":
                     {
                         UInt256 state_root = UInt256.Parse(_params[0].AsString());
-                        UInt160 script_hash = UInt160.Parse(_params[1].AsString());
-                        byte[] key = _params[2].AsString().HexToBytes();
-                        byte[] proof_bytes = _params[3].AsString().HexToBytes();
-                        var skey = new StorageKey{
-                            ScriptHash = script_hash,
-                            Key = key,
-                        };
-                        return VerifyProof(state_root, skey, proof_bytes);
+                        byte[] proof_bytes = _params[1].AsString().HexToBytes();
+                        return VerifyProof(state_root, proof_bytes);
                     }
                 default:
                     throw new RpcException(-32601, "Method not found");
@@ -818,6 +812,7 @@ namespace Neo.Network.RPC
             using (MemoryStream ms = new MemoryStream())
             using (BinaryWriter writer = new BinaryWriter(ms, Encoding.UTF8))
             {
+                writer.WriteVarBytes(skey.ToArray());
                 writer.WriteVarInt(proof.Count);
                 foreach (var item in proof)
                 {
@@ -830,13 +825,16 @@ namespace Neo.Network.RPC
             return json;
         }
 
-        private JObject VerifyProof(UInt256 state_root, StorageKey skey, byte[] proof_bytes)
+        private JObject VerifyProof(UInt256 state_root, byte[] proof_bytes)
         {
             var json = new JObject();
             var proof = new HashSet<byte[]>(ByteArrayEqualityComparer.Default);
+            StorageKey skey;
             using (MemoryStream ms = new MemoryStream(proof_bytes, false))
             using (BinaryReader reader = new BinaryReader(ms, Encoding.UTF8))
             {
+                var key = reader.ReadVarBytes();
+                skey = key.AsSerializable<StorageKey>();
                 var count = reader.ReadVarInt();
                 for (ulong i = 0; i < count; i++)
                 {
