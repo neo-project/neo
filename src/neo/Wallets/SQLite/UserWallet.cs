6eba2ee12d12b70d27181ff88f4ebf81550fe3ca
Qiao Jin <43407364+Qiao-Jin@users.noreply.github.com>
2020-03-23 14:39:48 +0800

    Fix change password (#1490)

--- src/neo/Wallets/SQLite/UserWallet.cs
+++ src/neo/Wallets/SQLite/UserWallet.cs
@@ -174,7 +174,7 @@ namespace Neo.Wallets.SQLite
             byte[] passwordKey = password_new.ToAesKey();
             try
             {
-                SaveStoredData("PasswordHash", passwordKey.Sha256());
+                SaveStoredData("PasswordHash", passwordKey.Concat(salt).ToArray().Sha256());
                 SaveStoredData("MasterKey", masterKey.AesEncrypt(passwordKey, iv));
                 return true;
             }
