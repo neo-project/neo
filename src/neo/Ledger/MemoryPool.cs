d60bd9515a6032d9a724853a839af9ab69f07527
Shargon <shargon@gmail.com>
2020-03-29 20:59:32 +0200

    Optimize

--- src/neo/Ledger/MemoryPool.cs
+++ src/neo/Ledger/MemoryPool.cs
@@ -27,7 +27,6 @@ namespace Neo.Ledger
 
         private readonly NeoSystem _system;
 
-        //
         /// <summary>
         /// Guarantees consistency of the pool data structures.
         ///
