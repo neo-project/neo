a38b996b49ffb0cf860032ca8a7cce410c17fea5
erikzhang <erik@neo.org>
2020-04-10 18:04:39 +0800

    Update CallFlags.cs

--- src/neo/SmartContract/CallFlags.cs
+++ src/neo/SmartContract/CallFlags.cs
@@ -8,14 +8,11 @@ namespace Neo.SmartContract
         None = 0,
 
         AllowStates = 0b00000001,
-        /// <summary>
-        /// AllowModifyStates include AllowStates
-        /// </summary>
         AllowModifyStates = 0b00000010,
         AllowCall = 0b00000100,
         AllowNotify = 0b00001000,
 
-        ReadOnly = AllowCall | AllowNotify | AllowStates,
+        ReadOnly = AllowStates | AllowCall | AllowNotify,
         All = AllowStates | AllowModifyStates | AllowCall | AllowNotify
     }
 }
