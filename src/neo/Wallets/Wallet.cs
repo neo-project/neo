827837e0d38948c47ae864487f298235f3b234d2
Shargon <shargon@gmail.com>
2020-04-18 08:38:19 +0200

    Fix comment

--- src/neo/Wallets/Wallet.cs
+++ src/neo/Wallets/Wallet.cs
@@ -340,7 +340,7 @@ namespace Neo.Wallets
                     if (engine.State.HasFlag(VMState.FAULT))
                         throw new InvalidOperationException($"Failed execution for '{script.ToHexString()}'");
                     tx.SystemFee = Math.Max(engine.GasConsumed - ApplicationEngine.GasFree, 0) +
-                        (oracleCache.Size * NativeContract.Policy.GetFeePerByte(snapshot)); // We should pay for the response extra cost of
+                        (oracleCache.Size * NativeContract.Policy.GetFeePerByte(snapshot)); // We should pay for the response extra cost
 
                     // Change the Transaction type because it's an oracle request
 
