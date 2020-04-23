dc6afc0760e34601398addad4dde50946673bec8
晨 黎 <lichen@neo.org>
2020-04-23 11:36:02 +0800

    Add KeyRecover function Unit Test

---
+++ neo.UnitTests/UT_ECDsa.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Cryptography.ECC;
+using Neo.Wallets;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Numerics;
+
+namespace Neo.Cryptography.ECC.Tests
+{
+    [TestClass()]
+    public class UT_ECDsa
+    {
+        private ECDsa ecdsa;
+
+
+        public static byte[] generatekey(int privateKeyLength) 
+        {
+            byte[] privateKey = new byte[privateKeyLength];
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create()) 
+            {
+                rng.GetBytes(privateKey);
+            }
+            return privateKey;
+        }
+
+        [TestMethod()]
+        public void KeyRecoverTest()
+        {
+            byte[] privateKey = generatekey(32);
+            ECPoint publickey= ECCurve.Secp256k1.G * privateKey;
+            ecdsa = new ECDsa(privateKey, ECCurve.Secp256k1);
+            byte[] message = System.Text.Encoding.Default.GetBytes("HelloWorld");
+            BigInteger[] signatures = ecdsa.GenerateSignature(message);
+
+            ECPoint recoverKey = ECDsa.KeyRecover(ECCurve.Secp256k1, signatures[0], signatures[1], message, publickey.Y.Value.IsEven, true);
+            Assert.IsTrue(recoverKey.Equals(publickey));
+
+            //wrong key part
+            var r = new System.Numerics.BigInteger(generatekey(32));
+            recoverKey = ECDsa.KeyRecover(ECCurve.Secp256k1, r, signatures[1], message, publickey.Y.Value.IsEven, true);
+            Assert.IsFalse(recoverKey.Equals(publickey));
+
+            var s = new System.Numerics.BigInteger(generatekey(32));
+            recoverKey = ECDsa.KeyRecover(ECCurve.Secp256k1, signatures[0], s, message, publickey.Y.Value.IsEven, true);
+            Assert.IsFalse(recoverKey.Equals(publickey));
+        }
+    }
+}
