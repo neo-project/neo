caa680dc7d81c7b9ea44162de67870cb6183f7fd
Jin Qiao <jinqiao@neo.org>
2020-04-28 10:43:10 +0800

    Revert "Remove ProtocolSettings Initialize"
    This reverts commit 58dda0b23e09a9b8a03ac9528fc6eb5908c7bf9e.

--- src/neo/ProtocolSettings.cs
+++ src/neo/ProtocolSettings.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Linq;
+using System.Threading;
 
 namespace Neo
 {
@@ -14,7 +15,32 @@ namespace Neo
         public uint MillisecondsPerBlock { get; }
         public int MemoryPoolMaxTransactions { get; }
 
-        public static readonly ProtocolSettings Default = new ProtocolSettings(Utility.LoadConfig("protocol").GetSection("ProtocolConfiguration"));
+        static ProtocolSettings _default;
+
+        static bool UpdateDefault(IConfiguration configuration)
+        {
+            var settings = new ProtocolSettings(configuration.GetSection("ProtocolConfiguration"));
+            return null == Interlocked.CompareExchange(ref _default, settings, null);
+        }
+
+        public static bool Initialize(IConfiguration configuration)
+        {
+            return UpdateDefault(configuration);
+        }
+
+        public static ProtocolSettings Default
+        {
+            get
+            {
+                if (_default == null)
+                {
+                    var configuration = Utility.LoadConfig("protocol");
+                    UpdateDefault(configuration);
+                }
+
+                return _default;
+            }
+        }
 
         private ProtocolSettings(IConfigurationSection section)
         {
--- tests/neo.UnitTests/UT_ProtocolSettings.cs
+++ tests/neo.UnitTests/UT_ProtocolSettings.cs
@@ -1,12 +1,36 @@
 using FluentAssertions;
+using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.Wallets;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Neo.UnitTests
 {
     [TestClass]
     public class UT_ProtocolSettings
     {
+        // since ProtocolSettings.Default is designed to be writable only once, use reflection to 
+        // reset the underlying _default field to null before and after running tests in this class.
+        static void ResetProtocolSettings()
+        {
+            var defaultField = typeof(ProtocolSettings)
+                .GetField("_default", BindingFlags.Static | BindingFlags.NonPublic);
+            defaultField.SetValue(null, null);
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            ResetProtocolSettings();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            ResetProtocolSettings();
+        }
+
         [TestMethod]
         public void CheckFirstLetterOfAddresses()
         {
@@ -21,6 +45,60 @@ namespace Neo.UnitTests
         {
             var mainNetMagic = 0x4F454Eu;
             ProtocolSettings.Default.Magic.Should().Be(mainNetMagic);
+        }
+
+        [TestMethod]
+        public void Can_initialize_ProtocolSettings()
+        {
+            var expectedMagic = 12345u;
+
+            var dict = new Dictionary<string, string>()
+            {
+                { "ProtocolConfiguration:Magic", $"{expectedMagic}" }
+            };
+
+            var config = new ConfigurationBuilder().AddInMemoryCollection(dict).Build();
+            ProtocolSettings.Initialize(config).Should().BeTrue();
+            ProtocolSettings.Default.Magic.Should().Be(expectedMagic);
+        }
+
+        [TestMethod]
+        public void Cant_initialize_ProtocolSettings_after_default_settings_used()
+        {
+            var mainNetMagic = 0x4F454Eu;
+            ProtocolSettings.Default.Magic.Should().Be(mainNetMagic);
+
+            var updatedMagic = 54321u;
+            var dict = new Dictionary<string, string>()
+            {
+                { "ProtocolConfiguration:Magic", $"{updatedMagic}" }
+            };
+
+            var config = new ConfigurationBuilder().AddInMemoryCollection(dict).Build();
+            ProtocolSettings.Initialize(config).Should().BeFalse();
+            ProtocolSettings.Default.Magic.Should().Be(mainNetMagic);
+        }
+
+        [TestMethod]
+        public void Cant_initialize_ProtocolSettings_twice()
+        {
+            var expectedMagic = 12345u;
+            var dict = new Dictionary<string, string>()
+            {
+                { "ProtocolConfiguration:Magic", $"{expectedMagic}" }
+            };
+
+            var config = new ConfigurationBuilder().AddInMemoryCollection(dict).Build();
+            ProtocolSettings.Initialize(config).Should().BeTrue();
+
+            var updatedMagic = 54321u;
+            dict = new Dictionary<string, string>()
+            {
+                { "ProtocolConfiguration:Magic", $"{updatedMagic}" }
+            };
+            config = new ConfigurationBuilder().AddInMemoryCollection(dict).Build();
+            ProtocolSettings.Initialize(config).Should().BeFalse();
+            ProtocolSettings.Default.Magic.Should().Be(expectedMagic);
         }
 
         [TestMethod]
