cce60d5fcad490b60042609baaf808745508f494
Jin Qiao <jinqiao@neo.org>
2020-04-29 10:31:47 +0800

    Add TODO comment to warn future delete case

--- src/neo/Ledger/Blockchain.cs
+++ src/neo/Ledger/Blockchain.cs
@@ -489,6 +489,7 @@ namespace Neo.Ledger
                 }
                 snapshot.Blocks.Add(block.Hash, block.Trim());
                 StoreView clonedSnapshot = snapshot.Clone();
+                // TODO: Handle occassions where Datacache.Delete() is called by variable snapshot
                 foreach (Transaction tx in block.Transactions)
                 {
                     var state = new TransactionState
