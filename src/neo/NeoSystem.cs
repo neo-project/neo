3cec48ba91dfbe777bfcdeaf5a3d658970f54519
Shargon <shargon@gmail.com>
2020-05-01 18:32:24 +0200

    Unify akka logs with neo logs (#1623)
    * Unify akka logs
    
    * Add event
    
    * dotnet format
    
    * Remove event
    
    * Remove overload
    
    * Clean changes
    
    * Fix compilation error
    
    * Simplify Logger
    
    Co-authored-by: erikzhang <erik@neo.org>

--- src/neo/NeoSystem.cs
+++ src/neo/NeoSystem.cs
@@ -12,7 +12,7 @@ namespace Neo
     public class NeoSystem : IDisposable
     {
         public ActorSystem ActorSystem { get; } = ActorSystem.Create(nameof(NeoSystem),
-            $"akka {{ log-dead-letters = off }}" +
+            $"akka {{ log-dead-letters = off , loglevel = warning, loggers = [ \"{typeof(Utility.Logger).AssemblyQualifiedName}\" ] }}" +
             $"blockchain-mailbox {{ mailbox-type: \"{typeof(BlockchainMailbox).AssemblyQualifiedName}\" }}" +
             $"task-manager-mailbox {{ mailbox-type: \"{typeof(TaskManagerMailbox).AssemblyQualifiedName}\" }}" +
             $"remote-node-mailbox {{ mailbox-type: \"{typeof(RemoteNodeMailbox).AssemblyQualifiedName}\" }}" +
