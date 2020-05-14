015e591892e98e48334e902f6bcd92d2f7bbce11
Shargon <shargon@gmail.com>
2020-05-14 15:38:43 +0200

    Clean code

--- src/neo/SmartContract/Native/NativeContract.cs
+++ src/neo/SmartContract/Native/NativeContract.cs
@@ -11,7 +11,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Security;
 using Array = Neo.VM.Types.Array;
 
 namespace Neo.SmartContract.Native
