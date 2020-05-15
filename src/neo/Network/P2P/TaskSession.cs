acd11c85d402da1706b8f48879c8f36bcf5cc1f2
Shawn <yangxiaoyun@neo.org>
2020-05-15 17:53:57 +0800

    del not used

--- src/neo/Network/P2P/TaskSession.cs
+++ src/neo/Network/P2P/TaskSession.cs
@@ -12,9 +12,6 @@ namespace Neo.Network.P2P
         public uint InvalidBlockCount = 0;
         public uint LastBlockIndex = 0;
 
-        public bool HasInvTask => InvTasks.Count > 0;
-        public bool HasIndexTask => IndexTasks.Count > 0;
-
         public TaskSession(uint lastBlockIndex)
         {
             this.LastBlockIndex = lastBlockIndex;
