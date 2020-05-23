c4bed045cb9e9b97491e823096a6df41b9b4e544
Erik Zhang <erik@neo.org>
2020-05-23 20:52:13 +0800

    Update OracleRequestAttribute.cs

--- src/neo/Network/P2P/Payloads/OracleRequestAttribute.cs
+++ src/neo/Network/P2P/Payloads/OracleRequestAttribute.cs
@@ -4,7 +4,6 @@ namespace Neo.Network.P2P.Payloads
 {
     public class OracleRequestAttribute : TransactionAttribute
     {
-        public override int Size => base.Size;
         public override TransactionAttributeType Type => TransactionAttributeType.OracleRequest;
 
         protected override void DeserializeWithoutType(BinaryReader reader) { }
