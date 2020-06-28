2e6fa2a05979f95c488dcac2b4c120cbde4d3512
KickSeason <ztgoforit@163.com>
2020-06-28 10:14:21 +0800

    fix size

--- neo/Consensus/PrepareResponse.cs
+++ neo/Consensus/PrepareResponse.cs
@@ -8,7 +8,7 @@ namespace Neo.Consensus
         public UInt256 PreparationHash;
         public byte[] StateRootSignature;
 
-        public override int Size => base.Size + PreparationHash.Size;
+        public override int Size => base.Size + PreparationHash.Size + StateRootSignature.Length;
 
         public PrepareResponse()
             : base(ConsensusMessageType.PrepareResponse)
