fa83632dc379b5385f6a045fe5050ebf831a5797
erikzhang <erik@neo.org>
2020-07-01 15:25:58 +0800

    Update ByteArrayComparer.cs

--- src/neo/IO/ByteArrayComparer.cs
+++ src/neo/IO/ByteArrayComparer.cs
@@ -8,22 +8,26 @@ namespace Neo.IO
         public static readonly ByteArrayComparer Default = new ByteArrayComparer(1);
         public static readonly ByteArrayComparer Reverse = new ByteArrayComparer(-1);
 
-        private int Direction;
+        private readonly int direction;
 
         private ByteArrayComparer(int direction)
         {
-            Direction = direction;
+            this.direction = direction;
         }
 
         public int Compare(byte[] x, byte[] y)
         {
+            int r;
             int length = Math.Min(x.Length, y.Length);
             for (int i = 0; i < length; i++)
             {
-                int r = x[i].CompareTo(y[i]);
-                if (r != 0) return r * Direction;
+                r = x[i].CompareTo(y[i]);
+                if (direction == -1) r = -r;
+                if (r != 0) return r;
             }
-            return x.Length.CompareTo(y.Length) * Direction;
+            r = x.Length.CompareTo(y.Length);
+            if (direction == -1) r = -r;
+            return r;
         }
     }
 }
