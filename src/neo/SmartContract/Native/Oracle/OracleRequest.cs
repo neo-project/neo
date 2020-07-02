c4214a585bc203a0f255071c839db4bc3cf6dbfa
doubiliu <liumengyu@ngd.neo.org>
2020-07-02 11:00:59 +0800

    little change

--- src/neo/SmartContract/Native/Oracle/OracleRequest.cs
+++ src/neo/SmartContract/Native/Oracle/OracleRequest.cs
@@ -8,24 +8,24 @@ namespace Neo.SmartContract.Native.Tokens
     public class OracleRequest : IInteroperable
     {
         public UInt256 RequestTxHash;
+        public string Url;
         public string FilterPath;
         public UInt160 CallbackContract;
         public string CallbackMethod;
         public uint ValidHeight;
         public long OracleFee;
-        public string Url;
         public RequestStatusType Status;
 
         public virtual void FromStackItem(StackItem stackItem)
         {
             Struct @struct = (Struct)stackItem;
             RequestTxHash = @struct[0].GetSpan().AsSerializable<UInt256>();
-            FilterPath = @struct[1].GetString();
-            CallbackContract = @struct[2].GetSpan().AsSerializable<UInt160>();
-            CallbackMethod = @struct[3].GetString();
-            ValidHeight = (uint)@struct[4].GetInteger();
-            OracleFee = (long)@struct[5].GetInteger();
-            Url = ((Struct)stackItem)[6].GetString();
+            Url = ((Struct)stackItem)[1].GetString();
+            FilterPath = @struct[2].GetString();
+            CallbackContract = @struct[3].GetSpan().AsSerializable<UInt160>();
+            CallbackMethod = @struct[4].GetString();
+            ValidHeight = (uint)@struct[5].GetInteger();
+            OracleFee = (long)@struct[6].GetInteger();
             Status = (RequestStatusType)@struct[7].GetSpan().ToArray()[0];
         }
 
@@ -34,12 +34,12 @@ namespace Neo.SmartContract.Native.Tokens
             return new Struct(referenceCounter)
             {
                 RequestTxHash.ToArray(),
+                Url,
                 FilterPath,
                 CallbackContract.ToArray(),
                 CallbackMethod,
                 ValidHeight,
                 OracleFee,
-                Url,
                 new byte[]{ (byte)Status }
             };
         }
