10c71e146d1504e6ebcb67d93952a121a1b22d72
erikzhang <erik@neo.org>
2020-07-07 13:34:47 +0800

    Fix

--- src/neo/SmartContract/NefFile.cs
+++ src/neo/SmartContract/NefFile.cs
@@ -117,7 +117,7 @@ namespace Neo.SmartContract
         {
             Span<byte> header = stackalloc byte[HeaderSize];
             fixed (byte* p = header)
-                using (UnmanagedMemoryStream ms = new UnmanagedMemoryStream(p, HeaderSize))
+                using (UnmanagedMemoryStream ms = new UnmanagedMemoryStream(p, HeaderSize, HeaderSize, FileAccess.Write))
                 using (BinaryWriter wr = new BinaryWriter(ms, Utility.StrictUTF8, false))
                 {
                     file.SerializeHeader(wr);
