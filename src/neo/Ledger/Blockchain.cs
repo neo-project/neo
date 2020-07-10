a2ae4a256e75a168d3d14610a9aca96529e627e3
Shargon <Shargon@gmail.com>
2020-07-10 13:44:19 +0200

    Explicit FeeOnly for DeployNativeContracts

--- src/neo/Ledger/Blockchain.cs
+++ src/neo/Ledger/Blockchain.cs
@@ -167,7 +167,12 @@ namespace Neo.Ledger
                 Version = 0,
                 Script = script,
                 SystemFee = 0,
-                Signers = new[] { new Signer() { Account = (new[] { (byte)OpCode.PUSH1 }).ToScriptHash() } },
+                Signers = new[] { new Signer()
+                    {
+                    Account = (new[] { (byte)OpCode.PUSH1 }).ToScriptHash(),
+                    Scopes = WitnessScope.FeeOnly
+                    }
+                },
                 Attributes = Array.Empty<TransactionAttribute>(),
                 Witnesses = new[]
                 {
