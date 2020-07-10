e28cd1dfad8315b2c2424cd5aeaba9ea55357de1
erikzhang <erik@neo.org>
2020-07-10 14:48:19 +0800

    Update TransactionAttributeType.cs

--- src/neo/Network/P2P/Payloads/TransactionAttributeType.cs
+++ src/neo/Network/P2P/Payloads/TransactionAttributeType.cs
@@ -1,10 +1,6 @@
-using Neo.IO.Caching;
-
 namespace Neo.Network.P2P.Payloads
 {
     public enum TransactionAttributeType : byte
     {
-        //[ReflectionCache(typeof(Signer))]
-        //Signer = 0x01
     }
 }
