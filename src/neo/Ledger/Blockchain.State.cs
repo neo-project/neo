f93cddf8f5e8deba606f7a1480946947662630ed
ZhangTao <ztgoforit@163.com>
2020-07-16 15:52:12 +0800

    [neox] Changes in Ledger (#1737)
    * cn state root verify and persist
    
    * check state root cache
    
    * remove mpt proof, move to plugin
    
    * TryAdd optimization
    
    * fix
    
    * ..
    
    * file name
    
    * filename
    
    * fix error
    
    Co-authored-by: Shargon <Shargon@gmail.com>

---
+++ src/neo/Ledger/Blockchain.State.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using Akka.Actor;
+using Neo.Cryptography.MPT;
+using Neo.Network.P2P;
+using Neo.Network.P2P.Payloads;
+using Neo.Persistence;
+
+namespace Neo.Ledger
+{
+    public sealed partial class Blockchain : UntypedActor
+    {
+        public StateRoot LatestValidatorsStateRoot
+        {
+            get
+            {
+                var state_root = currentSnapshot.ValidatorsStateRoot.Get();
+                return state_root.RootHash is null ? null : state_root;
+            }
+        }
+        public long StateHeight => LatestValidatorsStateRoot is null ? -1 : (long)LatestValidatorsStateRoot.Index;
+
+        private readonly int MaxStateRootCacheCount = 100;
+        private Dictionary<uint, StateRoot> state_root_cache = new Dictionary<uint, StateRoot>();
+
+        public UInt256 GetLocalStateRoot(uint index)
+        {
+            return currentSnapshot.LocalStateRoot.TryGet(index)?.Hash;
+        }
+
+        private VerifyResult OnNewStateRoot(StateRoot root)
+        {
+            if (LatestValidatorsStateRoot != null && root.Index <= LatestValidatorsStateRoot.Index) return VerifyResult.AlreadyExists;
+            if (Height + MaxStateRootCacheCount < root.Index) return VerifyResult.Invalid;
+            if (Height < root.Index && root.Index <= Height + MaxStateRootCacheCount)
+            {
+                if (!state_root_cache.TryAdd(root.Index, root)) return VerifyResult.AlreadyExists;
+                return VerifyResult.Succeed;
+            }
+            var result = PersistCnStateRoot(root);
+            if (result == VerifyResult.Succeed && Height < root.Index + 2)
+                system.LocalNode.Tell(new LocalNode.SendDirectly { Inventory = root });
+            return result;
+        }
+
+        private VerifyResult PersistCnStateRoot(StateRoot root)
+        {
+            if (!root.Verify(currentSnapshot)) return VerifyResult.Invalid;
+            if (currentSnapshot.LocalStateRoot.TryGet(root.Index)?.Hash == root.RootHash)
+            {
+                using (SnapshotView snapshot = GetSnapshot())
+                {
+                    var confirmedRoot = snapshot.ValidatorsStateRoot.GetAndChange();
+                    confirmedRoot.Version = root.Version;
+                    confirmedRoot.Index = root.Index;
+                    confirmedRoot.RootHash = root.RootHash;
+                    confirmedRoot.Witness = root.Witness;
+                    snapshot.Commit();
+                }
+                UpdateCurrentSnapshot();
+                return VerifyResult.Succeed;
+            }
+            return VerifyResult.Invalid;
+        }
+
+        private void CheckStateRootCache()
+        {
+            var index = Height - 1;
+            if (0 <= index && state_root_cache.TryGetValue(index, out StateRoot root))
+            {
+                state_root_cache.Remove(index);
+                OnNewStateRoot(root);
+            }
+        }
+    }
+}
