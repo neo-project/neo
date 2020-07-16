bcf453e164a3765e03fb9abcab67b1653fa6000f
Shargon <Shargon@gmail.com>
2020-07-16 19:01:48 +0200

    Optimizations

--- src/neo/Ledger/StorageItem.cs
+++ src/neo/Ledger/StorageItem.cs
@@ -1,7 +1,9 @@
 using Neo.IO;
 using Neo.SmartContract;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Numerics;
 
 namespace Neo.Ledger
@@ -10,7 +12,8 @@ namespace Neo.Ledger
     {
         private byte[] value;
         public bool IsConstant;
-        private object cached;
+        private object cache;
+        private Func<byte[]> serializer;
 
         public int Size => Value.GetVarSize() + sizeof(bool);
 
@@ -18,13 +21,14 @@ namespace Neo.Ledger
         {
             get
             {
-                if (value is null && cached is IInteroperable interoperable)
-                    value = BinarySerializer.Serialize(interoperable.ToStackItem(null), 4096);
+                if (value is null && serializer != null)
+                    value = serializer();
                 return value;
             }
             set
             {
-                cached = null;
+                cache = null;
+                serializer = null;
                 this.value = value;
             }
         }
@@ -33,21 +37,25 @@ namespace Neo.Ledger
 
         public StorageItem(byte[] value, bool isConstant = false)
         {
-            this.value = value;
             this.IsConstant = isConstant;
+            this.value = value;
+            this.cache = null;
+            this.serializer = null;
         }
 
         public StorageItem(BigInteger value, bool isConstant = false)
         {
-            this.value = value.ToByteArrayStandard();
-            this.cached = value;
             this.IsConstant = isConstant;
+            this.value = value.ToByteArrayStandard();
+            this.cache = value;
         }
 
         public StorageItem(IInteroperable interoperable, bool isConstant = false)
         {
-            this.cached = interoperable;
             this.IsConstant = isConstant;
+            this.value = null;
+            this.cache = interoperable;
+            this.serializer = () => BinarySerializer.Serialize(interoperable.ToStackItem(null), 4096);
         }
 
         StorageItem ICloneable<StorageItem>.Clone()
@@ -65,43 +73,17 @@ namespace Neo.Ledger
             IsConstant = reader.ReadBoolean();
         }
 
-        public T[] GetSerializableArray<T>(int max = 0x1000000) where T : ISerializable, new()
-        {
-            if (cached is T[] value) return value;
-
-            var ret = Value.AsSerializableArray<T>(max);
-            cached = ret;
-            return ret;
-        }
-
-        public T GetSerializable<T>() where T : ISerializable, new()
-        {
-            if (cached is T value) return value;
-
-            var ret = Value.AsSerializable<T>();
-            cached = ret;
-            return ret;
-        }
-
-        public BigInteger GetBigInteger()
-        {
-            if (cached is BigInteger value) return value;
-
-            var ret = new BigInteger(Value);
-            cached = ret;
-            return ret;
-        }
-
         public void Set(BigInteger value)
         {
             this.value = value.ToByteArrayStandard();
-            this.cached = value;
+            this.cache = value;
         }
 
         public void Set<T>(IReadOnlyCollection<T> value) where T : ISerializable
         {
-            this.value = value.ToByteArray();
-            this.cached = value;
+            this.value = null;
+            this.cache = value;
+            this.serializer = () => value.ToByteArray();
         }
 
         void ICloneable<StorageItem>.FromReplica(StorageItem replica)
@@ -112,14 +94,39 @@ namespace Neo.Ledger
 
         public T GetInteroperable<T>() where T : IInteroperable, new()
         {
-            if (cached is null)
+            if (cache is null)
             {
                 var interoperable = new T();
                 interoperable.FromStackItem(BinarySerializer.Deserialize(value, 16, 34));
-                cached = interoperable;
+                this.serializer = () => BinarySerializer.Serialize(interoperable.ToStackItem(null), 4096);
+                cache = interoperable;
             }
             value = null;
-            return (T)cached;
+            return (T)cache;
+        }
+
+        public T[] GetSerializableArray<T>(int max = 0x1000000) where T : ISerializable, new()
+        {
+            if (cache is null)
+            {
+                var ret = Value.AsSerializableArray<T>(max);
+                this.serializer = () => ret.ToByteArray();
+                cache = ret;
+            }
+            value = null;
+            return ((IReadOnlyCollection<T>)cache).ToArray();
+        }
+
+        public BigInteger GetBigInteger()
+        {
+            if (cache is null)
+            {
+                var ret = new BigInteger(Value);
+                this.serializer = () => ret.ToByteArrayStandard();
+                cache = ret;
+            }
+            value = null;
+            return (BigInteger)cache;
         }
 
         public void Serialize(BinaryWriter writer)
