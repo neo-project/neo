03e810c344c4b95e2c101620b9b3eab33137ff13
erikzhang <erik@neo.org>
2020-07-16 16:17:21 +0800

    Optimize

--- src/neo/IO/Helper.cs
+++ src/neo/IO/Helper.cs
@@ -1,7 +1,7 @@
 using K4os.Compression.LZ4;
-using Neo.Network.P2P;
 using System;
 using System.Buffers;
+using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -70,18 +70,19 @@ namespace Neo.IO
             int maxLength = LZ4Codec.MaximumOutputSize(data.Length);
             using var buffer = MemoryPool<byte>.Shared.Rent(maxLength);
             int length = LZ4Codec.Encode(data, buffer.Memory.Span);
-            byte[] result = new byte[length + 4];
+            byte[] result = new byte[sizeof(uint) + length];
+            BinaryPrimitives.WriteInt32LittleEndian(result, length);
             buffer.Memory[..length].CopyTo(result.AsMemory(4));
-            Array.Copy(BitConverter.GetBytes(data.Length), result, 4);
             return result;
         }
 
         public static byte[] DecompressLz4(this byte[] data, int maxOutput)
         {
-            int length = BitConverter.ToInt32(data);
+            int length = BinaryPrimitives.ReadInt32LittleEndian(data);
             if (length < 0 || length > maxOutput) throw new FormatException();
             byte[] result = new byte[length];
-            if (LZ4Codec.Decode(data.AsSpan(4, data.Length - 4), result) != length) throw new FormatException();
+            if (LZ4Codec.Decode(data.AsSpan(4), result) != length)
+                throw new FormatException();
             return result;
         }
 
