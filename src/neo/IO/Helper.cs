9d83a64ee5f0c2a5efe4295b4ba03c1fe26a2087
Shargon <shargon@gmail.com>
2020-07-18 11:57:03 +0200

    Add length before compression data (#1768)
    * Add length before compression
    
    * Optimize
    
    * Fix UT
    
    * Add UT
    
    * Add UT
    
    Co-authored-by: erikzhang <erik@neo.org>

--- src/neo/IO/Helper.cs
+++ src/neo/IO/Helper.cs
@@ -1,6 +1,7 @@
 using K4os.Compression.LZ4;
 using System;
 using System.Buffers;
+using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -69,20 +70,19 @@ namespace Neo.IO
             int maxLength = LZ4Codec.MaximumOutputSize(data.Length);
             using var buffer = MemoryPool<byte>.Shared.Rent(maxLength);
             int length = LZ4Codec.Encode(data, buffer.Memory.Span);
-            byte[] result = new byte[length];
-            buffer.Memory[..length].CopyTo(result);
+            byte[] result = new byte[sizeof(uint) + length];
+            BinaryPrimitives.WriteInt32LittleEndian(result, data.Length);
+            buffer.Memory[..length].CopyTo(result.AsMemory(4));
             return result;
         }
 
         public static byte[] DecompressLz4(this byte[] data, int maxOutput)
         {
-            var maxDecompressDataLength = data.Length * 255;
-            if (maxDecompressDataLength > 0) maxOutput = Math.Min(maxOutput, maxDecompressDataLength);
-            using var buffer = MemoryPool<byte>.Shared.Rent(maxOutput);
-            int length = LZ4Codec.Decode(data, buffer.Memory.Span);
+            int length = BinaryPrimitives.ReadInt32LittleEndian(data);
             if (length < 0 || length > maxOutput) throw new FormatException();
             byte[] result = new byte[length];
-            buffer.Memory[..length].CopyTo(result);
+            if (LZ4Codec.Decode(data.AsSpan(4), result) != length)
+                throw new FormatException();
             return result;
         }
 
