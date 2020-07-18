5c1ba7ee7c9a38c5ef6938b22a934ffe576085af
erikzhang <erik@neo.org>
2020-07-18 18:33:02 +0800

    Allow FeeOnly for cosigners

--- src/neo/Network/P2P/Payloads/Transaction.cs
+++ src/neo/Network/P2P/Payloads/Transaction.cs
@@ -179,10 +179,7 @@ namespace Neo.Network.P2P.Payloads
             for (int i = 0; i < count; i++)
             {
                 Signer signer = reader.ReadSerializable<Signer>();
-                if (i > 0 && signer.Scopes == WitnessScope.FeeOnly)
-                    throw new FormatException();
-                if (!hashset.Add(signer.Account))
-                    throw new FormatException();
+                if (!hashset.Add(signer.Account)) throw new FormatException();
                 yield return signer;
             }
         }
