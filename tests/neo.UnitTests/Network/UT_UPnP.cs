e842b4dec314decc25fcca7b21197edbfcf69361
Shargon <shargon@gmail.com>
2020-08-06 15:30:35 +0200

    Increase coverage

---
+++ tests/neo.UnitTests/Network/UT_UPnP.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Network;
+using System;
+
+namespace Neo.UnitTests.Network
+{
+    [TestClass]
+    public class UT_UPnP
+    {
+        [TestMethod]
+        public void GetTimeOut()
+        {
+            Assert.AreEqual(3, UPnP.TimeOut.TotalSeconds);
+        }
+
+        [TestMethod]
+        public void NoService()
+        {
+            Assert.ThrowsException<Exception>(() => UPnP.ForwardPort(1, System.Net.Sockets.ProtocolType.Tcp, ""));
+            Assert.ThrowsException<Exception>(() => UPnP.DeleteForwardingRule(1, System.Net.Sockets.ProtocolType.Tcp));
+            Assert.ThrowsException<Exception>(() => UPnP.GetExternalIP());
+        }
+    }
+}
