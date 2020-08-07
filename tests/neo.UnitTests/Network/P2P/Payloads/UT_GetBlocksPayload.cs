6a785ab0c21c56355a9f2d5ad2a2d634085912d0
Shargon <shargon@gmail.com>
2020-08-07 13:16:23 +0200

    Add coverage

--- tests/neo.UnitTests/Network/P2P/Payloads/UT_GetBlocksPayload.cs
+++ tests/neo.UnitTests/Network/P2P/Payloads/UT_GetBlocksPayload.cs
@@ -2,6 +2,7 @@ using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.IO;
 using Neo.Network.P2P.Payloads;
+using System;
 
 namespace Neo.UnitTests.Network.P2P.Payloads
 {
@@ -21,11 +22,16 @@ namespace Neo.UnitTests.Network.P2P.Payloads
         [TestMethod]
         public void DeserializeAndSerialize()
         {
-            var test = new GetBlocksPayload() { Count = 5, HashStart = UInt256.Parse("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff01") };
+            var test = GetBlocksPayload.Create(UInt256.Parse("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff01"), 5);
             var clone = test.ToArray().AsSerializable<GetBlocksPayload>();
 
             Assert.AreEqual(test.Count, clone.Count);
             Assert.AreEqual(test.HashStart, clone.HashStart);
+            Assert.AreEqual(5, clone.Count);
+            Assert.AreEqual("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff01", clone.HashStart.ToString());
+
+            Assert.ThrowsException<FormatException>(() => GetBlocksPayload.Create(UInt256.Zero, -2).ToArray().AsSerializable<GetBlocksPayload>());
+            Assert.ThrowsException<FormatException>(() => GetBlocksPayload.Create(UInt256.Zero, 0).ToArray().AsSerializable<GetBlocksPayload>());
         }
     }
 }
