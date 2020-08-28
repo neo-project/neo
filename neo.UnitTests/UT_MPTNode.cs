0e3fd9dad71f67e310ef942b6c91d0fe4052f4ab
KickSeason <ztgoforit@163.com>
2020-08-28 09:54:52 +0800

    add ut for MPTNode size

--- neo.UnitTests/UT_MPTNode.cs
+++ neo.UnitTests/UT_MPTNode.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.Trie.MPT;
+using Neo.IO;
 
 namespace Neo.UnitTests.Trie.MPT
 {
@@ -11,6 +12,23 @@ namespace Neo.UnitTests.Trie.MPT
         {
             var b = new BranchNode();
             Assert.IsTrue(b.Dirty);
+        }
+
+        [TestMethod]
+        public void TestSize()
+        {
+            var ln = new LeafNode(new byte[] { 1, 2, 8, 16, 32 });
+            Assert.AreEqual(ln.ToArray().Length, ln.Size);
+
+            var en = new ExtensionNode();
+            en.Key = new byte[] { 0x0a, 0x01 };
+            en.Next = ln;
+            Assert.AreEqual(en.ToArray().Length, en.Size);
+
+            var bn = new BranchNode();
+            bn.Children[1] = ln;
+            bn.Children[0x0a] = en;
+            Assert.AreEqual(bn.ToArray().Length, bn.Size);
         }
     }
 }
