15b74bc06d2372d69f75518f2d14dec853b4e8b1
Erik Zhang <erik@neo.org>
2020-10-10 17:59:24 +0800

    Fix CallFromNativeContract (#1994)

--- src/neo/SmartContract/ApplicationEngine.cs
+++ src/neo/SmartContract/ApplicationEngine.cs
@@ -87,7 +87,7 @@ namespace Neo.SmartContract
             InvocationState state = GetInvocationState(CurrentContext);
             state.ReturnType = typeof(void);
             state.Callback = onComplete;
-            CallContract(hash, method, new VMArray(args));
+            CallContract(hash, method, new VMArray(ReferenceCounter, args));
         }
 
         internal void CallFromNativeContract<T>(Action<T> onComplete, UInt160 hash, string method, params StackItem[] args)
@@ -95,7 +95,7 @@ namespace Neo.SmartContract
             InvocationState state = GetInvocationState(CurrentContext);
             state.ReturnType = typeof(T);
             state.Callback = onComplete;
-            CallContract(hash, method, new VMArray(args));
+            CallContract(hash, method, new VMArray(ReferenceCounter, args));
         }
 
         protected override void ContextUnloaded(ExecutionContext context)
