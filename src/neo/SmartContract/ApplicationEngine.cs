7fe1e9c9f85a8f9b4afdf95a79d21ab16fe731ad
Erik Zhang <erik@neo.org>
2020-11-05 17:55:06 +0800

    Fix CallFromNativeContract

--- src/neo/SmartContract/ApplicationEngine.cs
+++ src/neo/SmartContract/ApplicationEngine.cs
@@ -126,6 +126,7 @@ namespace Neo.SmartContract
                 case null:
                     break;
                 case Action action:
+                    Pop();
                     action();
                     break;
                 default:
