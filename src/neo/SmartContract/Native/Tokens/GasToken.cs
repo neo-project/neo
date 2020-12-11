a68cbb6543f774fd9fe842b5bef4d330641733ad
Erik Zhang <erik@neo.org>
2020-12-11 16:28:29 +0800

    Fix asset symbol (#2136)
    * Fix asset symbol
    
    * Increase coverage
    
    Co-authored-by: Shargon <shargon@gmail.com>

--- src/neo/SmartContract/Native/Tokens/GasToken.cs
+++ src/neo/SmartContract/Native/Tokens/GasToken.cs
@@ -9,7 +9,7 @@ namespace Neo.SmartContract.Native.Tokens
         public override int Id => -2;
         public override string Name => "GAS";
         public override uint ActiveBlockIndex => 0;
-        public override string Symbol => "gas";
+        public override string Symbol => "GAS";
         public override byte Decimals => 8;
 
         internal GasToken()
