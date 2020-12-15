e41303698da5cf091247d0207d94aca3b22fea2c
Tommo-L <luchuan@neo.org>
2020-12-15 23:16:38 +0800

    public SignatureContractCost

--- src/neo/SmartContract/Helper.cs
+++ src/neo/SmartContract/Helper.cs
@@ -14,6 +14,23 @@ namespace Neo.SmartContract
     {
         public const long MaxVerificationGas = 0_50000000;
 
+        public static long SignatureContractCost(uint execFeeFactor)
+        {
+            return execFeeFactor * (ApplicationEngine.OpCodePrices[OpCode.PUSHDATA1] * 2 +
+                ApplicationEngine.OpCodePrices[OpCode.PUSHNULL] +
+                ApplicationEngine.OpCodePrices[OpCode.SYSCALL] +
+                ApplicationEngine.ECDsaVerifyPrice);
+        }
+
+        public static long MultiSignatureContractCost(uint execFeeFactor, int m, int n)
+        {
+            return execFeeFactor * (ApplicationEngine.OpCodePrices[OpCode.PUSHDATA1] * (m + n) +
+                ApplicationEngine.OpCodePrices[OpCode.PUSHINT8] * 2 +
+                ApplicationEngine.OpCodePrices[OpCode.PUSHNULL] +
+                ApplicationEngine.OpCodePrices[OpCode.SYSCALL] +
+                ApplicationEngine.ECDsaVerifyPrice * n);
+        }
+
         public static UInt160 GetContractHash(UInt160 sender, byte[] script)
         {
             using var sb = new ScriptBuilder();
