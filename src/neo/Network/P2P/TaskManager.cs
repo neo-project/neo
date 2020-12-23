6c94178ea3f0152bbc83663e6faf841c5c6801d2
Tommo-L <luchuan@neo.org>
2020-12-23 16:18:02 +0800

    fix

--- src/neo/Network/P2P/TaskManager.cs
+++ src/neo/Network/P2P/TaskManager.cs
@@ -95,7 +95,7 @@ namespace Neo.Network.P2P
             if (!sessions.TryGetValue(Sender, out TaskSession session))
                 return;
             // Do not accept payload of type InventoryType.TX if not synced on best known HeaderHeight
-            if (payload.Type == InventoryType.TX && Blockchain.Singleton.Height < sessions.Values.Max(p => p.LastBlockIndex))
+            if (payload.Type == InventoryType.TX && sessions.Values.Where(p => p.LastBlockIndex > Blockchain.Singleton.Height).Count() > sessions.Count / 2)
                 return;
             HashSet<UInt256> hashes = new HashSet<UInt256>(payload.Hashes);
             // Remove all previously processed knownHashes from the list that is being requested
