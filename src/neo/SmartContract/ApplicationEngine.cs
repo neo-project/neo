198e9a61b735bb304ab57915c940d7323d1f8d2d
Erik Zhang <erik@neo.org>
2020-12-28 18:55:38 +0800

    Add native contract base class: NonfungibleToken (#2177)

--- src/neo/SmartContract/ApplicationEngine.cs
+++ src/neo/SmartContract/ApplicationEngine.cs
@@ -1,4 +1,5 @@
 using Neo.IO;
+using Neo.IO.Json;
 using Neo.Ledger;
 using Neo.Network.P2P.Payloads;
 using Neo.Persistence;
@@ -159,6 +160,7 @@ namespace Neo.SmartContract
                 byte[] data => data,
                 string s => s,
                 BigInteger i => i,
+                JObject o => o.ToByteArray(false),
                 IInteroperable interoperable => interoperable.ToStackItem(ReferenceCounter),
                 ISerializable i => i.ToArray(),
                 StackItem item => item,
