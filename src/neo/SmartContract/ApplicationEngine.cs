3c1811ddef7475f2faa37ab8f2c084b562b2633e
Erik Zhang <erik@neo.org>
2021-01-08 13:26:08 +0800

    Check return type in ABI

--- src/neo/SmartContract/ApplicationEngine.cs
+++ src/neo/SmartContract/ApplicationEngine.cs
@@ -106,6 +106,7 @@ namespace Neo.SmartContract
             CallFlags callingFlags = state.CallFlags;
 
             if (args.Length != md.Parameters.Length) throw new InvalidOperationException($"Method {method} Expects {md.Parameters.Length} Arguments But Receives {args.Length} Arguments");
+            if (hasReturnValue ^ (md.ReturnType != ContractParameterType.Void)) throw new InvalidOperationException();
             ExecutionContext context_new = LoadContract(contract, method, flags & callingFlags, hasReturnValue, (ushort)args.Length);
             state = context_new.GetState<ExecutionContextState>();
             state.CallingScriptHash = callingScriptHash;
