4099cf4388bb88a7fd226772b722755155e3b909
Erik Zhang <erik@neo.org>
2021-01-30 19:03:25 +0800

    Update TaskSession.cs

--- src/neo/Network/P2P/TaskSession.cs
+++ src/neo/Network/P2P/TaskSession.cs
@@ -8,9 +8,8 @@ namespace Neo.Network.P2P
 {
     internal class TaskSession
     {
-        public readonly Dictionary<UInt256, DateTime> Tasks = new Dictionary<UInt256, DateTime>();
-        public readonly HashSet<UInt256> AvailableTasks = new HashSet<UInt256>();
-
+        public Dictionary<UInt256, DateTime> Tasks { get; } = new Dictionary<UInt256, DateTime>();
+        public HashSet<UInt256> AvailableTasks { get; } = new HashSet<UInt256>();
         public bool HasTask => Tasks.Count > 0;
         public bool IsFullNode { get; }
         public uint LastBlockIndex { get; set; }
