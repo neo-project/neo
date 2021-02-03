a84ce4bb9cbd045fff16d6c3efc0061de950ccdd
Qiao Jin <jinqiao@onchain.com>
2021-02-03 10:15:39 +0800

    Revert "fix"
    This reverts commit 9d8057b656d418ec59f576fbcd43c9c9a047dd5a.

--- src/neo/Network/P2P/TaskManager.cs
+++ src/neo/Network/P2P/TaskManager.cs
@@ -74,7 +74,7 @@ namespace Neo.Network.P2P
             // Do not accept payload of type InventoryType.TX if not synced on HeaderHeight
             uint currentHeight = NativeContract.Ledger.CurrentIndex(Blockchain.Singleton.View);
             uint headerHeight = Blockchain.Singleton.HeaderCache.Last?.Index ?? currentHeight;
-            if (currentHeight < headerHeight)
+            if (payload.Type == InventoryType.TX && currentHeight < headerHeight)
             {
                 RequestTasks(Sender, session);
                 return;
