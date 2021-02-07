8d6935fc4045d8ddb2e2787f083b0d4ffda9051c
Shargon <shargon@gmail.com>
2021-02-07 13:42:11 +0100

    Clean todo

--- tests/neo.UnitTests/TestBlockchain.cs
+++ tests/neo.UnitTests/TestBlockchain.cs
@@ -1,9 +1,5 @@
-using Neo.Ledger;
 using Neo.Persistence;
 using System;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Reflection;
 
 namespace Neo.UnitTests
 {
@@ -16,26 +12,9 @@ namespace Neo.UnitTests
         {
             Console.WriteLine("initialize NeoSystem");
             TheNeoSystem = new NeoSystem(ProtocolSettings.Default, null, null);
-
-            // Ensure that blockchain is loaded
-
-            // TODO: fix extensibleWitnessWhiteList
-            //Blockchain.Singleton = TheNeoSystem.Blockchain;
-
-            //DefaultExtensibleWitnessWhiteList = (typeof(Blockchain).GetField("extensibleWitnessWhiteList",
-            //    BindingFlags.Instance | BindingFlags.NonPublic).GetValue(bc) as ImmutableHashSet<UInt160>).ToArray();
-            //AddWhiteList(DefaultExtensibleWitnessWhiteList); // Add other address
         }
 
         public static void InitializeMockNeoSystem() { }
-
-        public static void AddWhiteList(params UInt160[] address)
-        {
-            var builder = ImmutableHashSet.CreateBuilder<UInt160>();
-            foreach (var entry in address) builder.Add(entry);
-
-            //typeof(Blockchain).GetField("extensibleWitnessWhiteList", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(bc, builder.ToImmutable());
-        }
 
         internal static DataCache GetTestSnapshot()
         {
