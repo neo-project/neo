9f65cf86629fed8ef5d6c2771b591a360aef6ee9
Shargon <shargon@gmail.com>
2021-02-08 16:07:47 +0100

    Add test for UT_StdLib

---
+++ tests/neo.UnitTests/SmartContract/Native/UT_StdLib.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.SmartContract.Native;
+using System.Numerics;
+
+namespace Neo.UnitTests.SmartContract.Native
+{
+    [TestClass]
+    public class UT_StdLib
+    {
+        [TestMethod]
+        public void TestBinary()
+        {
+            var data = System.Array.Empty<byte>();
+
+            CollectionAssert.AreEqual(data, StdLib.Base64Decode(StdLib.Base64Encode(data)));
+            CollectionAssert.AreEqual(data, StdLib.Base58Decode(StdLib.Base58Encode(data)));
+
+            data = new byte[] { 1, 2, 3 };
+
+            CollectionAssert.AreEqual(data, StdLib.Base64Decode(StdLib.Base64Encode(data)));
+            CollectionAssert.AreEqual(data, StdLib.Base58Decode(StdLib.Base58Encode(data)));
+            Assert.AreEqual("AQIDBA==", StdLib.Base64Encode(new byte[] { 1, 2, 3, 4 }));
+            Assert.AreEqual("2VfUX", StdLib.Base58Encode(new byte[] { 1, 2, 3, 4 }));
+        }
+
+        [TestMethod]
+        public void TestItoaAtoi()
+        {
+            Assert.AreEqual("1", StdLib.Itoa(BigInteger.One, 10));
+            Assert.AreEqual("1", StdLib.Itoa(BigInteger.One, 16));
+            Assert.AreEqual("-1", StdLib.Itoa(BigInteger.MinusOne, 10));
+            Assert.AreEqual("f", StdLib.Itoa(BigInteger.MinusOne, 16));
+            Assert.AreEqual(-1, StdLib.Atoi("-1", 10));
+            Assert.AreEqual(1, StdLib.Atoi("+1", 10));
+            Assert.AreEqual(-1, StdLib.Atoi("ff", 16));
+            Assert.ThrowsException<System.FormatException>(() => StdLib.Atoi("a", 10));
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => StdLib.Atoi("a", 11));
+        }
+    }
+}
