435fb1d6612c845de42c4127a76f93082910b16a
Shargon <shargon@gmail.com>
2021-02-09 15:18:30 +0100

    Update CallFlags.cs

--- src/neo/SmartContract/CallFlags.cs
+++ src/neo/SmartContract/CallFlags.cs
@@ -8,7 +8,7 @@ namespace Neo.SmartContract
         None = 0,
 
         ReadStates = 0b00000001,
-        WriteStates = 0b00000011,
+        WriteStates = 0b00000010,
         AllowCall = 0b00000100,
         AllowNotify = 0b00001000,
 
