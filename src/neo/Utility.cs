d517a85e26381c4c1c005bbdbaba799bd35f6311
Erik Zhang <erik@neo.org>
2021-02-11 16:11:58 +0800

    Remove Utility.LoadConfig (#2330)
    Co-authored-by: Shargon <shargon@gmail.com>

--- src/neo/Utility.cs
+++ src/neo/Utility.cs
@@ -1,10 +1,6 @@
 using Akka.Actor;
 using Akka.Event;
-using Microsoft.Extensions.Configuration;
 using Neo.Plugins;
-using System;
-using System.IO;
-using System.Reflection;
 using System.Text;
 
 namespace Neo
@@ -27,39 +23,6 @@ namespace Neo
             StrictUTF8 = (Encoding)Encoding.UTF8.Clone();
             StrictUTF8.DecoderFallback = DecoderFallback.ExceptionFallback;
             StrictUTF8.EncoderFallback = EncoderFallback.ExceptionFallback;
-        }
-
-        /// <summary>
-        /// Load configuration with different Environment Variable
-        /// </summary>
-        /// <param name="config">Configuration</param>
-        /// <returns>IConfigurationRoot</returns>
-        public static IConfigurationRoot LoadConfig(string config)
-        {
-            var env = Environment.GetEnvironmentVariable("NEO_NETWORK");
-            var configFile = string.IsNullOrWhiteSpace(env) ? $"{config}.json" : $"{config}.{env}.json";
-
-            // Working directory
-            var file = Path.Combine(Environment.CurrentDirectory, configFile);
-            if (!File.Exists(file))
-            {
-                // EntryPoint folder
-                file = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), configFile);
-                if (!File.Exists(file))
-                {
-                    // neo.dll folder
-                    file = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), configFile);
-                    if (!File.Exists(file))
-                    {
-                        // default config
-                        return new ConfigurationBuilder().Build();
-                    }
-                }
-            }
-
-            return new ConfigurationBuilder()
-                .AddJsonFile(file, true)
-                .Build();
         }
 
         public static void Log(string source, LogLevel level, object message)
