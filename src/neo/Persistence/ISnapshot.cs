6107f0189a3084d69f6f22281b04c031bfb50c70
Erik Zhang <erik@neo.org>
2021-03-13 23:10:47 +0800

    Add XML comments part 3

--- src/neo/Persistence/ISnapshot.cs
+++ src/neo/Persistence/ISnapshot.cs
@@ -7,8 +7,22 @@ namespace Neo.Persistence
     /// </summary>
     public interface ISnapshot : IDisposable, IReadOnlyStore
     {
+        /// <summary>
+        /// Commits all changes in the snapshot to the database.
+        /// </summary>
         void Commit();
+
+        /// <summary>
+        /// Deletes an entry from the snapshot.
+        /// </summary>
+        /// <param name="key">The key of the entry.</param>
         void Delete(byte[] key);
+
+        /// <summary>
+        /// Puts an entry to the snapshot.
+        /// </summary>
+        /// <param name="key">The key of the entry.</param>
+        /// <param name="value">The data of the entry.</param>
         void Put(byte[] key, byte[] value);
     }
 }
