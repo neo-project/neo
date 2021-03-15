d8a1d5093509a9a2d36eadb33f516e9a9998bde3
Erik Zhang <erik@neo.org>
2021-03-15 17:24:39 +0800

    Add XML comments part 4

--- src/neo/SmartContract/Manifest/ContractPermission.cs
+++ src/neo/SmartContract/Manifest/ContractPermission.cs
@@ -9,23 +9,28 @@ using Array = Neo.VM.Types.Array;
 namespace Neo.SmartContract.Manifest
 {
     /// <summary>
-    /// The permissions field is an array containing a set of Permission objects. It describes which contracts may be invoked and which methods are called.
+    /// Represents a permission of a contract. It describes which contracts may be invoked and which methods are called.
+    /// If a contract invokes a contract or method that is not declared in the manifest at runtime, the invocation will fail.
     /// </summary>
     public class ContractPermission : IInteroperable
     {
         /// <summary>
-        /// The contract field indicates the contract to be invoked. It can be a hash of a contract, a public key of a group, or a wildcard *.
-        /// If it specifies a hash of a contract, then the contract will be invoked; If it specifies a public key of a group, then any contract in this group will be invoked; If it specifies a wildcard*, then any contract will be invoked.
+        /// Indicates which contract to be invoked.
+        /// It can be a hash of a contract, a public key of a group, or a wildcard *.
+        /// If it specifies a hash of a contract, then the contract will be invoked; If it specifies a public key of a group, then any contract in this group may be invoked; If it specifies a wildcard *, then any contract may be invoked.
         /// </summary>
         public ContractPermissionDescriptor Contract { get; set; }
 
         /// <summary>
-        /// The methods field is an array containing a set of methods to be called. It can also be assigned with a wildcard *. If it is a wildcard *, then it means that any method can be called.
-        /// If a contract invokes a contract or method that is not declared in the manifest at runtime, the invocation will fail.
+        /// Indicates which methods to be called.
+        /// It can also be assigned with a wildcard *. If it is a wildcard *, then it means that any method can be called.
         /// </summary>
         public WildcardContainer<string> Methods { get; set; }
 
-        public static readonly ContractPermission DefaultPermission = new ContractPermission
+        /// <summary>
+        /// A default permission that both <see cref="Contract"/> and <see cref="Methods"/> fields are set to wildcard *.
+        /// </summary>
+        public static readonly ContractPermission DefaultPermission = new()
         {
             Contract = ContractPermissionDescriptor.CreateWildcard(),
             Methods = WildcardContainer<string>.CreateWildcard()
@@ -57,10 +62,10 @@ namespace Neo.SmartContract.Manifest
         }
 
         /// <summary>
-        /// Parse ContractPermission from json
+        /// Converts the permission from a JSON object.
         /// </summary>
-        /// <param name="json">Json</param>
-        /// <returns>Return ContractPermission</returns>
+        /// <param name="json">The permission represented by a JSON object.</param>
+        /// <returns>The converted permission.</returns>
         public static ContractPermission FromJson(JObject json)
         {
             ContractPermission permission = new ContractPermission
@@ -74,9 +79,10 @@ namespace Neo.SmartContract.Manifest
             return permission;
         }
 
-        /// <summary
-        /// To json
+        /// <summary>
+        /// Converts the permission to a JSON object.
         /// </summary>
+        /// <returns>The permission represented by a JSON object.</returns>
         public JObject ToJson()
         {
             var json = new JObject();
@@ -86,11 +92,11 @@ namespace Neo.SmartContract.Manifest
         }
 
         /// <summary>
-        /// Return true if is allowed
+        /// Determines whether the method of the specified contract can be called by this contract.
         /// </summary>
-        /// <param name="targetContract">The contract that we are calling</param>
-        /// <param name="targetMethod">The method that we are calling</param>
-        /// <returns>Return true or false</returns>
+        /// <param name="targetContract">The contract being called.</param>
+        /// <param name="targetMethod">The method of the specified contract.</param>
+        /// <returns><see langword="true"/> if the contract allows to be called; otherwise, <see langword="false"/>.</returns>
         public bool IsAllowed(ContractState targetContract, string targetMethod)
         {
             if (Contract.IsHash)
--- src/neo/SmartContract/Manifest/WildCardContainer.cs
+++ src/neo/SmartContract/Manifest/WildCardContainer.cs
@@ -6,44 +6,47 @@ using System.Linq;
 
 namespace Neo.SmartContract.Manifest
 {
+    /// <summary>
+    /// A list that supports wildcard.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements.</typeparam>
     public class WildcardContainer<T> : IReadOnlyList<T>
     {
         private readonly T[] _data;
 
         public T this[int index] => _data[index];
 
-        /// <summary>
-        /// Number of items
-        /// </summary>
         public int Count => _data?.Length ?? 0;
 
         /// <summary>
-        /// Is wildcard?
+        /// Indicates whether the list is a wildcard.
         /// </summary>
         public bool IsWildcard => _data is null;
 
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="data">Data</param>
         private WildcardContainer(T[] data)
         {
             _data = data;
         }
 
         /// <summary>
-        /// Create a new WildCardContainer
+        /// Creates a new instance of the <see cref="WildcardContainer&lt;T&gt;"/> class with the initial elements.
         /// </summary>
-        /// <param name="data">Data</param>
-        /// <returns>WildCardContainer</returns>
-        public static WildcardContainer<T> Create(params T[] data) => new WildcardContainer<T>(data);
+        /// <param name="data">The initial elements.</param>
+        /// <returns>The created list.</returns>
+        public static WildcardContainer<T> Create(params T[] data) => new(data);
 
         /// <summary>
-        /// Create a wildcard
+        /// Creates a new instance of the <see cref="WildcardContainer&lt;T&gt;"/> class with wildcard.
         /// </summary>
-        /// <returns>WildCardContainer</returns>
-        public static WildcardContainer<T> CreateWildcard() => new WildcardContainer<T>(null);
+        /// <returns>The created list.</returns>
+        public static WildcardContainer<T> CreateWildcard() => new(null);
 
+        /// <summary>
+        /// Converts the list from a JSON object.
+        /// </summary>
+        /// <param name="json">The list represented by a JSON object.</param>
+        /// <param name="elementSelector">A converter for elements.</param>
+        /// <returns>The converted list.</returns>
         public static WildcardContainer<T> FromJson(JObject json, Func<JObject, T> elementSelector)
         {
             switch (json)
@@ -60,13 +63,17 @@ namespace Neo.SmartContract.Manifest
 
         public IEnumerator<T> GetEnumerator()
         {
-            if (_data == null) return ((IReadOnlyList<T>)new T[0]).GetEnumerator();
+            if (_data == null) return ((IReadOnlyList<T>)Array.Empty<T>()).GetEnumerator();
 
             return ((IReadOnlyList<T>)_data).GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+        /// <summary>
+        /// Converts the list to a JSON object.
+        /// </summary>
+        /// <returns>The list represented by a JSON object.</returns>
         public JObject ToJson()
         {
             if (IsWildcard) return "*";
