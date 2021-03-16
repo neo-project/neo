8e5c8fd7b06d6489b2fd9079d3da251f3f2fd479
Erik Zhang <erik@neo.org>
2021-03-16 20:11:33 +0800

    Add XML comments part 8

--- src/neo/SmartContract/FindOptions.cs
+++ src/neo/SmartContract/FindOptions.cs
@@ -2,18 +2,50 @@ using System;
 
 namespace Neo.SmartContract
 {
+    /// <summary>
+    /// Specify the options to be used during the search.
+    /// </summary>
     [Flags]
     public enum FindOptions : byte
     {
+        /// <summary>
+        /// No option is set. The results will be an iterator of (key, value).
+        /// </summary>
         None = 0,
 
+        /// <summary>
+        /// Indicates that only keys need to be returned. The results will be an iterator of keys.
+        /// </summary>
         KeysOnly = 1 << 0,
+
+        /// <summary>
+        /// Indicates that the prefix byte of keys should be removed before return.
+        /// </summary>
         RemovePrefix = 1 << 1,
+
+        /// <summary>
+        /// Indicates that only values need to be returned. The results will be an iterator of values.
+        /// </summary>
         ValuesOnly = 1 << 2,
+
+        /// <summary>
+        /// Indicates that values should be deserialized before return.
+        /// </summary>
         DeserializeValues = 1 << 3,
+
+        /// <summary>
+        /// Indicates that only the field 0 of the deserialized values need to be returned. This flag must be set together with <see cref="DeserializeValues"/>.
+        /// </summary>
         PickField0 = 1 << 4,
+
+        /// <summary>
+        /// Indicates that only the field 1 of the deserialized values need to be returned. This flag must be set together with <see cref="DeserializeValues"/>.
+        /// </summary>
         PickField1 = 1 << 5,
 
+        /// <summary>
+        /// This value is only for internal use, and shouldn't be used in smart contracts.
+        /// </summary>
         All = KeysOnly | RemovePrefix | ValuesOnly | DeserializeValues | PickField0 | PickField1
     }
 }
