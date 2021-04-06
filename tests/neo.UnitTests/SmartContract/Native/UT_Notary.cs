7ab45aaad91901711c0fc761ccad3c10d7767f15
doubiliu <liumengyu@ngd.neo.org>
2021-04-06 14:11:28 +0800

    Add UT

--- tests/neo.UnitTests/SmartContract/Native/UT_Notary.cs
+++ tests/neo.UnitTests/SmartContract/Native/UT_Notary.cs
@@ -6,6 +6,7 @@ using Neo.Persistence;
 using Neo.SmartContract;
 using Neo.SmartContract.Native;
 using Neo.UnitTests.Extensions;
+using Neo.VM;
 using System;
 using System.Linq;
 using System.Numerics;
@@ -25,11 +26,61 @@ namespace Neo.UnitTests.SmartContract.Native
         }
 
         [TestMethod]
-        public void Check_MaxNotValidBeforeDeltaAndNotaryServiceFeePerKey()
+        public void Check_BalanceOf()
+        {
+            NativeContract.Notary.BalanceOf(_snapshot, UInt160.Zero).Should().Be(0);
+        }
+
+        [TestMethod]
+        public void Check_ExpirationOf()
+        {
+            NativeContract.Notary.ExpirationOf(_snapshot,UInt160.Zero).Should().Be(0);
+        }
+
+        [TestMethod]
+        public void Check_GetMaxNotValidBeforeDelta()
+        {
+            NativeContract.Notary.GetMaxNotValidBeforeDelta(_snapshot).Should().Be(140);
+        }
+
+        [TestMethod]
+        public void Check_SetMaxNotValidBeforeDelta()
         {
             var snapshot = _snapshot.CreateSnapshot();
-            NativeContract.Notary.GetMaxNotValidBeforeDelta(snapshot).Should().Be(140);
-            NativeContract.Notary.GetNotaryServiceFeePerKey(snapshot).Should().Be(10000000L);
+            // Fake blockchain
+            var persistingBlock = new Block { Header = new Header { Index = 1000 } };
+            UInt160 committeeAddress = NativeContract.NEO.GetCommitteeAddress(snapshot);
+
+            using var engine = ApplicationEngine.Create(TriggerType.Application, new Nep17NativeContractExtensions.ManualWitness(committeeAddress), snapshot, persistingBlock, settings: TestBlockchain.TheNeoSystem.Settings);
+            using var script = new ScriptBuilder();
+            script.EmitDynamicCall(NativeContract.Notary.Hash, "setMaxNotValidBeforeDelta", 100);
+            engine.LoadScript(script.ToArray());
+            VMState vMState = engine.Execute();
+            vMState.Should().Be(VMState.HALT);
+            NativeContract.Notary.GetMaxNotValidBeforeDelta(snapshot).Should().Be(100);
+        }
+
+        [TestMethod]
+        public void Check_GetNotaryServiceFeePerKey()
+        {
+            NativeContract.Notary.GetNotaryServiceFeePerKey(_snapshot).Should().Be(10000000L);
+        }
+
+        [TestMethod]
+        public void Check_SetNotaryServiceFeePerKey()
+        {
+            var snapshot = _snapshot.CreateSnapshot();
+            // Fake blockchain
+            var persistingBlock = new Block { Header = new Header { Index = 1000 } };
+            UInt160 committeeAddress = NativeContract.NEO.GetCommitteeAddress(snapshot);
+
+            using var engine = ApplicationEngine.Create(TriggerType.Application, new Nep17NativeContractExtensions.ManualWitness(committeeAddress), snapshot, persistingBlock, settings: TestBlockchain.TheNeoSystem.Settings);
+            using var script = new ScriptBuilder();
+            script.EmitDynamicCall(NativeContract.Notary.Hash, "setNotaryServiceFeePerKey", 100);
+            engine.LoadScript(script.ToArray());
+            VMState vMState = engine.Execute();
+            vMState.Should().Be(VMState.HALT);
+            NativeContract.Notary.GetNotaryServiceFeePerKey(snapshot).Should().Be(100);
         }
 
         internal static StorageKey CreateStorageKey(byte prefix, uint key)
