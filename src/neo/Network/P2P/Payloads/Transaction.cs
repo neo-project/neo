9af95c25c230596afe6bc5cf671fbbc3d83f9711
Shargon <shargon@gmail.com>
2021-05-06 23:28:53 +0200

    Add reference counter?

--- src/neo/Network/P2P/Payloads/Transaction.cs
+++ src/neo/Network/P2P/Payloads/Transaction.cs
@@ -416,7 +416,7 @@ namespace Neo.Network.P2P.Payloads
                 // Transaction properties
                 (int)Version,
                 Nonce,
-                new Array(Signers.Select(u=> (ByteString)u.ToArray())),
+                new Array(referenceCounter, Signers.Select(u=> (ByteString)u.ToArray())),
                 SystemFee,
                 NetworkFee,
                 ValidUntilBlock,
