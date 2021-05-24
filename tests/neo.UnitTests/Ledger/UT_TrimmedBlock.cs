6a1ba3d3fd91f39cd6c0bb77595ca421401d9ac8
Jinghui Liao <jinghui@wayne.edu>
2021-05-24 18:57:41 -0400

    update test

--- tests/neo.UnitTests/Ledger/UT_TrimmedBlock.cs
+++ tests/neo.UnitTests/Ledger/UT_TrimmedBlock.cs
@@ -87,7 +87,7 @@ namespace Neo.UnitTests.Ledger
         {
             TrimmedBlock tblock = GetTrimmedBlockWithNoTransaction();
             tblock.Hashes = new UInt256[] { TestUtils.GetTransaction(UInt160.Zero).Hash };
-            tblock.Size.Should().Be(142); // 138 + nonce
+            tblock.Size.Should().Be(138);
         }
 
         [TestMethod]
