73b989ceb84e9bd482f508163c4e91a842000f61
Jinghui Liao <jinghui@wayne.edu>
2021-06-10 05:04:21 +0800

    add more test to `Murmur128`

--- tests/neo.UnitTests/Cryptography/UT_Murmur128.cs
+++ tests/neo.UnitTests/Cryptography/UT_Murmur128.cs
@@ -20,6 +20,12 @@ namespace Neo.UnitTests.Cryptography
         {
             byte[] array =  Encoding.ASCII.GetBytes("hello");
             array.Murmur128(123u).ToHexString().ToString().Should().Be("0bc59d0ad25fde2982ed65af61227a0e");
+
+            array = Encoding.ASCII.GetBytes("world");
+            array.Murmur128(123u).ToHexString().ToString().Should().Be("3d3810fed480472bd214a14023bb407f");
+
+            array = Encoding.ASCII.GetBytes("hello world");
+            array.Murmur128(123u).ToHexString().ToString().Should().Be("e0a0632d4f51302c55e3b3e48d28795d");
         }
     }
 }
