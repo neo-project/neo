5d5bde993630556ae61118f5598483ad5eb86695
Shargon <shargon@gmail.com>
2021-07-09 13:52:09 +0200

    Change to ExecutionEngineLimits.Default.MaxStackSize, ExecutionEngineLimits.Default

--- src/neo/SmartContract/Iterators/StorageIterator.cs
+++ src/neo/SmartContract/Iterators/StorageIterator.cs
@@ -38,7 +38,7 @@ namespace Neo.SmartContract.Iterators
                 key = key[prefixLength..];
 
             StackItem item = options.HasFlag(FindOptions.DeserializeValues)
-                ? BinarySerializer.Deserialize(value, 1024, 2048, referenceCounter)
+                ? BinarySerializer.Deserialize(value, ExecutionEngineLimits.Default.MaxStackSize, ExecutionEngineLimits.Default.MaxStackSize, referenceCounter)
                 : value;
 
             if (options.HasFlag(FindOptions.PickField0))
