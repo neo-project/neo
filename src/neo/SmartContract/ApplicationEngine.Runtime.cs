888d9ae90b0e9a7e22933128371299d2a8b35eb6
Jinghui Liao <jinghui@wayne.edu>
2021-08-15 17:33:26 +0800

    update copyright

--- src/neo/SmartContract/ApplicationEngine.Runtime.cs
+++ src/neo/SmartContract/ApplicationEngine.Runtime.cs
@@ -1,8 +1,8 @@
 // Copyright (C) 2014-2021 NEO GLOBAL DEVELOPMENT.
 // 
 // The neo is free software distributed under the MIT software license, 
-// see the accompanying file LICENSE in the main directory or 
-// the project http://www.opensource.org/licenses/mit-license.php 
+// see the accompanying file LICENSE in the main directory of the
+// project or http://www.opensource.org/licenses/mit-license.php 
 // for more details.
 // 
 // Redistribution and use in source and binary forms with or without
