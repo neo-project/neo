ace0026ddab86271bc3f4ccb137a8873bc039010
Jinghui Liao <jinghui@wayne.edu>
2021-08-15 17:21:55 +0800

    add copyright and license declaration.

--- src/neo/Network/P2P/Connection.cs
+++ src/neo/Network/P2P/Connection.cs
@@ -1,3 +1,13 @@
+// Copyright (C) 2014-2021 NEO GLOBAL DEVELOPMENT.
+// 
+// The neo is free software distributed under the MIT software license, 
+// see the accompanying file LICENSE in the main directory or 
+// the project http://www.opensource.org/licenses/mit-license.php 
+// for more details.
+// 
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
 using Akka.Actor;
 using Akka.IO;
 using System;
