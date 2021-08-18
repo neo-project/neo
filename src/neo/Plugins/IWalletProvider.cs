b71f2314e7412ace2e4be64111e4cb52032c3d9c
Jinghui Liao <jinghui@wayne.edu>
2021-08-18 17:00:09 +0800

    update copyright

--- src/neo/Plugins/IWalletProvider.cs
+++ src/neo/Plugins/IWalletProvider.cs
@@ -1,6 +1,6 @@
-// Copyright (C) 2015-2021 NEO GLOBAL DEVELOPMENT.
+// Copyright (C) 2015-2021 The Neo Project.
 // 
-// The Neo project is free software distributed under the MIT software license, 
+// The neo is free software distributed under the MIT software license, 
 // see the accompanying file LICENSE in the main directory of the
 // project or http://www.opensource.org/licenses/mit-license.php 
 // for more details.
