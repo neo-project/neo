02a40de446177d1c6dfaeeea16b78c99973dc15b
Jinghui Liao <jinghui@wayne.edu>
2021-09-16 08:28:39 -0400

    add base64 base58 to crypto lib

--- src/neo/SmartContract/Native/CryptoLib.cs
+++ src/neo/SmartContract/Native/CryptoLib.cs
@@ -20,6 +20,8 @@ namespace Neo.SmartContract.Native
     /// </summary>
     public sealed class CryptoLib : NativeContract
     {
+        private const int MaxInputLength = 1024;
+
         private static readonly Dictionary<NamedCurve, ECCurve> curves = new()
         {
             [NamedCurve.secp256k1] = ECCurve.Secp256k1,
@@ -69,6 +71,71 @@ namespace Neo.SmartContract.Native
             {
                 return false;
             }
+        }
+        /// <summary>
+        /// Encodes a byte array into a base64 <see cref="string"/>.
+        /// </summary>
+        /// <param name="data">The byte array to be encoded.</param>
+        /// <returns>The encoded <see cref="string"/>.</returns>
+        [ContractMethod(CpuFee = 1 << 5)]
+        public static string Base64Encode([MaxLength(MaxInputLength)] byte[] data)
+        {
+            return Convert.ToBase64String(data);
+        }
+
+        /// <summary>
+        /// Decodes a byte array from a base64 <see cref="string"/>.
+        /// </summary>
+        /// <param name="s">The base64 <see cref="string"/>.</param>
+        /// <returns>The decoded byte array.</returns>
+        [ContractMethod(CpuFee = 1 << 5)]
+        public static byte[] Base64Decode([MaxLength(MaxInputLength)] string s)
+        {
+            return Convert.FromBase64String(s);
+        }
+
+        /// <summary>
+        /// Encodes a byte array into a base58 <see cref="string"/>.
+        /// </summary>
+        /// <param name="data">The byte array to be encoded.</param>
+        /// <returns>The encoded <see cref="string"/>.</returns>
+        [ContractMethod(CpuFee = 1 << 13)]
+        public static string Base58Encode([MaxLength(MaxInputLength)] byte[] data)
+        {
+            return Base58.Encode(data);
+        }
+
+        /// <summary>
+        /// Decodes a byte array from a base58 <see cref="string"/>.
+        /// </summary>
+        /// <param name="s">The base58 <see cref="string"/>.</param>
+        /// <returns>The decoded byte array.</returns>
+        [ContractMethod(CpuFee = 1 << 10)]
+        public static byte[] Base58Decode([MaxLength(MaxInputLength)] string s)
+        {
+            return Base58.Decode(s);
+        }
+
+        /// <summary>
+        /// Converts a byte array to its equivalent <see cref="string"/> representation that is encoded with base-58 digits. The encoded <see cref="string"/> contains the checksum of the binary data.
+        /// </summary>
+        /// <param name="data">The byte array to be encoded.</param>
+        /// <returns>The encoded <see cref="string"/>.</returns>
+        [ContractMethod(CpuFee = 1 << 16)]
+        public static string Base58CheckEncode([MaxLength(MaxInputLength)] byte[] data)
+        {
+            return Base58.Base58CheckEncode(data);
+        }
+
+        /// <summary>
+        /// Converts the specified <see cref="string"/>, which encodes binary data as base-58 digits, to an equivalent byte array. The encoded <see cref="string"/> contains the checksum of the binary data.
+        /// </summary>
+        /// <param name="s">The base58 <see cref="string"/>.</param>
+        /// <returns>The decoded byte array.</returns>
+        [ContractMethod(CpuFee = 1 << 16)]
+        public static byte[] Base58CheckDecode([MaxLength(MaxInputLength)] string s)
+        {
+            return Base58.Base58CheckDecode(s);
         }
     }
 }
