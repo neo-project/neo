a7ff136d01d4050ca43d93e8756bf29e81cffd37
Erik Zhang <erik@neo.org>
2021-11-30 14:48:46 +0800

    Update Signer.cs

--- src/neo/Network/P2P/Payloads/Signer.cs
+++ src/neo/Network/P2P/Payloads/Signer.cs
@@ -140,15 +140,20 @@ namespace Neo.Network.P2P.Payloads
                 writer.Write(Rules);
         }
 
+        /// <summary>
+        /// Converts the signer from a JSON object.
+        /// </summary>
+        /// <param name="json">The signer represented by a JSON object.</param>
+        /// <returns>The converted signer.</returns>
         public static Signer FromJson(JObject json)
         {
             Signer signer = new();
-            signer.Account = UInt160.Parse(json["account"].AsString());
-            signer.Scopes = (WitnessScope)Enum.Parse(typeof(WitnessScope), json["scopes"].AsString());
+            signer.Account = UInt160.Parse(json["account"].GetString());
+            signer.Scopes = Enum.Parse<WitnessScope>(json["scopes"].GetString());
             if (signer.Scopes.HasFlag(WitnessScope.CustomContracts))
-                signer.AllowedContracts = json["allowedcontracts"].GetArray().Select(p => UInt160.Parse(p.AsString())).ToArray();
+                signer.AllowedContracts = json["allowedcontracts"].GetArray().Select(p => UInt160.Parse(p.GetString())).ToArray();
             if (signer.Scopes.HasFlag(WitnessScope.CustomGroups))
-                signer.AllowedGroups = json["allowedgroups"].GetArray().Select(p => ECPoint.Parse(p.AsString(), ECCurve.Secp256r1)).ToArray();
+                signer.AllowedGroups = json["allowedgroups"].GetArray().Select(p => ECPoint.Parse(p.GetString(), ECCurve.Secp256r1)).ToArray();
             if (signer.Scopes.HasFlag(WitnessScope.WitnessRules))
                 signer.Rules = json["rules"].GetArray().Select(p => WitnessRule.FromJson(p)).ToArray();
             return signer;
