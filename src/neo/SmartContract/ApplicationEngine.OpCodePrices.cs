899f8be641a3675d4887f6b5f3e6a3cf73dff9cf
Erik Zhang <erik@neo.org>
2022-05-09 11:42:52 +0800

    Modify the price as suggested by @dusmart.

--- src/neo/SmartContract/ApplicationEngine.OpCodePrices.cs
+++ src/neo/SmartContract/ApplicationEngine.OpCodePrices.cs
@@ -171,7 +171,7 @@ namespace Neo.SmartContract
             [OpCode.MOD] = 1 << 3,
             [OpCode.POW] = 1 << 6,
             [OpCode.SQRT] = 1 << 6,
-            [OpCode.MODMUL] = 1 << 4,
+            [OpCode.MODMUL] = 1 << 5,
             [OpCode.MODPOW] = 1 << 6,
             [OpCode.SHL] = 1 << 3,
             [OpCode.SHR] = 1 << 3,
