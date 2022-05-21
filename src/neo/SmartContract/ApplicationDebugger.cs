df3d3c1bc346613eb9a4375e8cfcc0b328f0f4e9
Hecate2 <2474101468@qq.com>
2022-05-21 13:08:50 +0800

    standalone ApplicationDebugger

---
+++ src/neo/SmartContract/ApplicationDebugger.cs
@@ -0,0 +1,45 @@
+// Copyright (C) 2015-2021 The Neo Project.
+// 
+// The neo is free software distributed under the MIT software license, 
+// see the accompanying file LICENSE in the main directory of the
+// project or http://www.opensource.org/licenses/mit-license.php 
+// for more details.
+// 
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using Neo.Network.P2P.Payloads;
+using Neo.Persistence;
+using Neo.VM;
+
+namespace Neo.SmartContract
+{
+    public class ApplicationDebugger : ApplicationEngine
+    {
+        protected ApplicationDebugger(TriggerType trigger, IVerifiable container, DataCache snapshot, Block persistingBlock, ProtocolSettings settings, long gas, Diagnostic diagnostic) : base(trigger, container, snapshot, persistingBlock, settings, gas, diagnostic)
+        {
+        }
+
+        public static new ApplicationDebugger Create(TriggerType trigger, IVerifiable container, DataCache snapshot, Block persistingBlock = null, ProtocolSettings settings = null, long gas = TestModeGas, Diagnostic diagnostic = null)
+        {
+            return new ApplicationDebugger(trigger, container, snapshot, persistingBlock, settings, gas, diagnostic);
+        }
+
+        public new VMState State
+        {
+            get
+            {
+                return base.State;
+            }
+            set
+            {
+                base.State = value;
+            }
+        }
+
+        public new void ExecuteNext()
+        {
+            base.ExecuteNext();
+        }
+    }
+}
