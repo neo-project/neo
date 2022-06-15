cb0beb32eae1d5b0eba31b525c6afd960129a626
Satoshi Chou <82646971+satoshichou@users.noreply.github.com>
2022-06-15 18:00:26 +0800

    Remove unnecessary default seedlist

--- src/neo/ProtocolSettings.cs
+++ src/neo/ProtocolSettings.cs
@@ -136,14 +136,7 @@ namespace Neo
                 ECPoint.Parse("02cd5a5547119e24feaa7c2a0f37b8c9366216bab7054de0065c9be42084003c8a", ECCurve.Secp256r1)
             },
             ValidatorsCount = 7,
-            SeedList = new[]
-            {
-                "seed1.neo.org:10333",
-                "seed2.neo.org:10333",
-                "seed3.neo.org:10333",
-                "seed4.neo.org:10333",
-                "seed5.neo.org:10333"
-            },
+            SeedList = Array.Empty<string>(),
             MillisecondsPerBlock = 15000,
             MaxTransactionsPerBlock = 512,
             MemoryPoolMaxTransactions = 50_000,
