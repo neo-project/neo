5f9f43fa2340e408053458fbb3f12fb46bb179ac
Jinghui Liao <jinghui@wayne.edu>
2022-06-19 16:37:54 -0400

    fix small issue

--- src/neo/SmartContract/Native/ContractMethodMetadata.cs
+++ src/neo/SmartContract/Native/ContractMethodMetadata.cs
@@ -1,10 +1,10 @@
 // Copyright (C) 2015-2022 The Neo Project.
-// 
-// The neo is free software distributed under the MIT software license, 
+//
+// The neo is free software distributed under the MIT software license,
 // see the accompanying file LICENSE in the main directory of the
-// project or http://www.opensource.org/licenses/mit-license.php 
+// project or http://www.opensource.org/licenses/mit-license.php
 // for more details.
-// 
+//
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
 
@@ -61,7 +61,7 @@ namespace Neo.SmartContract.Native
                 ReturnType = ToParameterType(Handler.ReturnType),
                 Parameters = Parameters.Select(p => new ContractParameterDefinition { Type = ToParameterType(p.Type), Name = p.Name }).ToArray(),
                 Safe = (attribute.RequiredCallFlags & ~CallFlags.ReadOnly) == 0,
-                NonReentry = (attribute.RequiredCallFlags & CallFlags.NonReentry) != 0,
+                NonReentry = (attribute.RequiredCallFlags & CallFlags.NoReentry) != 0
             };
         }
 
