cbec8317ae6ac3647cbba666e6f432c0f4613c4b
doubiliu <liumengyu@ngd.neo.org>
2022-07-19 18:01:06 +0800

    change compress to memorymarshal

--- src/Neo/SmartContract/Native/CryptoLib.BLS12_381.cs
+++ src/Neo/SmartContract/Native/CryptoLib.BLS12_381.cs
@@ -1,14 +1,35 @@
 using Neo.Cryptography.BLS12_381;
+using Neo.VM.Types;
 using System;
+using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 
 namespace Neo.SmartContract.Native;
 
 partial class CryptoLib
 {
-    private const int G1 = 48;
-    private const int G2 = 96;
+    private const int G1 = 104;
+    private const int G2 = 200;
     private const int Gt = 576;
+
+    /// <summary>
+    /// The implementation of System.Crypto.GetPoint.
+    /// Convert data to InteropInterface type.
+    /// </summary>
+    /// <param name="g">G point as byteArray</param>
+    /// <returns></returns>
+    [ContractMethod(CpuFee = 1 << 19)]
+    [RequiresPreviewFeatures]
+    public static InteropInterface Bls12381GetPoint(byte[] g)
+    {
+        return g.Length switch
+        {
+            G1 => new InteropInterface(g),
+            G2 => new InteropInterface(g),
+            Gt => new InteropInterface(g),
+            _ => throw new ArgumentException($"Bls12381 operation fault, type:format, error:valid point length"),
+        };
+    }
 
     /// <summary>
     /// The implementation of System.Crypto.PointAdd.
@@ -19,17 +40,26 @@ partial class CryptoLib
     /// <returns></returns>
     [ContractMethod(CpuFee = 1 << 19)]
     [RequiresPreviewFeatures]
-    public static byte[] Bls12381Add(byte[] g1, byte[] g2)
+    public static InteropInterface Bls12381Add(InteropInterface g1, InteropInterface g2)
     {
-        if (g1.Length != g2.Length)
+        byte[] t1 = g1.GetInterface<byte[]>();
+        byte[] t2 = g2.GetInterface<byte[]>();
+        if (t1.Length != t2.Length)
             throw new ArgumentException($"Bls12381 operation fault, type:format, error:type mismatch");
-        return g1.Length switch
+        switch (t1.Length)
         {
-            G1 => new G1Affine(new G1Projective(G1Affine.FromCompressed(g1)) + new G1Projective(G1Affine.FromCompressed(g2))).ToCompressed(),
-            G2 => new G2Affine(new G2Projective(G2Affine.FromCompressed(g1)) + new G2Projective(G2Affine.FromCompressed(g2))).ToCompressed(),
-            Gt => (Cryptography.BLS12_381.Gt.FromBytes(g1) + Cryptography.BLS12_381.Gt.FromBytes(g2)).ToArray(),
-            _ => throw new ArgumentException($"Bls12381 operation fault, type:format, error:valid point length"),
-        };
+            case G1:
+                var r1 = new G1Affine(new G1Projective(MemoryMarshal.AsRef<G1Affine>(t1)) + new G1Projective(MemoryMarshal.AsRef<G1Affine>(t2)));
+                return new InteropInterface(MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(ref r1, 1)).ToArray());
+            case G2:
+                var r2 = new G2Affine(new G2Projective(MemoryMarshal.AsRef<G2Affine>(t1)) + new G2Projective(MemoryMarshal.AsRef<G2Affine>(t2)));
+                return new InteropInterface(MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(ref r2, 1)).ToArray());
+            case Gt:
+                var r = MemoryMarshal.AsRef<Gt>(t1) + MemoryMarshal.AsRef<Gt>(t2);
+                return new InteropInterface(MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(ref r, 1)).ToArray());
+            default:
+                throw new ArgumentException($"Bls12381 operation fault, type:format, error:valid point length");
+        }
     }
 
     /// <summary>
@@ -41,16 +71,24 @@ partial class CryptoLib
     /// <returns></returns>
     [ContractMethod(CpuFee = 1 << 21)]
     [RequiresPreviewFeatures]
-    public static byte[] Bls12381Mul(byte[] g, long mul)
+    public static InteropInterface Bls12381Mul(InteropInterface g, long mul)
     {
         Scalar X = mul < 0 ? -new Scalar((ulong)-mul) : new Scalar((ulong)mul);
-        return g.Length switch
+        byte[] t = g.GetInterface<byte[]>();
+        switch (t.Length)
         {
-            G1 => new G1Affine(G1Affine.FromCompressed(g) * X).ToCompressed(),
-            G2 => new G2Affine(G2Affine.FromCompressed(g) * X).ToCompressed(),
-            Gt => (Cryptography.BLS12_381.Gt.FromBytes(g) * X).ToArray(),
-            _ => throw new ArgumentException($"Bls12381 operation fault, type:format, error:valid point length"),
-        };
+            case G1:
+                var r1 = MemoryMarshal.AsRef<G1Affine>(t) * X;
+                return new InteropInterface(MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(ref r1, 1)).ToArray());
+            case G2:
+                var r2 = MemoryMarshal.AsRef<G2Affine>(t) * X;
+                return new InteropInterface(MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(ref r2, 1)).ToArray());
+            case Gt:
+                var r = MemoryMarshal.AsRef<Gt>(t) * X;
+                return new InteropInterface(MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(ref r, 1)).ToArray());
+            default:
+                throw new ArgumentException($"Bls12381 operation fault, type:format, error:valid point length");
+        }
     }
 
     /// <summary>
@@ -62,10 +100,13 @@ partial class CryptoLib
     /// <returns></returns>
     [ContractMethod(CpuFee = 1 << 23)]
     [RequiresPreviewFeatures]
-    public static byte[] Bls12381Pairing(byte[] g1, byte[] g2)
+    public static InteropInterface Bls12381Pairing(InteropInterface g1, InteropInterface g2)
     {
-        if (g1.Length != G1 || g2.Length != G2)
+        byte[] t1 = g1.GetInterface<byte[]>();
+        byte[] t2 = g2.GetInterface<byte[]>();
+        if (t1.Length != G1 || t2.Length != G2)
             throw new ArgumentException($"Bls12381 operation fault, type:format, error:type mismatch");
-        return Bls12.Pairing(G1Affine.FromCompressed(g1), G2Affine.FromCompressed(g2)).ToArray();
+        var r = Bls12.Pairing(MemoryMarshal.AsRef<G1Affine>(t1), MemoryMarshal.AsRef<G2Affine>(t2));
+        return new InteropInterface(MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(ref r, 1)).ToArray());
     }
 }
