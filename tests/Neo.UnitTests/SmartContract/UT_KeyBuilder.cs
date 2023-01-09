a494c2a6ef2db9265f760717b4b87ec2ed919889
Shargon <shargon@gmail.com>
2023-01-09 01:44:37 -0800

    Change value too

--- tests/Neo.UnitTests/SmartContract/UT_KeyBuilder.cs
+++ tests/Neo.UnitTests/SmartContract/UT_KeyBuilder.cs
@@ -8,7 +8,7 @@ namespace Neo.UnitTests.SmartContract
     {
         private struct TestKey
         {
-            public int X;
+            public int Value;
         }
 
         [TestMethod]
@@ -28,11 +28,11 @@ namespace Neo.UnitTests.SmartContract
             Assert.AreEqual("010000000203040000000000000000000000000000000000000000", key.ToArray().ToHexString());
 
             key = new KeyBuilder(1, 2);
-            key = key.Add(new TestKey { X = 123 });
+            key = key.Add(new TestKey { Value = 123 });
             Assert.AreEqual("01000000027b000000", key.ToArray().ToHexString());
 
             key = new KeyBuilder(1, 0);
-            key = key.AddBigEndian(new TestKey { X = 1 });
+            key = key.AddBigEndian(new TestKey { Value = 1 });
             Assert.AreEqual("010000000000000001", key.ToArray().ToHexString());
         }
     }
