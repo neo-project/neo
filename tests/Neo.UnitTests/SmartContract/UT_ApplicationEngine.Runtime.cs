773b96a6953063dc04a095938d2d908e2501df92
Erik Zhang <erik@neo.org>
2023-08-18 17:24:57 +0800

    Check ABI for notifications (#2810)
    * Check ABI for notifications
    
    * Patch checkabi (#2884)
    
    * Remove extra space (#2878)
    
    * Add Hardfork for StrictMode (#2881)
    
    * init
    
    * rename
    
    * hardfork
    
    * Update ApplicationEngine.Runtime.cs
    
    ---------
    
    Co-authored-by: Shargon <shargon@gmail.com>
    
    * Check UTF8 string
    
    * Cleaner way
    
    * Fix Buffer
    
    * Update src/Neo/SmartContract/ApplicationEngine.Runtime.cs
    
    Co-authored-by: Anna Shaleva <anna@nspcc.ru>
    
    ---------
    
    Co-authored-by: Shargon <shargon@gmail.com>
    Co-authored-by: Vitor Nazário Coelho <vncoelho@gmail.com>
    Co-authored-by: Shine Li <bfshm@qq.com>
    Co-authored-by: Jimmy <jinghui@wayne.edu>
    Co-authored-by: Anna Shaleva <anna@nspcc.ru>

--- tests/Neo.UnitTests/SmartContract/UT_ApplicationEngine.Runtime.cs
+++ tests/Neo.UnitTests/SmartContract/UT_ApplicationEngine.Runtime.cs
@@ -2,8 +2,10 @@ using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.Network.P2P.Payloads;
 using Neo.SmartContract;
+using Neo.SmartContract.Manifest;
 using System;
 using System.Numerics;
+using System.Text;
 
 namespace Neo.UnitTests.SmartContract
 {
@@ -24,21 +26,44 @@ namespace Neo.UnitTests.SmartContract
         {
             using var engine = ApplicationEngine.Create(TriggerType.Application, null, null, TestBlockchain.TheNeoSystem.GenesisBlock, settings: TestBlockchain.TheNeoSystem.Settings, gas: 1100_00000000);
             engine.LoadScript(Array.Empty<byte>());
-            engine.CurrentContext.GetState<ExecutionContextState>().Contract = new();
+            engine.CurrentContext.GetState<ExecutionContextState>().Contract = new()
+            {
+                Manifest = new()
+                {
+                    Abi = new()
+                    {
+                        Events = new[]
+                        {
+                            new ContractEventDescriptor
+                            {
+                                Name = "e1",
+                                Parameters = new[]
+                                {
+                                    new ContractParameterDefinition
+                                    {
+                                        Type = ContractParameterType.Array
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
 
             // circular
 
             VM.Types.Array array = new();
             array.Add(array);
 
-            Assert.ThrowsException<NotSupportedException>(() => engine.RuntimeNotify(new byte[] { 0x01 }, array));
+            Assert.ThrowsException<NotSupportedException>(() => engine.RuntimeNotify(Encoding.ASCII.GetBytes("e1"), array));
 
             // Buffer
 
             array.Clear();
             array.Add(new VM.Types.Buffer(1));
+            engine.CurrentContext.GetState<ExecutionContextState>().Contract.Manifest.Abi.Events[0].Parameters[0].Type = ContractParameterType.ByteArray;
 
-            engine.RuntimeNotify(new byte[] { 0x01 }, array);
+            engine.RuntimeNotify(Encoding.ASCII.GetBytes("e1"), array);
             engine.Notifications[0].State[0].Type.Should().Be(VM.Types.StackItemType.ByteString);
 
             // Pointer
@@ -46,14 +71,15 @@ namespace Neo.UnitTests.SmartContract
             array.Clear();
             array.Add(new VM.Types.Pointer(Array.Empty<byte>(), 1));
 
-            Assert.ThrowsException<NotSupportedException>(() => engine.RuntimeNotify(new byte[] { 0x01 }, array));
+            Assert.ThrowsException<InvalidOperationException>(() => engine.RuntimeNotify(Encoding.ASCII.GetBytes("e1"), array));
 
             // InteropInterface
 
             array.Clear();
             array.Add(new VM.Types.InteropInterface(new object()));
+            engine.CurrentContext.GetState<ExecutionContextState>().Contract.Manifest.Abi.Events[0].Parameters[0].Type = ContractParameterType.InteropInterface;
 
-            Assert.ThrowsException<NotSupportedException>(() => engine.RuntimeNotify(new byte[] { 0x01 }, array));
+            Assert.ThrowsException<NotSupportedException>(() => engine.RuntimeNotify(Encoding.ASCII.GetBytes("e1"), array));
         }
 
         [TestMethod]
