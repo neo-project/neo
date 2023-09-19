841b695176f548ad3ba7108e02d30c410badbf20
Fernando Diaz Toledano <shargon@gmail.com>
2023-09-19 16:08:41 +0200

    Fix VerifyStateDependent

--- src/Neo/Network/P2P/Payloads/Transaction.cs
+++ src/Neo/Network/P2P/Payloads/Transaction.cs
@@ -365,10 +365,13 @@ namespace Neo.Network.P2P.Payloads
                 if (NativeContract.Policy.IsBlocked(snapshot, hash))
                     return VerifyResult.PolicyFail;
             if (!(context?.CheckTransaction(this, conflictsList, snapshot) ?? true)) return VerifyResult.InsufficientFunds;
+            long attributesFee = 0;
             foreach (TransactionAttribute attribute in Attributes)
                 if (!attribute.Verify(snapshot, this))
                     return VerifyResult.InvalidAttribute;
-            long net_fee = NetworkFee - Size * NativeContract.Policy.GetFeePerByte(snapshot);
+                else
+                    attributesFee += attribute.CalculateNetworkFee(snapshot, this);
+            long net_fee = NetworkFee - (Size * NativeContract.Policy.GetFeePerByte(snapshot)) - attributesFee;
             if (net_fee < 0) return VerifyResult.InsufficientFunds;
 
             if (net_fee > MaxVerificationGas) net_fee = MaxVerificationGas;
