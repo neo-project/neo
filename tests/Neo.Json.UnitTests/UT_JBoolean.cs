756fd8eb8f560bd2e60ae9c5303e967c12c4f61b
Jinghui Liao <jimmy@r3e.network>
2023-11-25 09:25:49 +0800

    behavior of jboolean is kinda weird. i have:
    Assert.AreEqual failed. Expected:<true>. Actual:<true>.

--- tests/Neo.Json.UnitTests/UT_JBoolean.cs
+++ tests/Neo.Json.UnitTests/UT_JBoolean.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 namespace Neo.Json.UnitTests
 {
     [TestClass]
@@ -19,5 +20,61 @@ namespace Neo.Json.UnitTests
             jFalse.AsNumber().Should().Be(0);
             jTrue.AsNumber().Should().Be(1);
         }
+        [TestMethod]
+        public void TestDefaultConstructor()
+        {
+            var defaultJBoolean = new JBoolean();
+            defaultJBoolean.AsNumber().Should().Be(0);
+        }
+
+        [TestMethod]
+        public void TestExplicitFalse()
+        {
+            var explicitFalse = new JBoolean(false);
+            explicitFalse.AsNumber().Should().Be(0);
+        }
+
+        [TestMethod]
+        public void TestNullJBoolean()
+        {
+            JBoolean nullJBoolean = null;
+            Assert.ThrowsException<NullReferenceException>(() => nullJBoolean.AsNumber());
+        }
+
+        [TestMethod]
+        public void TestConversionToOtherTypes()
+        {
+            Assert.AreEqual("true", jTrue.ToString());
+            Assert.AreEqual("false", jFalse.ToString());
+        }
+
+        [TestMethod]
+        public void TestComparisonsWithOtherBooleans()
+        {
+            Assert.IsTrue(jTrue.Equals(new JBoolean(true)));
+            Assert.IsFalse(jFalse.Equals(new JBoolean()));
+        }
+
+        // [TestMethod]
+        // public void TestTypeCasting()
+        // {
+        //     bool castedValue = (bool)jTrue;
+        //     Assert.AreEqual(true, castedValue);
+        // }
+
+        [TestMethod]
+        public void TestSerializationAndDeserialization()
+        {
+            string serialized = JsonConvert.SerializeObject(jTrue);
+            var deserialized = JsonConvert.DeserializeObject<JBoolean>(serialized);
+            Assert.AreEqual(jTrue, deserialized);
+        }
+
+        // [TestMethod]
+        // public void TestInteractionWithLogicalOperators()
+        // {
+        //     var result = jTrue && new JBoolean(false);
+        //     Assert.IsFalse(result.Value);
+        // }
     }
 }
