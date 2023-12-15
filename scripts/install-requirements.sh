9c67603353d9bbcba668a1473a2453f0ae1d2640
Jinghui Liao <jimmy@r3e.network>
2023-12-15 21:10:34 +0800

    add opensuse and install other tools

--- scripts/install-requirements.sh
+++ scripts/install-requirements.sh
@@ -5,19 +5,25 @@ OS="$(uname -s)"
 DISTRO=""
 
 # Define installation functions
-install_leveldb_ubuntu() {
-    echo "Installing LevelDB on Ubuntu..."
-    sudo apt-get update && sudo apt-get install -y libleveldb-dev
+install_requirements_ubuntu() {
+    echo "Installing requirements and dependencies on Ubuntu..."
+    sudo apt-get update
+    sudo apt-get install -y libleveldb-dev sqlite3 libsqlite3-dev libunwind8-dev
 }
 
-install_leveldb_fedora() {
-    echo "Installing LevelDB on Fedora..."
-    sudo dnf install -y leveldb-devel
+install_requirements_fedora() {
+    echo "Installing requirements and dependencies on Fedora..."
+    sudo dnf install -y leveldb-devel sqlite sqlite-devel libunwind-devel
 }
 
-install_leveldb_macos() {
-    echo "Installing LevelDB on macOS..."
-    brew install leveldb
+install_requirements_opensuse() {
+    echo "Installing requirements and dependencies on openSUSE..."
+    sudo zypper install -y libleveldb-devel sqlite3 libsqlite3-devel libunwind-devel
+}
+
+install_requirements_macos() {
+    echo "Installing requirements and dependencies on macOS..."
+    brew install leveldb sqlite3 libunwind
 }
 
 # Detect Linux distribution
@@ -34,10 +40,13 @@ case $OS in
         detect_linux_distro
         case $DISTRO in
             ubuntu)
-                install_leveldb_ubuntu
+                install_requirements_ubuntu
                 ;;
             fedora)
-                install_leveldb_fedora
+                install_requirements_fedora
+                ;;
+            opensuse*|suse|sles)
+                install_requirements_opensuse
                 ;;
             *)
                 echo "Unsupported Linux distribution: $DISTRO"
@@ -46,7 +55,7 @@ case $OS in
         esac
         ;;
     Darwin*)
-        install_leveldb_macos
+        install_requirements_macos
         ;;
     *)
         echo "Unsupported operating system: $OS"
@@ -54,4 +63,4 @@ case $OS in
         ;;
 esac
 
-echo "LevelDB installation completed"
+echo "Requirements and dependencies installation completed"
