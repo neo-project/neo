4171bc053544d53a39e17152a35188bdb5ea6921
Shargon <shargon@gmail.com>
2024-01-02 04:59:02 -0800

    Update CryptoLib.cs

--- src/Neo/SmartContract/Native/CryptoLib.cs
+++ src/Neo/SmartContract/Native/CryptoLib.cs
@@ -1,4 +1,4 @@
-// Copyright right (C) 2015-2024 The Neo Project.
+// Copyright (C) 2015-2024 The Neo Project.
 //
 // CryptoLib.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
