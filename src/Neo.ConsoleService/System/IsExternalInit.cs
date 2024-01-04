59960f9e72881059b3e12fc20701a77d7f09e467
Christopher R Schuchardt <cschuchardt88@gmail.com>
2024-01-04 05:33:46 -0500

    Fixed header

--- src/Neo.ConsoleService/System/IsExternalInit.cs
+++ src/Neo.ConsoleService/System/IsExternalInit.cs
@@ -1,9 +1,13 @@
-// Copyright (C) 2023 Christopher R Schuchardt
+// Copyright (C) 2015-2024 The Neo Project.
 //
-// File "IsExternalInit.cs" is part of free software
-// distributed under the GPLv3 software license, see
-// the accompanying file "LICENSE" in the main directory
-// of the project for more details.
+// IsExternalInit.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
 
 namespace System.Runtime.CompilerServices
 {
