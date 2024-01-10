97c827b5574885c9329785c3bf0746c36384d5a7
Christopher R Schuchardt <cschuchardt88@gmail.com>
2024-01-10 01:30:53 -0500

    Fixed build dpkg script and add codesign for macos

---
+++ .github/scripts/neo-cli-csharp/build-dpkg.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Copyright (C) 2015-2024 The Neo Project.
+#
+# build-dpkg-amd64.sh file belongs to the neo project and is free
+# software distributed under the MIT software license, see the
+# accompanying file LICENSE in the main directory of the
+# repository or http://www.opensource.org/licenses/mit-license.php
+# for more details.
+#
+# Redistribution and use in source and binary forms with or without
+# modifications are permitted.
+
+# Set variables
+ARCH=${1};
+PKGS_PATH="${DPKGS_PATH}/neo-cli-${OS_RUNTIME}_${NEO_VERSION}-1_${ARCH}";
+TEMPLATE_PATH="./dpkg";
+
+# Make directory
+mkdir -p ${PKGS_PATH};
+
+# Copy workflow deb template
+cp -vpR ${TEMPLATE_PATH}/* ${PKGS_PATH}/;
+status=${?}
+
+if [ ${status} -ne 0 ]; then
+  exit ${status};
+fi
+
+# Create "bin" Path
+mkdir -vp ${PKGS_PATH}/usr/bin;
+
+# Change permissions
+chmod -R 755 ${PKGS_PATH}/;
+chmod -R a+rw ${PKGS_PATH}/srv/neo-node/dbs;
+chmod -R a+rw ${PKGS_PATH}/srv/neo-node/Plugins;
+
+# Clean up ".gitkeep"
+rm -v ${PKGS_PATH}/srv/neo-node/dbs/.gitkeep;
+rm -v ${PKGS_PATH}/srv/neo-node/Plugins/.gitkeep;
+
+# Append deb package config
+echo -e "Version: ${NEO_VERSION}" >> ${PKGS_PATH}/DEBIAN/control;
+echo -e "Architecture: ${ARCH}" >> ${PKGS_PATH}/DEBIAN/control;
+
+# Create shortcut to "neo-cli" binary file
+ln -sv /srv/neo-node/neo-cli ${PKGS_PATH}/usr/bin/neo-cli;
+
+# Build "deb" package
+dpkg-deb --build ${PKGS_PATH};
+status=${?};
+
+if [ ${status} -eq 0 ]; then
+  # Debug Output
+  dpkg-deb --info "${PKGS_PATH}.deb";
+fi
+
+exit ${status};
