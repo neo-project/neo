1dc177d9639d3f7a80e82114e581f825d5cafe47
Fernando Diaz Toledano <shargon@gmail.com>
2024-02-08 18:25:17 +0100

    LoadFromJson

--- src/Neo/Persistence/IStore.cs
+++ src/Neo/Persistence/IStore.cs
@@ -9,7 +9,9 @@
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
 
+using Neo.Json;
 using System;
+using System.Linq;
 
 namespace Neo.Persistence
 {
@@ -43,5 +45,40 @@ namespace Neo.Persistence
         /// <param name="key">The key of the entry.</param>
         /// <param name="value">The data of the entry.</param>
         void PutSync(byte[] key, byte[] value) => Put(key, value);
+
+        /// <summary>
+        /// Load data from json
+        ///
+        /// Expected data (in base64):
+        /// 
+        /// - "key":"value"
+        /// - "prefix": { "key":"value" }
+        /// </summary>
+        /// <param name="json">Json Object</param>
+        public void LoadFromJson(JObject json)
+        {
+            foreach (var entry in json.Properties)
+            {
+                if (entry.Value is JString str)
+                {
+                    // "key":"value" in base64
+
+                    Put(Convert.FromBase64String(entry.Key), Convert.FromBase64String(str.Value));
+                }
+                else if (entry.Value is JObject obj)
+                {
+                    // "prefix": { "key":"value" }  in base64
+
+                    foreach (var subEntry in obj.Properties)
+                    {
+                        if (subEntry.Value is JString subStr)
+                        {
+                            Put(Convert.FromBase64String(entry.Key).Concat(Convert.FromBase64String(subEntry.Key)).ToArray(),
+                                Convert.FromBase64String(subStr.Value));
+                        }
+                    }
+                }
+            }
+        }
     }
 }
---
+++ tests/Neo.UnitTests/Persistence/UT_IStore.cs
@@ -0,0 +1,62 @@
+// Copyright (C) 2015-2024 The Neo Project.
+//
+// UT_IStore.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Json;
+using Neo.Persistence;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace Neo.UnitTests.Persistence
+{
+    [TestClass]
+    public class UT_IStore
+    {
+        [TestMethod]
+        public void LoadFromJsonTest()
+        {
+            using IStore store = new MemoryStore();
+
+            // empty
+
+            var entries = store.Seek(Array.Empty<byte>(), SeekDirection.Forward).ToArray();
+            Assert.AreEqual(entries.Length, 0);
+
+            // simple object
+
+            var json = @"{""a2V5"":""dmFsdWU=""}";
+
+            store.LoadFromJson((JObject)JToken.Parse(json));
+
+            entries = store.Seek(Array.Empty<byte>(), SeekDirection.Forward).ToArray();
+            Assert.AreEqual(entries.Length, 1);
+
+            Assert.AreEqual("key", Encoding.ASCII.GetString(entries[0].Key));
+            Assert.AreEqual("value", Encoding.ASCII.GetString(entries[0].Value));
+
+            // prefix object
+
+            json = @"{""bXkt"":{""a2V5"":""bXktdmFsdWU=""}}";
+
+            store.LoadFromJson((JObject)JToken.Parse(json));
+
+            entries = store.Seek(Array.Empty<byte>(), SeekDirection.Forward).ToArray();
+            Assert.AreEqual(entries.Length, 2);
+
+            Assert.AreEqual("key", Encoding.ASCII.GetString(entries[0].Key));
+            Assert.AreEqual("value", Encoding.ASCII.GetString(entries[0].Value));
+
+            Assert.AreEqual("my-key", Encoding.ASCII.GetString(entries[1].Key));
+            Assert.AreEqual("my-value", Encoding.ASCII.GetString(entries[1].Value));
+        }
+    }
+}
