25b08b08ce732fbe4039328e124bdc34e34fff6a
Shargon <shargon@gmail.com>
2024-04-11 07:33:25 -0700

    Clean usings (#3193)

--- src/Neo.VM/JumpTable/JumpTable.Slot.cs
+++ src/Neo.VM/JumpTable/JumpTable.Slot.cs
@@ -9,7 +9,6 @@
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
 
-using Neo.VM;
 using Neo.VM.Types;
 using System;
 using System.Runtime.CompilerServices;
