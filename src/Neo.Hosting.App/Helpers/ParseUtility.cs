c136dc0141c1869a3d30a861b2c4afb406eaa2ef
Christopher Schuchardt <cschuchardt88@gmail.com>
2024-04-22 02:38:19 -0400

    dotnet format

--- src/Neo.Hosting.App/Helpers/ParseUtility.cs
+++ src/Neo.Hosting.App/Helpers/ParseUtility.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2024 The Neo Project.
 //
-// ParseUtilities.cs file belongs to the neo project and is free
+// ParseUtility.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
 // repository or http://www.opensource.org/licenses/mit-license.php
