0b2c834a0c45b7faf0c7f579f2ace6dc7a91ae55
Christopher Schuchardt <cschuchardt88@gmail.com>
2024-05-29 06:55:15 -0400

    Fixed file headers

--- src/Neo.Hosting.App/NamedPipes/Protocol/Payloads/PipeVersionPayload.cs
+++ src/Neo.Hosting.App/NamedPipes/Protocol/Payloads/PipeVersionPayload.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2024 The Neo Project.
 //
-// PipeVersion.cs file belongs to the neo project and is free
+// PipeVersionPayload.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
 // repository or http://www.opensource.org/licenses/mit-license.php
--- tests/Neo.Hosting.App.Tests/NamedPipes/Protocol/Payloads/UT_PipeSerializablePayload.cs
+++ tests/Neo.Hosting.App.Tests/NamedPipes/Protocol/Payloads/UT_PipeSerializablePayload.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2024 The Neo Project.
 //
-// UT_PipeBlock.cs file belongs to the neo project and is free
+// UT_PipeSerializablePayload.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
 // repository or http://www.opensource.org/licenses/mit-license.php
