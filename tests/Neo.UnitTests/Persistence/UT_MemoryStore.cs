dfb0a5a768146f4c6470d5c70369e97ae4178014
Jimmy <jimmy@r3e.network>
2024-06-01 23:36:44 +0800

    fix memstore commit issue

--- tests/Neo.UnitTests/Persistence/UT_MemoryStore.cs
+++ tests/Neo.UnitTests/Persistence/UT_MemoryStore.cs
@@ -11,8 +11,10 @@
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.Persistence;
+using Neo.SmartContract;
 using System;
 using System.Linq;
+using System.Text;
 
 namespace Neo.UnitTests.Persistence
 {
@@ -30,27 +32,42 @@ namespace Neo.UnitTests.Persistence
         {
             using var store = new MemoryStore();
 
-            store.Delete(new byte[] { 1 });
-            Assert.AreEqual(null, store.TryGet(new byte[] { 1 }));
-            store.Put(new byte[] { 1 }, new byte[] { 1, 2, 3 });
-            CollectionAssert.AreEqual(new byte[] { 1, 2, 3 }, store.TryGet(new byte[] { 1 }));
+            store.Delete([1]);
+            Assert.AreEqual(null, store.TryGet([1]));
+            store.Put([1], [1, 2, 3]);
+            CollectionAssert.AreEqual(new byte[] { 1, 2, 3 }, store.TryGet([1]));
 
-            store.Put(new byte[] { 2 }, new byte[] { 4, 5, 6 });
-            CollectionAssert.AreEqual(new byte[] { 1 }, store.Seek(Array.Empty<byte>(), SeekDirection.Forward).Select(u => u.Key).First());
-            CollectionAssert.AreEqual(new byte[] { 2 }, store.Seek(new byte[] { 2 }, SeekDirection.Backward).Select(u => u.Key).First());
-            CollectionAssert.AreEqual(new byte[] { 1 }, store.Seek(new byte[] { 1 }, SeekDirection.Backward).Select(u => u.Key).First());
+            store.Put([2], [4, 5, 6]);
+            CollectionAssert.AreEqual(new byte[] { 1 }, store.Seek(Array.Empty<byte>()).Select(u => u.Key).First());
+            CollectionAssert.AreEqual(new byte[] { 2 }, store.Seek([2], SeekDirection.Backward).Select(u => u.Key).First());
+            CollectionAssert.AreEqual(new byte[] { 1 }, store.Seek([1], SeekDirection.Backward).Select(u => u.Key).First());
 
-            store.Delete(new byte[] { 1 });
-            store.Delete(new byte[] { 2 });
+            store.Delete([1]);
+            store.Delete([2]);
 
-            store.Put(new byte[] { 0x00, 0x00, 0x00 }, new byte[] { 0x00 });
-            store.Put(new byte[] { 0x00, 0x00, 0x01 }, new byte[] { 0x01 });
-            store.Put(new byte[] { 0x00, 0x00, 0x02 }, new byte[] { 0x02 });
-            store.Put(new byte[] { 0x00, 0x00, 0x03 }, new byte[] { 0x03 });
-            store.Put(new byte[] { 0x00, 0x00, 0x04 }, new byte[] { 0x04 });
+            store.Put([0x00, 0x00, 0x00], [0x00]);
+            store.Put([0x00, 0x00, 0x01], [0x01]);
+            store.Put([0x00, 0x00, 0x02], [0x02]);
+            store.Put([0x00, 0x00, 0x03], [0x03]);
+            store.Put([0x00, 0x00, 0x04], [0x04]);
 
             var entries = store.Seek(Array.Empty<byte>(), SeekDirection.Backward).ToArray();
-            Assert.AreEqual(entries.Count(), 0);
+            Assert.AreEqual(entries.Length, 0);
         }
+
+        [TestMethod]
+        public void NeoSystemTest()
+        {
+            var neoSystem = new NeoSystem(TestProtocolSettings.Default, new MemoryStoreProvider());
+            Assert.IsNotNull(neoSystem.StoreView);
+            var snapshot = neoSystem.StoreView;
+            var key = new StorageKey( Encoding.UTF8.GetBytes("testKey"));
+            var value =new StorageItem( Encoding.UTF8.GetBytes("testValue"));
+            snapshot.Add(key, value);
+            snapshot.Commit();
+            var result = snapshot.TryGet(key);
+            Assert.AreEqual("testValue", Encoding.UTF8.GetString(result.Value.ToArray()));
+        }
+
     }
 }
