7bf2546fdac3eab1360f8d2745a5f1cee56793ad
Christopher Schuchardt <cschuchardt88@gmail.com>
2024-06-01 11:31:13 -0400

    Add more command for `ContractCommand`

---
+++ src/Neo.Hosting.App/CommandLine/Prompt/ContractCommand.cs
@@ -0,0 +1,29 @@
+// Copyright (C) 2015-2024 The Neo Project.
+//
+// ContractCommand.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using System.CommandLine;
+
+namespace Neo.Hosting.App.CommandLine.Prompt
+{
+    internal sealed partial class ContractCommand : Command
+    {
+        public ContractCommand() : base("contract", "Invoke, destroy, update or deploy contact(s)")
+        {
+            var deployCommand = new DeployCommand();
+            var updateCommand = new UpdateCommand();
+            var invokeCommand = new InvokeCommand();
+
+            AddCommand(deployCommand);
+            AddCommand(updateCommand);
+            AddCommand(invokeCommand);
+        }
+    }
+}
