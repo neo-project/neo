0a8f39cf02a7906641b118675f0aa85dc117326f
Christopher Schuchardt <cschuchardt88@gmail.com>
2024-06-07 22:47:31 -0400

    Added `CandidateCommands`

---
+++ src/Neo.Hosting.App/CommandLine/Prompt/CandidateCommand.Register.cs
@@ -0,0 +1,47 @@
+// Copyright (C) 2015-2024 The Neo Project.
+//
+// CandidateCommand.Register.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+
+namespace Neo.Hosting.App.CommandLine.Prompt
+{
+    internal partial class CandidateCommand
+    {
+        internal sealed class RegisterCommand : Command
+        {
+            public RegisterCommand() : base("register", "Register to become a candidate")
+            {
+                var accountArgument = new Argument<string>("ACCOUNT", "160-bit hash (hex)");
+
+                AddArgument(accountArgument);
+
+                this.SetHandler(async context => await new Handler().InvokeAsync(context));
+            }
+
+            internal sealed new class Handler : ICommandHandler
+            {
+                public Task<int> InvokeAsync(InvocationContext context)
+                {
+                    return Task.FromResult(0);
+                }
+
+                public int Invoke(InvocationContext context)
+                {
+                    throw new NotImplementedException();
+                }
+
+            }
+        }
+    }
+}
