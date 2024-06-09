f89c3c1fd5480a10ad2dd0207d32bf18161ed661
Jimmy <jimmy@r3e.network>
2024-06-10 02:28:18 +0800

    format

--- src/Plugins/RollbackService/RollbackPlugin.cs
+++ src/Plugins/RollbackService/RollbackPlugin.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2024 The Neo Project.
 //
-// RpcServerPlugin.cs file belongs to the neo project and is free
+// RollbackPlugin.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
 // repository or http://www.opensource.org/licenses/mit-license.php
