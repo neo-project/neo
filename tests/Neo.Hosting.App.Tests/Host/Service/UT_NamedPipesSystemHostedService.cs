9e7983377febbe08afc3b2ccf3176ffb25919e14
Christopher Schuchardt <cschuchardt88@gmail.com>
2024-06-09 20:36:11 -0400

    Added `Block` and `Transaction` Tests

--- tests/Neo.Hosting.App.Tests/Host/Service/UT_NamedPipesSystemHostedService.cs
+++ tests/Neo.Hosting.App.Tests/Host/Service/UT_NamedPipesSystemHostedService.cs
@@ -22,6 +22,7 @@ using Neo.Hosting.App.NamedPipes.Protocol.Messages;
 using Neo.Hosting.App.NamedPipes.Protocol.Payloads;
 using Neo.Hosting.App.Tests.UTHelpers.Default;
 using Neo.Hosting.App.Tests.UTHelpers.SetupClasses;
+using Neo.Network.P2P.Payloads;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -58,7 +59,7 @@ namespace Neo.Hosting.App.Tests.Host.Service
             await pipeService.StartAsync(default).DefaultTimeout();
 
             var client = NamedPipeFactory.CreateClientStream(pipeService.LocalEndPoint);
-            var message = PipeMessage.Create(1, PipeCommand.GetVersion, PipeMessage.Null);
+            var message = PipeMessage.Create(Random.Shared.Next(), PipeCommand.GetVersion, PipeMessage.Null);
 
             await client.ConnectAsync().DefaultTimeout();
             await client.WriteAsync(message.ToArray()).DefaultTimeout();
@@ -74,6 +75,61 @@ namespace Neo.Hosting.App.Tests.Host.Service
 
             Assert.True(count <= buffer.Length);
             Assert.Equal(PipeCommand.Version, message.Command);
+        }
+
+        [Fact]
+        public async Task Test_MessageProcess_GetBlock_From_Server()
+        {
+            await using var connectionListener = NamedPipeFactory.CreateListener(NamedPipeFactory.GetUniquePipeName(), loggerFactory: LoggerFactory);
+            using var pipeService = new NamedPipeSystemHostedService(_neoSystemService, connectionListener, loggerFactory: LoggerFactory);
+            await pipeService.StartAsync(default).DefaultTimeout();
+
+            var client = NamedPipeFactory.CreateClientStream(pipeService.LocalEndPoint);
+            var payloadRequest = new PipeUnmanagedPayload<uint> { Value = 0 };
+            var message = PipeMessage.Create(Random.Shared.Next(), PipeCommand.GetBlock, payloadRequest);
+
+            await client.ConnectAsync().DefaultTimeout();
+            await client.WriteAsync(message.ToArray()).DefaultTimeout();
+
+            var buffer = new byte[4096];
+            var count = await client.ReadAsync(buffer.AsMemory()).DefaultTimeout();
+
+            await pipeService.StopAsync(default).DefaultTimeout();
+
+            message.FromArray(buffer);
+
+            var payloadResponse = message.Payload as PipeSerializablePayload<Block>;
+
+            Assert.True(count <= buffer.Length);
+            Assert.Equal(PipeCommand.Block, message.Command);
+        }
+
+        [Fact]
+        public async Task Test_MessageProcess_GetTransaction_From_Server()
+        {
+            await using var connectionListener = NamedPipeFactory.CreateListener(NamedPipeFactory.GetUniquePipeName(), loggerFactory: LoggerFactory);
+            using var pipeService = new NamedPipeSystemHostedService(_neoSystemService, connectionListener, loggerFactory: LoggerFactory);
+            await pipeService.StartAsync(default).DefaultTimeout();
+
+            var client = NamedPipeFactory.CreateClientStream(pipeService.LocalEndPoint);
+            var payloadRequest = new PipeSerializablePayload<UInt256> { Value = UInt256.Zero };
+            var message = PipeMessage.Create(Random.Shared.Next(), PipeCommand.GetTransaction, payloadRequest);
+
+            await client.ConnectAsync().DefaultTimeout();
+            await client.WriteAsync(message.ToArray()).DefaultTimeout();
+
+            var buffer = new byte[4096];
+            var count = await client.ReadAsync(buffer.AsMemory()).DefaultTimeout();
+
+            await pipeService.StopAsync(default).DefaultTimeout();
+
+            message.FromArray(buffer);
+
+            var payloadResponse = message.Payload as PipeSerializablePayload<Transaction>;
+
+            Assert.True(count <= buffer.Length);
+            Assert.Equal(PipeCommand.Transaction, message.Command);
+            Assert.Null(payloadResponse.Value); // no transaction
         }
     }
 }
