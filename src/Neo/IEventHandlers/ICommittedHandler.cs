a8d54b4db8a5fd7e646661a9348a08c13e3929ed
Jimmy <jimmy@r3e.network>
2024-06-13 04:32:05 +0300

    add missing using

--- src/Neo/IEventHandlers/ICommittedHandler.cs
+++ src/Neo/IEventHandlers/ICommittedHandler.cs
@@ -9,6 +9,7 @@
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
 
+using Neo.Ledger;
 using Neo.Network.P2P.Payloads;
 
 namespace Neo.IEventHandlers;
