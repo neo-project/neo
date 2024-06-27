ebc93779df1e71e97bbb813c3a29c8a7f6de1b5b
Jimmy <jimmy@r3e.network>
2024-06-28 02:27:49 +0800

    fix test setting

--- tests/Neo.UnitTests/TestProtocolSettings.cs
+++ tests/Neo.UnitTests/TestProtocolSettings.cs
@@ -59,6 +59,7 @@ namespace Neo.UnitTests
             MemoryPoolMaxTransactions = ProtocolSettings.Default.MemoryPoolMaxTransactions,
             MaxTraceableBlocks = ProtocolSettings.Default.MaxTraceableBlocks,
             InitialGasDistribution = ProtocolSettings.Default.InitialGasDistribution,
+            MemPoolSettings = ProtocolSettings.Default.MemPoolSettings,
             Hardforks = ProtocolSettings.Default.Hardforks
         };
     }
