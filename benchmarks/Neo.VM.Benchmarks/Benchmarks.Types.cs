c2a0a59dadf7b3e76b3bb4311591caf9f7931c38
Shargon <shargon@gmail.com>
2024-07-29 02:31:40 -0700

    Update benchmarks/Neo.VM.Benchmarks/Benchmarks.Types.cs

--- benchmarks/Neo.VM.Benchmarks/Benchmarks.Types.cs
+++ benchmarks/Neo.VM.Benchmarks/Benchmarks.Types.cs
@@ -70,7 +70,7 @@ public class Benchmarks_Types
         _ = root.DeepCopy();
     }
 
-    private static void CreateNestedArray(Array rootArray, int depth, int elementsPerLevel = 1, ReferenceCounter referenceCounter = null)
+    private static void CreateNestedArray(Array? rootArray, int depth, int elementsPerLevel = 1, ReferenceCounter? referenceCounter = null)
     {
         if (depth < 0)
         {
