d32098041b2e204ea715fe9d34899b1ee9d7b8e7
陈志同 <bitcoin2077@outlook.com>
2024-08-21 17:27:55 +0800

    Update UT_MemoryReader.cs

--- tests/Neo.UnitTests/IO/UT_MemoryReader.cs
+++ tests/Neo.UnitTests/IO/UT_MemoryReader.cs
@@ -9,8 +9,10 @@
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
 
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.IO;
+using System;
 using System.IO;
 using System.Text;
 
@@ -48,6 +50,121 @@ namespace Neo.UnitTests.IO
             MemoryReader reader = new(bs);
             var n = reader.ReadNullableArray<UInt256>();
             Assert.AreEqual(5, reader.Position);
+        }
+
+        [TestMethod]
+        public void TestReadSByte()
+        {
+            sbyte value = -5;
+            byte[] byteArray = new byte[1];
+            byteArray[0] = (byte)value;
+            MemoryReader reader = new(byteArray);
+            var n = reader.ReadSByte();
+            n.Should().Be(value);
+        }
+
+        [TestMethod]
+        public void TestReadInt32()
+        {
+            int value = -5;
+            byte[] bytes = BitConverter.GetBytes(value);
+            MemoryReader reader = new(bytes);
+            var n = reader.ReadInt32();
+            n.Should().Be(value);
+        }
+
+        [TestMethod]
+        public void TestReadUInt64()
+        {
+            ulong value = 12345;
+            byte[] bytes = BitConverter.GetBytes(value);
+            MemoryReader reader = new(bytes);
+            var n = reader.ReadUInt64();
+            n.Should().Be(value);
+        }
+
+        [TestMethod]
+        public void TestReadInt16BigEndian()
+        {
+            short value = 12345;
+            byte[] bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            MemoryReader reader = new(bytes);
+            var n = reader.ReadInt16BigEndian();
+            n.Should().Be(value);
+        }
+
+        [TestMethod]
+        public void TestReadUInt16BigEndian()
+        {
+            ushort value = 12345;
+            byte[] bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            MemoryReader reader = new(bytes);
+            var n = reader.ReadUInt16BigEndian();
+            n.Should().Be(value);
+        }
+
+        [TestMethod]
+        public void TestReadInt32BigEndian()
+        {
+            int value = 12345;
+            byte[] bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            MemoryReader reader = new(bytes);
+            var n = reader.ReadInt32BigEndian();
+            n.Should().Be(value);
+        }
+
+        [TestMethod]
+        public void TestReadUInt32BigEndian()
+        {
+            uint value = 12345;
+            byte[] bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            MemoryReader reader = new(bytes);
+            var n = reader.ReadUInt32BigEndian();
+            n.Should().Be(value);
+        }
+
+        [TestMethod]
+        public void TestReadInt64BigEndian()
+        {
+            long value = 12345;
+            byte[] bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            MemoryReader reader = new(bytes);
+            var n = reader.ReadInt64BigEndian();
+            n.Should().Be(value);
+        }
+
+        [TestMethod]
+        public void TestReadUInt64BigEndian()
+        {
+            ulong value = 12345;
+            byte[] bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            MemoryReader reader = new(bytes);
+            var n = reader.ReadUInt64BigEndian();
+            n.Should().Be(value);
         }
     }
 }
