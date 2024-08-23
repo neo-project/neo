a4bfcc323aba9c928e751da3ad80d02a1bc54f37
Christopher Schuchardt <cschuchardt88@gmail.com>
2024-08-22 21:52:06 -0400

    Added `UnitTest` for `SecureStringExtensions`

---
+++ tests/Neo.Extensions.Tests/UT_SecureStringExtensions.cs
@@ -0,0 +1,36 @@
+// Copyright (C) 2015-2024 The Neo Project.
+//
+// UT_SecureStringExtensions.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neo.Extensions.Tests
+{
+    [TestClass]
+    public class UT_SecureStringExtensions
+    {
+        [TestMethod]
+        public void Test_String_To_SecureString()
+        {
+            var expected = "Hello World";
+            var expectedSecureString = expected.ToSecureString();
+
+
+            var actual = expectedSecureString.GetClearText();
+
+            Assert.IsTrue(expectedSecureString.IsReadOnly());
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
