7dc92f308ab6d54823e658986158786f0a4716be
Jimmy <jimmy@r3e.network>
2024-08-28 00:46:04 +0800

    add edge case

--- tests/Neo.Extensions.Tests/UT_BigIntegerExtensions.cs
+++ tests/Neo.Extensions.Tests/UT_BigIntegerExtensions.cs
@@ -11,6 +11,7 @@
 
 using FluentAssertions;
 using Neo.Extensions;
+using Neo.Json;
 using System;
 using System.Collections.Generic;
 using System.Numerics;
@@ -43,6 +44,15 @@ namespace Neo.Extensions.Tests
         }
 
         [TestMethod]
+        public void TestGetLowestSetBit_EdgeCases()
+        {
+            BigInteger.MinusOne.GetLowestSetBit().Should().Be(0);
+            BigInteger.One.GetLowestSetBit().Should().Be(0);
+            new BigInteger(ulong.MaxValue).GetLowestSetBit().Should().Be(0);
+            (BigInteger.One << 1000).GetLowestSetBit().Should().Be(1000);
+        }
+
+        [TestMethod]
         public void TestToByteArrayStandard()
         {
             BigInteger number = BigInteger.Zero;
@@ -56,12 +66,32 @@ namespace Neo.Extensions.Tests
         }
 
         [TestMethod]
+        public void TestToByteArrayStandard_EdgeCases()
+        {
+            BigInteger.MinusOne.ToByteArrayStandard().Should().Equal(new byte[] { 0xFF });
+            new BigInteger(byte.MaxValue).ToByteArrayStandard().Should().Equal(new byte[] { 0xFF, 0x00 });
+            new BigInteger(ushort.MaxValue).ToByteArrayStandard().Should().Equal(new byte[] { 0xFF, 0xFF, 0x00 });
+            new BigInteger(JNumber.MIN_SAFE_INTEGER).ToByteArrayStandard().Should().Equal(new byte[] { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0xE0 });
+        }
+
+        [TestMethod]
         public void TestMod()
         {
             var x = new BigInteger(-13);
             var y = new BigInteger(5);
             var result = x.Mod(y);
             result.Should().Be(2); // -13 % 5 is -3, but Mod method should return 2
+        }
+
+        [TestMethod]
+        public void TestMod_EdgeCases()
+        {
+            BigInteger.Zero.Mod(5).Should().Be(0);
+            BigInteger.MinusOne.Mod(5).Should().Be(4);
+            new BigInteger(long.MinValue).Mod(long.MaxValue).Should().Be(9223372036854775806);
+            // Sign of the result: % can return negative values, while Mod always returns non-negative values.
+            var longMod = long.MinValue % long.MaxValue;
+            new BigInteger(long.MinValue).Mod(long.MaxValue).Should().NotBe(longMod);
         }
 
         [TestMethod]
@@ -90,6 +120,20 @@ namespace Neo.Extensions.Tests
         }
 
         [TestMethod]
+        public void TestModInverse_EdgeCases()
+        {
+            Action act = () => BigInteger.Zero.ModInverse(11);
+            act.Should().Throw<ArithmeticException>();
+
+            BigInteger.One.ModInverse(2).Should().Be(1);
+
+            act = () => new BigInteger(2).ModInverse(4);
+            act.Should().Throw<ArithmeticException>();
+
+            new BigInteger(long.MaxValue - 1).ModInverse(long.MaxValue).Should().Be(long.MaxValue - 1);
+        }
+
+        [TestMethod]
         public void TestBit()
         {
             var bigInteger = new BigInteger(5); // Binary: 101
@@ -102,11 +146,30 @@ namespace Neo.Extensions.Tests
         }
 
         [TestMethod]
+        public void TestBit_EdgeCases()
+        {
+            BigInteger.Zero.TestBit(0).Should().BeFalse();
+            BigInteger.Zero.TestBit(100).Should().BeFalse();
+            BigInteger.MinusOne.TestBit(0).Should().BeTrue();
+            BigInteger.MinusOne.TestBit(1000).Should().BeTrue();
+            (BigInteger.One << 1000).TestBit(1000).Should().BeTrue();
+            (BigInteger.One << 1000).TestBit(999).Should().BeFalse();
+        }
+
+        [TestMethod]
         public void TestSum()
         {
             var bigIntegers = new List<BigInteger> { 1, 2, 3, 4 };
             var result = bigIntegers.Sum();
             result.Should().Be(10);
+        }
+
+        [TestMethod]
+        public void TestSum_EdgeCases()
+        {
+            new List<BigInteger>().Sum().Should().Be(0);
+            new List<BigInteger> { JNumber.MIN_SAFE_INTEGER, JNumber.MAX_SAFE_INTEGER }.Sum().Should().Be(0);
+            new List<BigInteger> { JNumber.MAX_SAFE_INTEGER, JNumber.MAX_SAFE_INTEGER }.Sum().Should().Be(JNumber.MAX_SAFE_INTEGER * 2);
         }
     }
 }
