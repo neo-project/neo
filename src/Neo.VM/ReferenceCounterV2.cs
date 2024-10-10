f7cb00fb9fc019d0f1f1024cda075990b6a9e674
Fernando Diaz Toledano <shargon@gmail.com>
2024-10-10 16:28:40 +0200

    Remove warning

--- src/Neo.VM/ReferenceCounterV2.cs
+++ src/Neo.VM/ReferenceCounterV2.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2024 The Neo Project.
 //
-// ReferenceCounter.cs file belongs to the neo project and is free
+// ReferenceCounterV2.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
 // repository or http://www.opensource.org/licenses/mit-license.php
