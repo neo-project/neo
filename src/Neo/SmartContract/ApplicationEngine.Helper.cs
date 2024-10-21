f28363914fa23bb0bee11934aec3c3f4096cfa6c
Hecate2 <2474101468@qq.com>
2024-10-21 13:16:47 +0800

    helper to get engine error info

---
+++ src/Neo/SmartContract/ApplicationEngine.Helper.cs
@@ -0,0 +1,58 @@
+// Copyright (C) 2015-2024 The Neo Project.
+//
+// ApplicationEngine.Helper.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using Neo.SmartContract.Native;
+using Neo.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neo.SmartContract
+{
+    public partial class ApplicationEngine : ExecutionEngine
+    {
+        public string GetEngineErrorInfo()
+        {
+            if (State != VMState.FAULT || FaultException == null)
+                return null;
+            StringBuilder traceback = new();
+            try
+            {
+                if (CallingScriptHash != null)
+                    traceback.AppendLine($"CallingScriptHash={CallingScriptHash}[{NativeContract.ContractManagement.GetContract(SnapshotCache, CallingScriptHash)?.Manifest.Name}]");
+            }
+            catch { }
+            try
+            {
+                traceback.AppendLine($"CurrentScriptHash={CurrentScriptHash}[{NativeContract.ContractManagement.GetContract(SnapshotCache, CurrentScriptHash)?.Manifest.Name}]");
+            }
+            catch { }
+            try
+            {
+                traceback.AppendLine($"EntryScriptHash={EntryScriptHash}");
+            }
+            catch { }
+
+            foreach (ExecutionContext context in InvocationStack.Reverse())
+            {
+                UInt160 contextScriptHash = context.GetScriptHash();
+                string contextContractName = NativeContract.ContractManagement.GetContract(SnapshotCache, contextScriptHash)?.Manifest.Name;
+                traceback.AppendLine($"\tInstructionPointer={context.InstructionPointer}, OpCode {context.CurrentInstruction?.OpCode}, Script Length={context.Script.Length} {contextScriptHash}[{contextContractName}]");
+            }
+            Exception baseException = FaultException.GetBaseException();
+            traceback.AppendLine(baseException.StackTrace);
+            traceback.AppendLine(baseException.Message);
+
+            return traceback.ToString();
+        }
+    }
+}
