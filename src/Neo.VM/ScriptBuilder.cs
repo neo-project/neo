bd7ddfeb01b1e346e1cc70ee39ce9c8fb0dc6aaf
陈志同 <bitcoin2077@outlook.com>
2024-11-13 17:51:21 +0800

    update

--- src/Neo.VM/ScriptBuilder.cs
+++ src/Neo.VM/ScriptBuilder.cs
@@ -126,7 +126,7 @@ namespace Neo.VM
         /// <returns>A reference to this instance after the emit operation has completed.</returns>
         public ScriptBuilder EmitPush(ReadOnlySpan<byte> data)
         {
-            if (data.IsEmpty)
+            if (data == null)
                 throw new ArgumentNullException(nameof(data));
             if (data.Length < 0x100)
             {
