0c16261c101c318e13d5c622d30a619026d04a6e
Fernando Diaz Toledano <shargon@gmail.com>
2024-11-21 20:32:04 +0100

    Solve HF problem

--- src/Neo/SmartContract/ApplicationEngine.cs
+++ src/Neo/SmartContract/ApplicationEngine.cs
@@ -176,20 +176,12 @@ namespace Neo.SmartContract
         /// <param name="settings">The <see cref="Neo.ProtocolSettings"/> used by the engine.</param>
         /// <param name="gas">The maximum gas, in the unit of datoshi, used in this execution. The execution will fail when the gas is exhausted.</param>
         /// <param name="diagnostic">The diagnostic to be used by the <see cref="ApplicationEngine"/>.</param>
+        /// <param name="referenceCounter">Reference Counter</param>
         /// <param name="jumpTable">The jump table to be used by the <see cref="ApplicationEngine"/>.</param>
         protected unsafe ApplicationEngine(
             TriggerType trigger, IVerifiable container, DataCache snapshotCache, Block persistingBlock,
-            ProtocolSettings settings, long gas, IDiagnostic diagnostic, JumpTable jumpTable = null)
-            : base(
-               // this is IsHardforkEnabled
-               settings is not null
-                   ? (persistingBlock is null
-                       ? settings.Hardforks.ContainsKey(Hardfork.HF_Echidna)
-                       : settings.IsHardforkEnabled(Hardfork.HF_Echidna, persistingBlock.Index))
-                            ? new ReferenceCounterV2()
-                            : new ReferenceCounter()
-                   : new ReferenceCounterV2(),
-                jumpTable ?? DefaultJumpTable)
+            ProtocolSettings settings, long gas, IDiagnostic diagnostic, IReferenceCounter? referenceCounter = null, JumpTable jumpTable = null)
+            : base(referenceCounter ?? new ReferenceCounterV2(), jumpTable ?? DefaultJumpTable)
         {
             Trigger = trigger;
             ScriptContainer = container;
@@ -408,11 +400,16 @@ namespace Neo.SmartContract
         /// <returns>The engine instance created.</returns>
         public static ApplicationEngine Create(TriggerType trigger, IVerifiable container, DataCache snapshot, Block persistingBlock = null, ProtocolSettings settings = null, long gas = TestModeGas, IDiagnostic diagnostic = null)
         {
+            var index = persistingBlock?.Index ?? NativeContract.Ledger.CurrentIndex(snapshot);
+
             // Adjust jump table according persistingBlock
-            var jumpTable = ApplicationEngine.DefaultJumpTable;
+            var jumpTable = DefaultJumpTable;
+            IReferenceCounter referenceCounter =
+                settings.IsHardforkEnabled(Hardfork.HF_Echidna, index) ?
+                new ReferenceCounterV2() : new ReferenceCounter();
 
             return Provider?.Create(trigger, container, snapshot, persistingBlock, settings, gas, diagnostic, jumpTable)
-                  ?? new ApplicationEngine(trigger, container, snapshot, persistingBlock, settings, gas, diagnostic, jumpTable);
+                  ?? new ApplicationEngine(trigger, container, snapshot, persistingBlock, settings, gas, diagnostic, referenceCounter, jumpTable);
         }
 
         public override void LoadContext(ExecutionContext context)
