a369d002043c657c0232267de3c728a4e5740b92
Jimmy <jinghui@wayne.edu>
2024-11-21 15:45:17 +0800

    do more newarray bench

--- benchmarks/Neo.VM.Benchmarks/OpCode/Arrays/OpCode.NEWARRAY.cs
+++ benchmarks/Neo.VM.Benchmarks/OpCode/Arrays/OpCode.NEWARRAY.cs
@@ -49,3 +49,17 @@ namespace Neo.VM.Benchmark.OpCode
     //     | Bench_OneOpCode | 2040      |        43.552 us |     0.8678 us |     1.6299 us |        43.150 us |
     //     | Bench_OneGAS    | 2040      | 1,056,640.087 us | 9,672.7187 us | 9,047.8670 us | 1,056,767.500 us |
 }
+
+
+// | Method          | ItemCount | Mean             | Error          | StdDev        | Median           |
+//     |---------------- |---------- |-----------------:|---------------:|--------------:|-----------------:|
+//     | Bench_OneOpCode | 2         |         3.307 us |      0.0618 us |     0.1469 us |         3.250 us |
+//     | Bench_OneGAS    | 2         |    12,691.531 us |    238.7211 us |   234.4561 us |    12,724.550 us |
+//     | Bench_OneOpCode | 32        |         4.223 us |      0.2059 us |     0.5671 us |         4.000 us |
+//     | Bench_OneGAS    | 32        |    29,996.893 us |    577.0231 us |   808.9058 us |    29,989.200 us |
+//     | Bench_OneOpCode | 128       |         7.861 us |      0.3762 us |     1.1092 us |         7.600 us |
+//     | Bench_OneGAS    | 128       |    76,677.836 us |  1,425.6028 us | 1,263.7598 us |    76,646.750 us |
+//     | Bench_OneOpCode | 1024      |        37.225 us |      0.7347 us |     0.7216 us |        37.050 us |
+//     | Bench_OneGAS    | 1024      |   527,543.793 us |  5,679.4756 us | 5,034.7072 us |   527,402.100 us |
+//     | Bench_OneOpCode | 2040      |        47.888 us |      0.9389 us |     1.8533 us |        47.200 us |
+//     | Bench_OneGAS    | 2040      | 1,006,428.933 us | 10,354.7827 us | 9,685.8702 us | 1,008,789.900 us |
