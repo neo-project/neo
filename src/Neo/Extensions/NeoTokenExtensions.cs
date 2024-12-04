4e951f15b4a847624b1eb003b14b2915fbf863c5
Christopher Schuchardt <cschuchardt88@gmail.com>
2024-12-04 02:18:17 -0500

    [Add] Get Accounts and Balances for NeoToken

---
+++ src/Neo/Extensions/NeoTokenExtensions.cs
@@ -0,0 +1,63 @@
+// Copyright (C) 2015-2024 The Neo Project.
+//
+// NeoTokenExtensions.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using Neo.Persistence;
+using Neo.SmartContract;
+using Neo.SmartContract.Native;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Neo.Extensions
+{
+    public static class NeoTokenExtensions
+    {
+        private const byte Prefix_Account = 20;
+
+        public static IEnumerable<(UInt160 Address, BigInteger Balance)> GetAccounts(this NeoToken neoToken, DataCache snapshot)
+        {
+            if (snapshot is null)
+                throw new ArgumentNullException(nameof(snapshot));
+
+            var kb = new KeyBuilder(neoToken.Id, Prefix_Account);
+            var prefixKey = kb.ToArray();
+
+            foreach (var (key, value) in snapshot.Seek(prefixKey, SeekDirection.Forward))
+            {
+                var keyBytes = key.ToArray();
+
+                if (keyBytes.AsSpan().StartsWith(prefixKey))
+                {
+                    var addressHash = new UInt160(keyBytes.AsSpan(prefixKey.Length));
+                    yield return new(addressHash, value.GetInteroperable<AccountState>().Balance);
+                }
+                else
+                    yield break;
+            }
+        }
+
+        public static BigInteger GetAccountBalance(this NeoToken neoToken, DataCache snapshot, UInt160 address)
+        {
+            if (snapshot is null)
+                throw new ArgumentNullException(nameof(snapshot));
+
+            if (address is null)
+                throw new ArgumentNullException(nameof(address));
+
+            var kb = new KeyBuilder(neoToken.Id, Prefix_Account)
+                .Add(address);
+
+            var prefixKey = kb.ToArray();
+            var account = snapshot.TryGet(prefixKey);
+            return account.GetInteroperable<AccountState>().Balance;
+        }
+    }
+}
