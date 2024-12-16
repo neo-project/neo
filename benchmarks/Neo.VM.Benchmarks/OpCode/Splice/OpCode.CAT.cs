f589590c25723dfc83a43f25b5e62642f0449936
陈志同 <bitcoin2077@outlook.com>
2024-12-16 14:45:25 +0800

    CAT, LEFT, RIGHT

--- benchmarks/Neo.VM.Benchmarks/OpCode/Splice/OpCode.CAT.cs
+++ benchmarks/Neo.VM.Benchmarks/OpCode/Splice/OpCode.CAT.cs
@@ -9,27 +9,72 @@
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
 
+using Neo.VM.Types;
+
 namespace Neo.VM.Benchmark.OpCode
 {
     public class OpCode_CAT : OpCodeBase
     {
-        // hide the bench param ItemCount
-        public int ItemCount { get; set; } = 2040;
-
         protected override VM.OpCode Opcode => VM.OpCode.CAT;
 
         protected override byte[] CreateOneOpCodeScript()
         {
             var builder = new InstructionBuilder();
             builder.Push(ItemCount);
-            builder.Push(0);
+            builder.AddInstruction(VM.OpCode.NEWBUFFER);
+            builder.AddInstruction(new Instruction
+            {
+                _opCode = VM.OpCode.CONVERT,
+                _operand = [(byte)StackItemType.ByteString]
+            });
+            builder.AddInstruction(VM.OpCode.DUP);
             builder.AddInstruction(Opcode);
             return builder.ToArray();
         }
 
         protected override byte[] CreateOneGASScript()
         {
-            throw new NotImplementedException();
+            var builder = new InstructionBuilder();
+            builder.Push(ItemCount);
+            builder.AddInstruction(VM.OpCode.NEWBUFFER);
+            builder.AddInstruction(new Instruction
+            {
+                _opCode = VM.OpCode.CONVERT,
+                _operand = [(byte)StackItemType.ByteString]
+            });
+
+            var loopBegin = new JumpTarget { _instruction = builder.AddInstruction(VM.OpCode.NOP) };
+
+            builder.AddInstruction(VM.OpCode.DUP);
+            builder.AddInstruction(VM.OpCode.DUP);
+            builder.AddInstruction(Opcode);
+            builder.AddInstruction(VM.OpCode.DROP);
+
+            builder.Jump(VM.OpCode.JMP, loopBegin);
+            return builder.ToArray();
         }
     }
 }
+
+/// | Method          | ItemCount | Mean          | Error         | StdDev        | Median        |
+/// |---------------- |---------- |--------------:|--------------:|--------------:|--------------:|
+/// | Bench_OneOpCode | 4         |      2.531 us |     0.1734 us |     0.5084 us |      2.300 us |
+/// | Bench_OneGAS    | 4         |  7,354.089 us | 2,107.4629 us | 6,213.9024 us |  3,056.950 us |
+/// | Bench_OneOpCode | 8         |      2.188 us |     0.0665 us |     0.1786 us |      2.150 us |
+/// | Bench_OneGAS    | 8         |  6,770.574 us | 1,862.5357 us | 5,462.4951 us |  4,202.000 us |
+/// | Bench_OneOpCode | 16        |      2.196 us |     0.0583 us |     0.1514 us |      2.200 us |
+/// | Bench_OneGAS    | 16        |  7,297.803 us | 1,873.4257 us | 5,523.8383 us |  3,237.550 us |
+/// | Bench_OneOpCode | 32        |      2.488 us |     0.1525 us |     0.4447 us |      2.300 us |
+/// | Bench_OneGAS    | 32        | 10,363.423 us |   171.1694 us |   142.9342 us | 10,370.600 us |
+/// | Bench_OneOpCode | 64        |      2.212 us |     0.0559 us |     0.1522 us |      2.200 us |
+/// | Bench_OneGAS    | 64        |  7,836.663 us | 1,643.4393 us | 4,819.9234 us |  6,299.400 us |
+/// | Bench_OneOpCode | 128       |      2.125 us |     0.0463 us |     0.1179 us |      2.100 us |
+/// | Bench_OneGAS    | 128       |  7,557.801 us | 1,827.8226 us | 5,360.6877 us |  4,057.400 us |
+/// | Bench_OneOpCode | 256       |      2.600 us |     0.1930 us |     0.5537 us |      2.400 us |
+/// | Bench_OneGAS    | 256       |  8,188.538 us | 2,081.1449 us | 6,103.6383 us |  4,268.200 us |
+/// | Bench_OneOpCode | 512       |      2.187 us |     0.0420 us |     0.0629 us |      2.200 us |
+/// | Bench_OneGAS    | 512       |  7,020.697 us | 1,361.4439 us | 3,749.8122 us |  4,722.650 us |
+/// | Bench_OneOpCode | 1024      |      2.325 us |     0.0400 us |     0.0936 us |      2.300 us |
+/// | Bench_OneGAS    | 1024      |  6,066.211 us |   312.1295 us |   838.5158 us |  5,755.600 us |
+/// | Bench_OneOpCode | 2040      |      2.323 us |     0.0800 us |     0.2122 us |      2.300 us |
+/// | Bench_OneGAS    | 2040      |  8,367.546 us | 1,146.3423 us | 3,214.4671 us |  6,659.300 us |
--- benchmarks/Neo.VM.Benchmarks/OpCode/Splice/OpCode.RIGHT.cs
+++ benchmarks/Neo.VM.Benchmarks/OpCode/Splice/OpCode.RIGHT.cs
@@ -9,25 +9,76 @@
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
 
+using Neo.VM.Types;
+
 namespace Neo.VM.Benchmark.OpCode
 {
     public class OpCode_RIGHT : OpCodeBase
     {
 
-        protected override VM.OpCode Opcode => VM.OpCode.PICKITEM;
+        protected override VM.OpCode Opcode => VM.OpCode.RIGHT;
+
+        public const int MAX_LEN = ushort.MaxValue;
 
         protected override byte[] CreateOneOpCodeScript()
         {
             var builder = new InstructionBuilder();
+            builder.Push(MAX_LEN);
+            builder.AddInstruction(VM.OpCode.NEWBUFFER);
+            builder.AddInstruction(new Instruction
+            {
+                _opCode = VM.OpCode.CONVERT,
+                _operand = [(byte)StackItemType.ByteString]
+            });
             builder.Push(ItemCount);
-            builder.Push(0);
             builder.AddInstruction(Opcode);
+
             return builder.ToArray();
         }
 
         protected override byte[] CreateOneGASScript()
         {
-            throw new NotImplementedException();
+            var builder = new InstructionBuilder();
+            builder.Push(MAX_LEN);
+            builder.AddInstruction(VM.OpCode.NEWBUFFER);
+            builder.AddInstruction(new Instruction
+            {
+                _opCode = VM.OpCode.CONVERT,
+                _operand = [(byte)StackItemType.ByteString]
+            });
+
+            var loopBegin = new JumpTarget { _instruction = builder.AddInstruction(VM.OpCode.NOP) };
+
+            builder.AddInstruction(VM.OpCode.DUP);
+            builder.Push(ItemCount);
+            builder.AddInstruction(Opcode);
+            builder.AddInstruction(VM.OpCode.DROP);
+
+            builder.Jump(VM.OpCode.JMP, loopBegin);
+            return builder.ToArray();
         }
     }
 }
+
+/// | Method          | ItemCount | Mean          | Error         | StdDev        | Median        |
+/// |---------------- |---------- |--------------:|--------------:|--------------:|--------------:|
+/// | Bench_OneOpCode | 4         |      2.860 us |     0.1128 us |     0.3237 us |      2.800 us |
+/// | Bench_OneGAS    | 4         |  7,371.909 us | 2,192.7840 us | 6,465.4737 us |  2,941.400 us |
+/// | Bench_OneOpCode | 8         |      2.701 us |     0.0632 us |     0.1718 us |      2.700 us |
+/// | Bench_OneGAS    | 8         |  8,276.648 us | 2,089.1983 us | 6,160.0488 us |  7,424.050 us |
+/// | Bench_OneOpCode | 16        |      2.941 us |     0.0736 us |     0.2076 us |      2.900 us |
+/// | Bench_OneGAS    | 16        |  6,213.548 us | 1,251.4915 us | 3,650.6597 us |  5,860.350 us |
+/// | Bench_OneOpCode | 32        |      2.518 us |     0.0534 us |     0.1090 us |      2.500 us |
+/// | Bench_OneGAS    | 32        |  7,500.268 us | 2,140.7244 us | 6,311.9747 us |  2,982.950 us |
+/// | Bench_OneOpCode | 64        |      2.603 us |     0.1081 us |     0.3015 us |      2.500 us |
+/// | Bench_OneGAS    | 64        |  7,123.892 us | 1,879.9781 us | 5,513.6507 us |  3,343.800 us |
+/// | Bench_OneOpCode | 128       |      2.872 us |     0.1279 us |     0.3566 us |      2.800 us |
+/// | Bench_OneGAS    | 128       |  6,545.854 us | 1,954.8252 us | 5,733.1642 us |  3,162.900 us |
+/// | Bench_OneOpCode | 256       |      2.897 us |     0.1002 us |     0.2940 us |      2.800 us |
+/// | Bench_OneGAS    | 256       | 10,724.053 us |   212.8602 us |   199.1095 us | 10,702.100 us |
+/// | Bench_OneOpCode | 512       |      2.787 us |     0.0544 us |     0.1036 us |      2.800 us |
+/// | Bench_OneGAS    | 512       | 10,817.380 us |   215.8381 us |   201.8951 us | 10,848.100 us |
+/// | Bench_OneOpCode | 1024      |      2.920 us |     0.0776 us |     0.2190 us |      2.900 us |
+/// | Bench_OneGAS    | 1024      |  8,326.519 us | 2,234.9798 us | 6,589.8888 us |  3,857.700 us |
+/// | Bench_OneOpCode | 2040      |      2.862 us |     0.1498 us |     0.4298 us |      2.700 us |
+/// | Bench_OneGAS    | 2040      |  7,857.649 us | 1,754.6136 us | 5,062.4618 us |  4,817.050 us |
