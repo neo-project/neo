705f4bb8cb0708dd6458608c8648a06c4051df8c
Shargon <shargon@gmail.com>
2024-12-19 08:37:19 +0100

    Fix NEO callstates (#3599)
    * Allow callstates to use HF
    
    * Rename to method
    
    * Other rename
    
    * Change the way
    
    * Reduce changes
    
    * Reduce changes
    
    * Adapt name always
    
    * Avoid string when only is lower the first char
    
    * UT
    
    * Test all
    
    * Update src/Neo/ProtocolSettings.cs
    
    Co-authored-by: Christopher Schuchardt <cschuchardt88@gmail.com>
    
    * Update src/Neo/ProtocolSettings.cs
    
    Co-authored-by: Christopher Schuchardt <cschuchardt88@gmail.com>
    
    * Reuse Load from stream
    
    * Unify
    
    * Fix default logic
    
    * Change ContractMethod to allowMultiple
    
    * Use LowerInvariant
    
    * Move CheckingHardfork
    
    * Remove optional arg
    
    * Fix build
    
    * Avoid file not found error
    
    ---------
    
    Co-authored-by: Christopher Schuchardt <cschuchardt88@gmail.com>

--- src/Neo/SmartContract/Native/NeoToken.cs
+++ src/Neo/SmartContract/Native/NeoToken.cs
@@ -332,7 +332,8 @@ namespace Neo.SmartContract.Native
             return CalculateBonus(snapshot, state, end);
         }
 
-        [ContractMethod(RequiredCallFlags = CallFlags.States)]
+        [ContractMethod(true, Hardfork.HF_Echidna, RequiredCallFlags = CallFlags.States)]
+        [ContractMethod(Hardfork.HF_Echidna, /* */ RequiredCallFlags = CallFlags.States | CallFlags.AllowNotify)]
         private bool RegisterCandidate(ApplicationEngine engine, ECPoint pubkey)
         {
             if (!engine.CheckWitnessInternal(Contract.CreateSignatureRedeemScript(pubkey).ToScriptHash()))
@@ -349,7 +350,8 @@ namespace Neo.SmartContract.Native
             return true;
         }
 
-        [ContractMethod(CpuFee = 1 << 16, RequiredCallFlags = CallFlags.States)]
+        [ContractMethod(true, Hardfork.HF_Echidna, CpuFee = 1 << 16, RequiredCallFlags = CallFlags.States)]
+        [ContractMethod(Hardfork.HF_Echidna, /* */ CpuFee = 1 << 16, RequiredCallFlags = CallFlags.States | CallFlags.AllowNotify)]
         private bool UnregisterCandidate(ApplicationEngine engine, ECPoint pubkey)
         {
             if (!engine.CheckWitnessInternal(Contract.CreateSignatureRedeemScript(pubkey).ToScriptHash()))
@@ -366,7 +368,8 @@ namespace Neo.SmartContract.Native
             return true;
         }
 
-        [ContractMethod(CpuFee = 1 << 16, RequiredCallFlags = CallFlags.States)]
+        [ContractMethod(true, Hardfork.HF_Echidna, CpuFee = 1 << 16, RequiredCallFlags = CallFlags.States)]
+        [ContractMethod(Hardfork.HF_Echidna, /* */ CpuFee = 1 << 16, RequiredCallFlags = CallFlags.States | CallFlags.AllowNotify)]
         private async ContractTask<bool> Vote(ApplicationEngine engine, UInt160 account, ECPoint voteTo)
         {
             if (!engine.CheckWitnessInternal(account)) return false;
--- tests/Neo.UnitTests/SmartContract/Native/UT_NativeContract.cs
+++ tests/Neo.UnitTests/SmartContract/Native/UT_NativeContract.cs
@@ -71,7 +71,7 @@ namespace Neo.UnitTests.SmartContract.Native
             string json = UT_ProtocolSettings.CreateHFSettings("\"HF_Cockatrice\": 20");
             var file = Path.GetTempFileName();
             File.WriteAllText(file, json);
-            ProtocolSettings settings = ProtocolSettings.Load(file, false);
+            ProtocolSettings settings = ProtocolSettings.Load(file);
             File.Delete(file);
 
             Assert.IsFalse(NativeContract.IsActive(new active() { ActiveIn = Hardfork.HF_Cockatrice, DeprecatedIn = null }, settings.IsHardforkEnabled, 1));
@@ -87,7 +87,7 @@ namespace Neo.UnitTests.SmartContract.Native
             string json = UT_ProtocolSettings.CreateHFSettings("\"HF_Echidna\": 20");
             var file = Path.GetTempFileName();
             File.WriteAllText(file, json);
-            ProtocolSettings settings = ProtocolSettings.Load(file, false);
+            ProtocolSettings settings = ProtocolSettings.Load(file);
             File.Delete(file);
 
             var before = NativeContract.RoleManagement.GetContractState(settings.IsHardforkEnabled, 19);
@@ -112,7 +112,7 @@ namespace Neo.UnitTests.SmartContract.Native
 
             var file = Path.GetTempFileName();
             File.WriteAllText(file, json);
-            ProtocolSettings settings = ProtocolSettings.Load(file, false);
+            ProtocolSettings settings = ProtocolSettings.Load(file);
             File.Delete(file);
 
             Assert.IsTrue(NativeContract.CryptoLib.IsInitializeBlock(settings, 0, out var hf));
