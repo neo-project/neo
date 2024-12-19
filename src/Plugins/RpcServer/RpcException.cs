61de3a44811c924b0e5466e6e199fc2ca0a59c67
Jimmy <jinghui@wayne.edu>
2024-12-19 11:18:08 +0800

    [Plugin RpcServer] fix issue 3629 (#3630)
    * fix issue 3629
    
    * update test
    
    * format
    
    * add debug for rpcexception
    
    * Update src/Plugins/RpcServer/RpcException.cs
    
    ---------
    
    Co-authored-by: nan01ab <yjcc201374@outlook.com>

--- src/Plugins/RpcServer/RpcException.cs
+++ src/Plugins/RpcServer/RpcException.cs
@@ -15,9 +15,17 @@ namespace Neo.Plugins.RpcServer
 {
     public class RpcException : Exception
     {
+        private readonly RpcError _rpcError;
+
         public RpcException(RpcError error) : base(error.ErrorMessage)
         {
             HResult = error.Code;
+            _rpcError = error;
+        }
+
+        public RpcError GetError()
+        {
+            return _rpcError;
         }
     }
 }
