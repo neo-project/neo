8914661dbcf3ee6b2c979536b8846a05ac6d5576
Shargon <shargon@gmail.com>
2024-12-27 01:08:08 -0800

    Update benchmarks/Neo.Benchmarks/Persistence/Bechmarks_ReadOnlyStoreView.cs

--- benchmarks/Neo.Benchmarks/Persistence/Bechmarks_ReadOnlyStoreView.cs
+++ benchmarks/Neo.Benchmarks/Persistence/Bechmarks_ReadOnlyStoreView.cs
@@ -9,7 +9,6 @@
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
 
-
 using BenchmarkDotNet.Attributes;
 using Neo.Persistence;
 using Neo.Plugins.Storage;
