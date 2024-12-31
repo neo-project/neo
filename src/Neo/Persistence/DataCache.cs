7c06a23e1556dca725690ded3aa55e531c8152ec
nan01ab <yjcc201374@outlook.com>
2024-12-31 11:17:59 +0800

    [`Add`]: add `IReadOnlyStoreView` (#3642)
    * feature: add IReadOnlyStoreView and ReadOnlyStoreView
    
    * Update src/Neo/Persistence/IReadOnlyStoreView.cs
    
    * Update benchmarks/Neo.Benchmarks/Persistence/Bechmarks_ReadOnlyStoreView.cs
    
    * Clean Bechmarks_ReadOnlyStoreView.cs
    
    ---------
    
    Co-authored-by: Jimmy <jinghui@wayne.edu>
    Co-authored-by: Shargon <shargon@gmail.com>
    Co-authored-by: NGD Admin <154295625+NGDAdmin@users.noreply.github.com>

--- src/Neo/Persistence/DataCache.cs
+++ src/Neo/Persistence/DataCache.cs
@@ -14,13 +14,14 @@ using Neo.SmartContract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Neo.Persistence
 {
     /// <summary>
     /// Represents a cache for the underlying storage of the NEO blockchain.
     /// </summary>
-    public abstract class DataCache
+    public abstract class DataCache : IReadOnlyStoreView
     {
         /// <summary>
         /// Represents an entry in the cache.
@@ -509,6 +510,14 @@ namespace Neo.Persistence
                 });
                 return value;
             }
+        }
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryGet(StorageKey key, out StorageItem item)
+        {
+            item = TryGet(key);
+            return item != null;
         }
 
         /// <summary>
---
+++ tests/Neo.UnitTests/Persistence/UT_ReadOnlyStoreView.cs
@@ -0,0 +1,52 @@
+// Copyright (C) 2015-2024 The Neo Project.
+//
+// UT_ReadOnlyStoreView.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Persistence;
+using Neo.SmartContract;
+using System.Collections.Generic;
+
+namespace Neo.UnitTests.Persistence
+{
+    public class UT_ReadOnlyStoreView
+    {
+        [TestMethod]
+        public void TestReadOnlyStoreView()
+        {
+            var store = new MemoryStore();
+            var key = new KeyBuilder(1, 2).Add(new UInt160()).ToArray();
+            var view = new ReadOnlyStoreView(store);
+
+            // Test Contains
+            Assert.IsFalse(view.Contains(key));
+
+            // Test TryGet
+            Assert.IsFalse(view.TryGet(key, out var item));
+
+            // Test this[]
+            Assert.ThrowsException<KeyNotFoundException>(() => view[key]);
+
+            // Test Put
+            var value = new byte[] { 1, 2, 3 };
+            store.Put(key, value);
+
+            // Test Contains
+            Assert.IsTrue(view.Contains(key));
+
+            // Test this[]
+            CollectionAssert.AreEqual(value, view[key].Value.ToArray());
+
+            // Test TryGet
+            Assert.IsTrue(view.TryGet(key, out item));
+            CollectionAssert.AreEqual(value, item.Value.ToArray());
+        }
+    }
+}
