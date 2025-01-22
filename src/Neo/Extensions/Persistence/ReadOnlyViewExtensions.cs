5628012145ded7a109cbee08b43f257d567b5559
nan01ab <yjcc201374@outlook.com>
2025-01-22 08:46:14 +0800

    Improve: add more comments

--- src/Neo/Extensions/Persistence/ReadOnlyViewExtensions.cs
+++ src/Neo/Extensions/Persistence/ReadOnlyViewExtensions.cs
@@ -23,21 +23,36 @@ namespace Neo.Extensions
     public static class ReadOnlyViewExtensions
     {
         /// <summary>
+        /// Gets the 0xff bytes of the specified length.
+        /// </summary>
+        /// <param name="length">The length of the bytes.</param>
+        /// <returns>The 0xff bytes.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static byte[] Get0xffBytes(int length)
+        {
+            var bytes = new byte[length];
+            Array.Fill(bytes, (byte)0xff);
+            return bytes;
+        }
+
+        /// <summary>
         /// Scans the entries starting with the specified prefix.
+        /// <para>
+        /// If <paramref name="direction"/> is <see cref="SeekDirection.Forward"/>,
+        /// it seeks to the first entry if <paramref name="keyPrefix"/> is null or empty.
+        /// </para>
+        /// <para>
+        /// If <paramref name="direction"/> is <see cref="SeekDirection.Backward"/>,
+        /// the <paramref name="keyPrefix"/> cannot be null or empty.
+        /// </para>
+        /// <para>
+        /// If want to scan all entries with <see cref="SeekDirection.Backward"/>,
+        /// set <paramref name="keyPrefix"/> to be N * 0xff, where N is the max length of the key.
+        /// See <see cref="Get0xffBytes"/>.
+        /// </para>
         /// </summary>
         /// <param name="view">The view to scan.</param>
-        /// <param name="keyPrefix">
-        /// The prefix of the key.
-        /// 
-        /// If <paramref name="direction"/> is <see cref="SeekDirection.Backward"/>, 
-        /// the <paramref name="keyPrefix"/> cannot be null or empty.
-        /// 
-        /// If <paramref name="direction"/> is <see cref="SeekDirection.Forward"/>, 
-        /// it seeks to the first entry if <paramref name="keyPrefix"/> is null or empty.
-        /// 
-        /// If want to scan all entries with backward, 
-        /// set <paramref name="keyPrefix"/> to be n * 0xff, where n is the max length of the key.
-        /// </param>
+        /// <param name="keyPrefix">The prefix of the key.</param>
         /// <param name="direction">The search direction.</param>
         /// <returns>The entries found with the desired prefix.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -67,6 +82,19 @@ namespace Neo.Extensions
 
         /// <summary>
         /// Scans the entries in the specified range.
+        /// <para>
+        /// If <paramref name="direction"/> is <see cref="SeekDirection.Forward"/>,
+        /// it seeks to the first entry if <paramref name="inclusiveStartKey"/> is null or empty.
+        /// </para>
+        /// <para>
+        /// If <paramref name="direction"/> is <see cref="SeekDirection.Backward"/>,
+        /// the <paramref name="inclusiveStartKey"/> cannot be null or empty.
+        /// </para>
+        /// <para>
+        /// If want to scan all entries with <see cref="SeekDirection.Backward"/>,
+        /// set <paramref name="inclusiveStartKey"/> to be N * 0xff and <paramref name="exclusiveEndKey"/> to be empty,
+        /// where N is the max length of the key.
+        /// </para>
         /// </summary>
         /// <param name="view">The view to scan.</param>
         /// <param name="inclusiveStartKey">The inclusive start key.</param>
@@ -93,14 +121,17 @@ namespace Neo.Extensions
 
         /// <summary>
         /// Gets the seek prefix for the specified key prefix.
+        /// <para>
+        /// If the <paramref name="keyPrefix"/> is all 0xff, and <paramref name="maxSizeWhenAll0xff"/> > 0,
+        /// the seek prefix will be set to be byte[maxSizeWhenAll0xff] and filled with 0xff.
+        /// </para>
+        /// <para>
+        /// If the <paramref name="keyPrefix"/> is all 0xff and <paramref name="maxSizeWhenAll0xff"/> is less than or equal to 0,
+        /// an ArgumentException will be thrown.
+        /// </para>
         /// </summary>
         /// <param name="keyPrefix">The key prefix.</param>
-        /// <param name="maxSizeWhenAll0xff">
-        /// The maximum size when all bytes are 0xff.
-        /// If the key prefix is all 0xff, and <paramref name="maxSizeWhenAll0xff"/> > 0,
-        /// the seek prefix will be set to be byte[maxSizeWhenAll0xff] and filled with 0xff.
-        /// If <paramref name="maxSizeWhenAll0xff"/> is less than or equal to 0, an ArgumentException will be thrown.
-        /// </param>
+        /// <param name="maxSizeWhenAll0xff">The maximum size when all bytes are 0xff.</param>
         /// <returns>The seek prefix.</returns>
         /// <exception cref="ArgumentNullException">Thrown when <paramref name="keyPrefix"/> is null.</exception>
         /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="keyPrefix"/> is empty.</exception>
@@ -129,14 +160,9 @@ namespace Neo.Extensions
             if (seekPrefix == null)
             {
                 if (maxSizeWhenAll0xff > 0)
-                {
-                    seekPrefix = new byte[maxSizeWhenAll0xff];
-                    Array.Fill(seekPrefix, (byte)0xff);
-                }
+                    seekPrefix = Get0xffBytes(maxSizeWhenAll0xff);
                 else
-                {
                     throw new ArgumentException($"{nameof(keyPrefix)} with all bytes being 0xff is not supported now");
-                }
             }
             return seekPrefix;
         }
