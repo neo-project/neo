8e1cb4c32a27376570ac01b99d4271772d58cbf1
Christopher Schuchardt <cschuchardt88@gmail.com>
2025-02-05 03:13:09 -0500

    Revert file header

--- src/Neo.VM/Instruction.cs
+++ src/Neo.VM/Instruction.cs
@@ -2,8 +2,9 @@
 //
 // Instruction.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
-// accompanying file LICENSE in the main directory of the repository
-// or https://opensource.org/license/mit for more details.
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
 //
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
