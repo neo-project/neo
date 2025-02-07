e9824b219ed080a06b8d0aa0503a52ec4b880c83
Christopher Schuchardt <cschuchardt88@gmail.com>
2025-02-07 16:28:59 -0500

    dotnet format

--- tests/Neo.Build.Tests/MSTestSettings.cs
+++ tests/Neo.Build.Tests/MSTestSettings.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) 2015-2025 The Neo Project.
+// Copyright (C) 2015-2025 The Neo Project.
 //
 // MSTestSettings.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
--- tests/Neo.Build.Tests/Utilities/Core/NeoWallet.cs
+++ tests/Neo.Build.Tests/Utilities/Core/NeoWallet.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2025 The Neo Project.
 //
-// Test1.cs file belongs to the neo project and is free
+// NeoWallet.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
 // repository or http://www.opensource.org/licenses/mit-license.php
