63f00dfb31b125a5ac4c34f89ec5272d7186e137
Christopher Schuchardt <cschuchardt88@gmail.com>
2025-02-07 16:26:29 -0500

    Update src/Neo.Build/Json/Converters/JsonStringKeyPairConverter.cs

--- src/Neo.Build/Json/Converters/JsonStringKeyPairConverter.cs
+++ src/Neo.Build/Json/Converters/JsonStringKeyPairConverter.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2025 The Neo Project.
 //
-// JsonKeyPairConverter.cs file belongs to the neo project and is free
+// JsonStringKeyPairConverter.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
 // repository or http://www.opensource.org/licenses/mit-license.php
