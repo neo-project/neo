d0b603a8ae37857a4e749e89d755f2ad6ae40dca
Christopher Schuchardt <cschuchardt88@gmail.com>
2025-02-09 08:51:08 -0500

    Add Neo Build Error Base Class

---
+++ src/Shared/Neo.Build.Core/Exceptions/NeoBuildException.cs
@@ -0,0 +1,40 @@
+// Copyright (C) 2015-2025 The Neo Project.
+//
+// NeoBuildException.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using System;
+
+namespace Neo.Build.Core.Exceptions
+{
+    public class NeoBuildException(
+        string errorMessage,
+        int errorCode = NeoBuildErrorCodes.General.InternalException) : Exception(), INeoBuildException
+    {
+        /// <summary>
+        /// Exit code from the build process.
+        /// </summary>
+        public int ExitCode => HResult = errorCode;
+
+        /// <summary>
+        /// An error code for referencing the problem.
+        /// </summary>
+        public string ErrorCode => $"{NeoBuildErrorCodes.StringPrefix}{ExitCode:d04}";
+
+        /// <summary>
+        /// A description of the root cause of the problem.
+        /// </summary>
+        public override string Message => InnerException is null ?
+            $"{ErrorCode} {errorMessage}" :
+            $"{ErrorCode} {errorMessage} {InnerException}";
+
+        public override string ToString() =>
+            Message;
+    }
+}
