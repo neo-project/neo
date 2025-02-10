c62f0caafed0712cb80c4821643535dc6642c3b7
Fernando Diaz Toledano <shargon@gmail.com>
2025-02-10 14:30:38 +0100

    fix header

--- src/Neo/Persistence/StoreCache.cs
+++ src/Neo/Persistence/StoreCache.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2025 The Neo Project.
 //
-// SnapshotCache.cs file belongs to the neo project and is free
+// StoreCache.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
 // repository or http://www.opensource.org/licenses/mit-license.php
