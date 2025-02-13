46f92c40763264b9745484046e8007a8613e57d7
Fernando Diaz Toledano <shargon@gmail.com>
2025-02-13 16:06:04 +0100

    Fix clone cache tests

--- tests/Neo.UnitTests/Persistence/UT_CloneCache.cs
+++ tests/Neo.UnitTests/Persistence/UT_CloneCache.cs
@@ -24,121 +24,131 @@ namespace Neo.UnitTests.IO.Caching
     [TestClass]
     public class UT_CloneCache
     {
-        private readonly MemoryStore store = new();
-        private StoreCache myDataCache;
-        private ClonedCache clonedCache;
+        private readonly MemoryStore _store = new();
 
-        private static readonly StorageKey key1 = new() { Id = 0, Key = Encoding.UTF8.GetBytes("key1") };
-        private static readonly StorageKey key2 = new() { Id = 0, Key = Encoding.UTF8.GetBytes("key2") };
-        private static readonly StorageKey key3 = new() { Id = 0, Key = Encoding.UTF8.GetBytes("key3") };
-        private static readonly StorageKey key4 = new() { Id = 0, Key = Encoding.UTF8.GetBytes("key4") };
+        private static readonly StorageKey s_key1 = new() { Id = 0, Key = Encoding.UTF8.GetBytes("key1") };
+        private static readonly StorageKey s_key2 = new() { Id = 0, Key = Encoding.UTF8.GetBytes("key2") };
+        private static readonly StorageKey s_key3 = new() { Id = 0, Key = Encoding.UTF8.GetBytes("key3") };
+        private static readonly StorageKey s_key4 = new() { Id = 0, Key = Encoding.UTF8.GetBytes("key4") };
 
-        private static readonly StorageItem value1 = new(Encoding.UTF8.GetBytes("value1"));
-        private static readonly StorageItem value2 = new(Encoding.UTF8.GetBytes("value2"));
-        private static readonly StorageItem value3 = new(Encoding.UTF8.GetBytes("value3"));
-        private static readonly StorageItem value4 = new(Encoding.UTF8.GetBytes("value4"));
-
-        [TestInitialize]
-        public void Init()
-        {
-            myDataCache = new(store, false);
-            clonedCache = new ClonedCache(myDataCache);
-        }
+        private static readonly StorageItem s_value1 = new(Encoding.UTF8.GetBytes("value1"));
+        private static readonly StorageItem s_value2 = new(Encoding.UTF8.GetBytes("value2"));
+        private static readonly StorageItem s_value3 = new(Encoding.UTF8.GetBytes("value3"));
 
         [TestMethod]
         public void TestCloneCache()
         {
+            var myDataCache = new StoreCache(_store);
+            var clonedCache = myDataCache.CloneCache();
             Assert.IsNotNull(clonedCache);
         }
 
         [TestMethod]
         public void TestAddInternal()
         {
-            clonedCache.Add(key1, value1);
-            Assert.AreEqual(value1, clonedCache[key1]);
+            var myDataCache = new StoreCache(_store);
+            var clonedCache = myDataCache.CloneCache();
+
+            clonedCache.Add(s_key1, s_value1);
+            Assert.AreEqual(s_value1, clonedCache[s_key1]);
 
             clonedCache.Commit();
-            Assert.IsTrue(myDataCache[key1].Value.Span.SequenceEqual(value1.Value.Span));
+            Assert.IsTrue(myDataCache[s_key1].Value.Span.SequenceEqual(s_value1.Value.Span));
         }
 
         [TestMethod]
         public void TestDeleteInternal()
         {
-            myDataCache.Add(key1, value1);
-            clonedCache.Delete(key1);   //  trackable.State = TrackState.Deleted
+            var myDataCache = new StoreCache(_store);
+            var clonedCache = myDataCache.CloneCache();
+
+            myDataCache.Add(s_key1, s_value1);
+            clonedCache.Delete(s_key1);   //  trackable.State = TrackState.Deleted
             clonedCache.Commit();
 
-            Assert.IsNull(clonedCache.TryGet(key1));
-            Assert.IsNull(myDataCache.TryGet(key1));
+            Assert.IsNull(clonedCache.TryGet(s_key1));
+            Assert.IsNull(myDataCache.TryGet(s_key1));
         }
 
         [TestMethod]
         public void TestFindInternal()
         {
-            clonedCache.Add(key1, value1);
-            myDataCache.Add(key2, value2);
-            store.Put(key3.ToArray(), value3.ToArray());
+            var myDataCache = new StoreCache(_store);
+            var clonedCache = myDataCache.CloneCache();
 
-            var items = clonedCache.Find(key1.ToArray());
-            Assert.AreEqual(key1, items.ElementAt(0).Key);
-            Assert.AreEqual(value1, items.ElementAt(0).Value);
+            clonedCache.Add(s_key1, s_value1);
+            myDataCache.Add(s_key2, s_value2);
+            _store.Put(s_key3.ToArray(), s_value3.ToArray());
+
+            var items = clonedCache.Find(s_key1.ToArray());
+            Assert.AreEqual(s_key1, items.ElementAt(0).Key);
+            Assert.AreEqual(s_value1, items.ElementAt(0).Value);
             Assert.AreEqual(1, items.Count());
 
-            items = clonedCache.Find(key2.ToArray());
-            Assert.AreEqual(key2, items.ElementAt(0).Key);
-            Assert.IsTrue(value2.EqualsTo(items.ElementAt(0).Value));
+            items = clonedCache.Find(s_key2.ToArray());
+            Assert.AreEqual(s_key2, items.ElementAt(0).Key);
+            Assert.IsTrue(s_value2.EqualsTo(items.ElementAt(0).Value));
             Assert.AreEqual(1, items.Count());
 
-            items = clonedCache.Find(key3.ToArray());
-            Assert.AreEqual(key3, items.ElementAt(0).Key);
-            Assert.IsTrue(value3.EqualsTo(items.ElementAt(0).Value));
+            items = clonedCache.Find(s_key3.ToArray());
+            Assert.AreEqual(s_key3, items.ElementAt(0).Key);
+            Assert.IsTrue(s_value3.EqualsTo(items.ElementAt(0).Value));
             Assert.AreEqual(1, items.Count());
 
-            items = clonedCache.Find(key4.ToArray());
+            items = clonedCache.Find(s_key4.ToArray());
             Assert.AreEqual(0, items.Count());
         }
 
         [TestMethod]
         public void TestGetInternal()
         {
-            clonedCache.Add(key1, value1);
-            myDataCache.Add(key2, value2);
-            store.Put(key3.ToArray(), value3.ToArray());
+            var myDataCache = new StoreCache(_store);
+            var clonedCache = myDataCache.CloneCache();
 
-            Assert.IsTrue(value1.EqualsTo(clonedCache[key1]));
-            Assert.IsTrue(value2.EqualsTo(clonedCache[key2]));
-            Assert.IsTrue(value3.EqualsTo(clonedCache[key3]));
+            clonedCache.Add(s_key1, s_value1);
+            myDataCache.Add(s_key2, s_value2);
+            _store.Put(s_key3.ToArray(), s_value3.ToArray());
 
-            Action action = () =>
+            Assert.IsTrue(s_value1.EqualsTo(clonedCache[s_key1]));
+            Assert.IsTrue(s_value2.EqualsTo(clonedCache[s_key2]));
+            Assert.IsTrue(s_value3.EqualsTo(clonedCache[s_key3]));
+
+            void Action()
             {
-                var item = clonedCache[key4];
-            };
-            Assert.ThrowsException<KeyNotFoundException>(action);
+                var item = clonedCache[s_key4];
+            }
+            Assert.ThrowsException<KeyNotFoundException>(Action);
         }
 
         [TestMethod]
         public void TestTryGetInternal()
         {
-            clonedCache.Add(key1, value1);
-            myDataCache.Add(key2, value2);
-            store.Put(key3.ToArray(), value3.ToArray());
+            var myDataCache = new StoreCache(_store);
+            var clonedCache = myDataCache.CloneCache();
 
-            Assert.IsTrue(value1.EqualsTo(clonedCache.TryGet(key1)));
-            Assert.IsTrue(value2.EqualsTo(clonedCache.TryGet(key2)));
-            Assert.IsTrue(value3.EqualsTo(clonedCache.TryGet(key3)));
-            Assert.IsNull(clonedCache.TryGet(key4));
+            clonedCache.Add(s_key1, s_value1);
+            myDataCache.Add(s_key2, s_value2);
+            _store.Put(s_key3.ToArray(), s_value3.ToArray());
+
+            Assert.IsTrue(s_value1.EqualsTo(clonedCache.TryGet(s_key1)));
+            Assert.IsTrue(s_value2.EqualsTo(clonedCache.TryGet(s_key2)));
+            Assert.IsTrue(s_value3.EqualsTo(clonedCache.TryGet(s_key3)));
+            Assert.IsNull(clonedCache.TryGet(s_key4));
         }
 
         [TestMethod]
         public void TestUpdateInternal()
         {
-            clonedCache.Add(key1, value1);
-            myDataCache.Add(key2, value2);
-            store.Put(key3.ToArray(), value3.ToArray());
+            var myDataCache = new StoreCache(_store);
+            var clonedCache = myDataCache.CloneCache();
 
-            clonedCache.GetAndChange(key1).Value = Encoding.Default.GetBytes("value_new_1");
-            clonedCache.GetAndChange(key2).Value = Encoding.Default.GetBytes("value_new_2");
-            clonedCache.GetAndChange(key3).Value = Encoding.Default.GetBytes("value_new_3");
+            clonedCache.Add(s_key1, s_value1);
+            myDataCache.Add(s_key2, s_value2);
+            _store.Put(s_key3.ToArray(), s_value3.ToArray());
+
+            clonedCache.GetAndChange(s_key1).Value = Encoding.Default.GetBytes("value_new_1");
+            clonedCache.GetAndChange(s_key2).Value = Encoding.Default.GetBytes("value_new_2");
+            clonedCache.GetAndChange(s_key3).Value = Encoding.Default.GetBytes("value_new_3");
 
             clonedCache.Commit();
 
@@ -146,10 +156,10 @@ namespace Neo.UnitTests.IO.Caching
             StorageItem value_new_2 = new(Encoding.UTF8.GetBytes("value_new_2"));
             StorageItem value_new_3 = new(Encoding.UTF8.GetBytes("value_new_3"));
 
-            Assert.IsTrue(value_new_1.EqualsTo(clonedCache[key1]));
-            Assert.IsTrue(value_new_2.EqualsTo(clonedCache[key2]));
-            Assert.IsTrue(value_new_3.EqualsTo(clonedCache[key3]));
-            Assert.IsTrue(value_new_2.EqualsTo(clonedCache[key2]));
+            Assert.IsTrue(value_new_1.EqualsTo(clonedCache[s_key1]));
+            Assert.IsTrue(value_new_2.EqualsTo(clonedCache[s_key2]));
+            Assert.IsTrue(value_new_3.EqualsTo(clonedCache[s_key3]));
+            Assert.IsTrue(value_new_2.EqualsTo(clonedCache[s_key2]));
         }
 
         [TestMethod]
