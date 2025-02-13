785506bc97139dbe0443ca57ea6c402fcc49f4a7
Christopher Schuchardt <cschuchardt88@gmail.com>
2025-02-13 06:18:30 -0500

    [`Add`] Neo Build Error Base - Part 2 (#3730)
    * Init Neo Build Project
    
    * Add `Neo.Build.Core.Tests` Project
    
    * Add Project References to `Neo.Build.Core.Tests`
    
    * Neo Build Error Definition
    
    * fixed typos
    
    * updated text to be more clear
    
    * typos
    
    * Added more clear requirements in section `Factors`
    
    * typo
    
    * Add Neo Build Error Base Class
    
    * Add `NeoBuildException` tests
    
    * Add test `TestIsPropertiesSetForSystemException`
    
    * typo
    
    * Fixed error message
    
    * Added `NeoBuildFileAccessDeniedException`
    
    * Revert "Added `NeoBuildFileAccessDeniedException`"
    
    This reverts commit 027dafeb0e0fafe2fdc112393928713fbe4274eb.
    
    * Fixed letters
    
    * Changed to `SystemException`

---
+++ src/Shared/Neo.Build.Core/Exceptions/NeoBuildException.cs
@@ -0,0 +1,41 @@
+// Copyright (C) 2015-2025 The Neo Project.
+//
+// NeoBuildException.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using System;
+
+namespace Neo.Build.Core.Exceptions
+{
+    public class NeoBuildException(
+        string errorMessage,
+        int errorCode = NeoBuildErrorCodes.General.InternalException) : SystemException(), INeoBuildException
+    {
+        public NeoBuildException(Exception exception) : this(exception.Message, exception.HResult) { }
+        public NeoBuildException(Exception exception, int exitCode) : this(exception.Message, exitCode) { }
+
+        /// <summary>
+        /// Exit code from the build process.
+        /// </summary>
+        public int ExitCode => HResult = errorCode;
+
+        /// <summary>
+        /// An error code for referencing the problem.
+        /// </summary>
+        public string ErrorCode => $"{NeoBuildErrorCodes.StringPrefix}{(uint)ExitCode:d04}";
+
+        /// <summary>
+        /// A description of the root cause of the problem.
+        /// </summary>
+        public override string Message => $"Error {ErrorCode} {errorMessage}";
+
+        public override string ToString() =>
+            Message;
+    }
+}
