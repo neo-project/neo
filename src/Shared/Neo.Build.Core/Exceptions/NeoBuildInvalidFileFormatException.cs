d535a073a6ddff13e884744b3ba1fa1ac46e7ceb
Christopher Schuchardt <cschuchardt88@gmail.com>
2025-02-13 06:29:46 -0500

    [`Add`] Invalid File Format exception - Part 3 (#3750)
    * Init Neo Build Project
    
    * Add `Neo.Build.Core.Tests` Project
    
    * Add Project References to `Neo.Build.Core.Tests`
    
    * Neo Build Error Definition
    
    * fixed typos
    
    * updated text to be more clear
    
    * typos
    
    * Added more clear requirements in section `Factors`
    
    * typo
    
    * Add Neo Build Error Base Class
    
    * Add `NeoBuildException` tests
    
    * Add test `TestIsPropertiesSetForSystemException`
    
    * typo
    
    * Fixed error message
    
    * Added `NeoBuildFileAccessDeniedException`
    
    * Revert "Added `NeoBuildFileAccessDeniedException`"
    
    This reverts commit 027dafeb0e0fafe2fdc112393928713fbe4274eb.
    
    * Fixed letters
    
    * Changed to `SystemException`
    
    * Added `NeoBuildInvalidFileFormatException`

---
+++ src/Shared/Neo.Build.Core/Exceptions/NeoBuildInvalidFileFormatException.cs
@@ -0,0 +1,19 @@
+// Copyright (C) 2015-2025 The Neo Project.
+//
+// NeoBuildInvalidFileFormatException.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+namespace Neo.Build.Core.Exceptions
+{
+    public class NeoBuildInvalidFileFormatException(
+        string filename) : NeoBuildException($"File '{filename}' is not in the correct format.", NeoBuildErrorCodes.General.InvalidFileFormat)
+    {
+        public string Filename => filename;
+    }
+}
