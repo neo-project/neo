167c5e48d71e6477ed9f1de3c9fc2c41d59f6a8f
Shargon <shargon@gmail.com>
2025-02-14 18:13:31 +0100

    Remove CleanUp

--- tests/Neo.UnitTests/Network/P2P/UT_RemoteNodeMailbox.cs
+++ tests/Neo.UnitTests/Network/P2P/UT_RemoteNodeMailbox.cs
@@ -24,12 +24,6 @@ namespace Neo.UnitTests.Network.P2P
     {
         RemoteNodeMailbox uut;
 
-        [TestCleanup]
-        public void Cleanup()
-        {
-
-        }
-
         [TestInitialize]
         public void TestSetup()
         {
