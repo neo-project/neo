f5e85eeb8a2a6782c8beb274855700559381d8e7
Christopher Schuchardt <cschuchardt88@gmail.com>
2025-02-17 01:29:24 -0500

    Add jsonmodel

---
+++ src/Shared/Neo.Build.Core/Models/JsonModel.cs
@@ -0,0 +1,46 @@
+// Copyright (C) 2015-2025 The Neo Project.
+//
+// JsonModel.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using System.IO;
+using System.Text.Json;
+
+namespace Neo.Build.Core.Models
+{
+    public abstract class JsonModel
+    {
+        protected JsonSerializerOptions _jsonSerializerOptions = NeoBuildDefaults.JsonDefaultSerializerOptions;
+
+        public abstract override string? ToString();
+
+        public abstract string ToJson(JsonSerializerOptions? options);
+
+        public static T? FromJson<T>(string jsonString, JsonSerializerOptions? options = default)
+            where T : notnull, JsonModel =>
+            JsonSerializer.Deserialize<T>(
+                jsonString,
+                options ?? NeoBuildDefaults.JsonDefaultSerializerOptions);
+
+        public static T? FromJson<T>(FileInfo file, JsonSerializerOptions? options = default)
+            where T : notnull, JsonModel
+        {
+            if (file.Exists == false)
+                throw new NeoBuildFileNotFoundException(file);
+
+            var jsonOptions = options ?? NeoBuildDefaults.JsonDefaultSerializerOptions;
+            var jsonString = File.ReadAllText(file.FullName);
+
+            if (string.IsNullOrEmpty(jsonString))
+                throw new NeoBuildFileNotFoundException(file);
+
+            return FromJson<T>(jsonString, jsonOptions);
+        }
+    }
+}
---
+++ src/Shared/Neo.Build.Core/NeoBuildDefaults.cs
@@ -0,0 +1,37 @@
+// Copyright (C) 2015-2025 The Neo Project.
+//
+// NeoBuildDefaults.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Neo.Build.Core
+{
+    public static class NeoBuildDefaults
+    {
+        public static readonly JsonSerializerOptions JsonDefaultSerializerOptions = new()
+        {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+            ReadCommentHandling = JsonCommentHandling.Disallow,
+            UnknownTypeHandling = JsonUnknownTypeHandling.JsonNode,
+            PreferredObjectCreationHandling = JsonObjectCreationHandling.Replace,
+            UnmappedMemberHandling = JsonUnmappedMemberHandling.Skip,
+            WriteIndented = true,
+            RespectNullableAnnotations = true,
+            Converters =
+            {
+                new JsonStringEnumConverter(),
+            }
+        };
+    }
+}
