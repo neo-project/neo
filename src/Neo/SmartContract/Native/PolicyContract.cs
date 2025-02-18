7d73ebe0c43b58dd0b86551168bde854cd8741ae
Shargon <shargon@gmail.com>
2025-02-18 01:33:13 -0800

    Optimize `StorageKey` creation (#3756)
    * Optimize key builder
    
    * Optimize with GetSpan
    
    * Use Span
    
    * Clean
    
    * Rename
    
    * Update src/Neo/SmartContract/KeyBuilder.cs
    
    Co-authored-by: nan01ab <yjcc201374@outlook.com>
    
    * Update KeyBuilder.cs
    
    * @cschuchardt88 feedback
    
    * Avoid ToArray in StorageKey constructor
    
    * Use sizeof
    
    * Optimize ECPoint
    
    * Optimize ECPoint
    
    * Add ISerializableSpan
    
    * Move to StorageKey
    
    * Use ReadOnlySpan
    
    ---------
    
    Co-authored-by: nan01ab <yjcc201374@outlook.com>
    Co-authored-by: Christopher Schuchardt <cschuchardt88@gmail.com>

--- src/Neo/SmartContract/Native/PolicyContract.cs
+++ src/Neo/SmartContract/Native/PolicyContract.cs
@@ -167,7 +167,7 @@ namespace Neo.SmartContract.Native
             if (!Enum.IsDefined(typeof(TransactionAttributeType), attributeType) || (!allowNotaryAssisted && attributeType == (byte)(TransactionAttributeType.NotaryAssisted)))
                 throw new InvalidOperationException($"Unsupported value {attributeType} of {nameof(attributeType)}");
 
-            var key = CreateStorageKey(Prefix_AttributeFee).Add(attributeType);
+            var key = CreateStorageKey(Prefix_AttributeFee, attributeType);
             return snapshot.TryGet(key, out var item) ? (uint)(BigInteger)item : DefaultAttributeFee;
         }
 
@@ -180,7 +180,7 @@ namespace Neo.SmartContract.Native
         [ContractMethod(CpuFee = 1 << 15, RequiredCallFlags = CallFlags.ReadStates)]
         public bool IsBlocked(IReadOnlyStore snapshot, UInt160 account)
         {
-            return snapshot.Contains(CreateStorageKey(Prefix_BlockedAccount).Add(account));
+            return snapshot.Contains(CreateStorageKey(Prefix_BlockedAccount, account));
         }
 
         /// <summary>
@@ -225,7 +225,7 @@ namespace Neo.SmartContract.Native
             if (value > MaxAttributeFee) throw new ArgumentOutOfRangeException(nameof(value));
             if (!CheckCommittee(engine)) throw new InvalidOperationException();
 
-            engine.SnapshotCache.GetAndChange(CreateStorageKey(Prefix_AttributeFee).Add(attributeType), () => new StorageItem(DefaultAttributeFee)).Set(value);
+            engine.SnapshotCache.GetAndChange(CreateStorageKey(Prefix_AttributeFee, attributeType), () => new StorageItem(DefaultAttributeFee)).Set(value);
         }
 
         [ContractMethod(CpuFee = 1 << 15, RequiredCallFlags = CallFlags.States)]
@@ -263,7 +263,7 @@ namespace Neo.SmartContract.Native
         {
             if (IsNative(account)) throw new InvalidOperationException("It's impossible to block a native contract.");
 
-            var key = CreateStorageKey(Prefix_BlockedAccount).Add(account);
+            var key = CreateStorageKey(Prefix_BlockedAccount, account);
             if (snapshot.Contains(key)) return false;
 
             snapshot.Add(key, new StorageItem(Array.Empty<byte>()));
@@ -275,7 +275,7 @@ namespace Neo.SmartContract.Native
         {
             if (!CheckCommittee(engine)) throw new InvalidOperationException();
 
-            var key = CreateStorageKey(Prefix_BlockedAccount).Add(account);
+            var key = CreateStorageKey(Prefix_BlockedAccount, account);
             if (!engine.SnapshotCache.Contains(key)) return false;
 
             engine.SnapshotCache.Delete(key);
