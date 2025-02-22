398b4ce9289672800ec102d4c28c56be214ff2e9
Christopher Schuchardt <cschuchardt88@gmail.com>
2025-02-22 09:18:22 -0500

    [`Add`] Neo Toolset Application (#3780)
    * Added Neo.Build.ToolSet
    
    * Added basic setup for nebuild executable

---
+++ src/Shared/Neo.Build.ToolSet/Commands/ProgramRootCommand.cs
@@ -0,0 +1,37 @@
+// Copyright (C) 2015-2025 The Neo Project.
+//
+// ProgramRootCommand.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+
+namespace Neo.Build.ToolSet.Commands
+{
+    internal class ProgramRootCommand : RootCommand
+    {
+        public ProgramRootCommand() : base("Neo Build Command-line Tool")
+        {
+        }
+
+        public new sealed class Handler : ICommandHandler
+        {
+            public int Invoke(InvocationContext context) =>
+                InvokeAsync(context).GetAwaiter().GetResult();
+
+            public Task<int> InvokeAsync(InvocationContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+}
