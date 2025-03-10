eeade94072d737936fb39995ceddbc202643c51a
Shargon <shargon@gmail.com>
2025-03-10 10:00:43 +0100

    Nullable for `HashIndexState` and `TransactionState`  (#3801)
    * Fix interop serialization
    
    * Clean
    
    * Ensure is valid
    
    * Clean
    
    * Fix new class
    
    * Clean HashIndexState
    
    * Update src/Neo/SmartContract/Native/HashIndexState.cs
    
    * TransactionState nullable
    
    * Clean
    
    * fix
    
    * Revert storageItem changes
    
    * optimize
    
    * Revert some changes
    
    ---------
    
    Co-authored-by: Vitor Nazário Coelho <vncoelho@gmail.com>
    Co-authored-by: Christopher Schuchardt <cschuchardt88@gmail.com>

--- src/Neo.CLI/CLI/MainService.Blockchain.cs
+++ src/Neo.CLI/CLI/MainService.Blockchain.cs
@@ -119,7 +119,7 @@ namespace Neo.CLI
             {
                 var tx = NativeContract.Ledger.GetTransactionState(NeoSystem.StoreView, hash);
 
-                if (tx is null)
+                if (tx?.Transaction is null)
                 {
                     ConsoleHelper.Error($"Transaction {hash} doesn't exist.");
                     return;
@@ -127,7 +127,7 @@ namespace Neo.CLI
 
                 var block = NativeContract.Ledger.GetHeader(NeoSystem.StoreView, tx.BlockIndex);
 
-                DateTime transactionDatetime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                var transactionDatetime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
                 transactionDatetime = transactionDatetime.AddMilliseconds(block.Timestamp).ToLocalTime();
 
                 ConsoleHelper.Info("", "-------------", "Transaction", "-------------");
