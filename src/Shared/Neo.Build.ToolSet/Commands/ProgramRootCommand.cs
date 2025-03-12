1b4acc8a58aef47780c84d872eb7f75797a43475
Christopher Schuchardt <cschuchardt88@gmail.com>
2025-03-12 03:42:54 -0400

    Merge `toolset-configurations`

--- src/Shared/Neo.Build.ToolSet/Commands/ProgramRootCommand.cs
+++ src/Shared/Neo.Build.ToolSet/Commands/ProgramRootCommand.cs
@@ -11,6 +11,7 @@
 
 using Microsoft.Extensions.Hosting;
 using Neo.Build.ToolSet.Extensions;
+using Neo.Build.ToolSet.Options;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.Threading.Tasks;
@@ -23,9 +24,12 @@ namespace Neo.Build.ToolSet.Commands
         {
         }
 
-        public new sealed class Handler(IHostEnvironment env) : ICommandHandler
+        public new sealed class Handler(
+            IHostEnvironment env,
+            NeoSystemOptions neoSystemOptions) : ICommandHandler
         {
             private readonly IHostEnvironment _env = env;
+            private readonly NeoSystemOptions _neoSystemOptions = neoSystemOptions;
 
             public int Invoke(InvocationContext context) =>
                 InvokeAsync(context).GetAwaiter().GetResult();
@@ -33,7 +37,14 @@ namespace Neo.Build.ToolSet.Commands
             public Task<int> InvokeAsync(InvocationContext context)
             {
                 context.Console.WriteLine("      Environment: {0}", _env.EnvironmentName);
-                context.Console.WriteLine("Working Directory: {0}", _env.ContentRootPath);
+                context.Console.WriteLine("     Content Root: {0}", _env.ContentRootPath);
+                context.Console.WriteLine("       Store Root: {0}", _neoSystemOptions.Storage.StoreRoot);
+                context.Console.WriteLine("  Checkpoint Root: {0}", _neoSystemOptions.Storage.CheckPointRoot);
+                context.Console.WriteLine("           Listen: {0}", _neoSystemOptions.Network.Listen);
+                context.Console.WriteLine("             Port: {0}", _neoSystemOptions.Network.Port);
+                context.Console.WriteLine("  Max Connections: {0}", _neoSystemOptions.Network.MaxConnections);
+                context.Console.WriteLine("  Min Connections: {0}", _neoSystemOptions.Network.MinDesiredConnections);
+                context.Console.WriteLine("    Max Addresses: {0}", _neoSystemOptions.Network.MaxConnectionsPerAddress);
                 context.Console.WriteLine();
 
                 return Task.FromResult(0);
