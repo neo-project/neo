neo-project/neo

commit 0284512ae9ccb695863934df7cbe47af09d880b7

apply all suggestions

src/Plugins/DBFTPlugin/Consensus/ConsensusService.cs
@@ -40,6 +40,7 @@ namespace Neo.Plugins.DBFTPlugin.Consensus
         private uint onPrepareBlockIndex;
         private uint block_received_index;
         private bool started = false;
+        private TimeSpan blockGenTime;
 
         /// <summary>
         /// This will record the information from last scheduled timer
@@ -81,11 +82,6 @@ namespace Neo.Plugins.DBFTPlugin.Consensus
             InitializeConsensus(0);
         }
 
-        private TimeSpan GetBlockGenTime()
-        {
-            return neoSystem.GetBlockGenTime();
-        }
-
         private void InitializeConsensus(byte viewNumber)
         {
             context.Reset(viewNumber);
@@ -93,33 +89,30 @@ namespace Neo.Plugins.DBFTPlugin.Consensus
                 Log($"View changed: view={viewNumber} primary={context.Validators[context.GetPrimaryIndex((byte)(viewNumber - 1u))]}", LogLevel.Warning);
             Log($"Initialize: height={context.Block.Index} view={viewNumber} index={context.MyIndex} role={(context.IsPrimary ? "Primary" : context.WatchOnly ? "WatchOnly" : "Backup")}");
             if (context.WatchOnly) return;
+            blockGenTime = neoSystem.GetBlockGenTime();
             if (context.IsPrimary)
             {
                 if (isRecovering)
                 {
-                    TimeSpan blockTime = GetBlockGenTime();
-                    ChangeTimer(TimeSpan.FromMilliseconds((int)blockTime.TotalMilliseconds << (viewNumber + 1)));
+                    ChangeTimer(TimeSpan.FromMilliseconds((int)blockGenTime.TotalMilliseconds << (viewNumber + 1)));
                 }
                 else
                 {
-                    TimeSpan span = GetBlockGenTime();
-
                     if (block_received_index + 1 == context.Block.Index && onPrepareBlockIndex + 1 == context.Block.Index)
                     {
                         // Include the consensus time into the consensus intervals.
                         var diff = TimeProvider.Current.UtcNow - onPrepareReceivedTime;
-                        if (diff >= span)
-                            span = TimeSpan.Zero;
+                        if (diff >= blockGenTime)
+                            blockGenTime = TimeSpan.Zero;
                         else
-                            span -= diff;
+                            blockGenTime -= diff;
                     }
-                    ChangeTimer(span);
+                    ChangeTimer(blockGenTime);
                 }
             }
             else
             {
-                TimeSpan blockTime = GetBlockGenTime();
-                ChangeTimer(TimeSpan.FromMilliseconds((int)blockTime.TotalMilliseconds << (viewNumber + 1)));
+                ChangeTimer(TimeSpan.FromMilliseconds((int)blockGenTime.TotalMilliseconds << (viewNumber + 1)));
             }
         }
 
@@ -192,8 +185,7 @@ namespace Neo.Plugins.DBFTPlugin.Consensus
                     // Re-send commit periodically by sending recover message in case of a network issue.
                     Log($"Sending {nameof(RecoveryMessage)} to resend {nameof(Commit)}");
                     localNode.Tell(new LocalNode.SendDirectly { Inventory = context.MakeRecoveryMessage() });
-                    TimeSpan blockTime = GetBlockGenTime();
-                    ChangeTimer(TimeSpan.FromMilliseconds((int)blockTime.TotalMilliseconds << 1));
+                    ChangeTimer(blockGenTime);
                 }
                 else
                 {
@@ -222,8 +214,7 @@ namespace Neo.Plugins.DBFTPlugin.Consensus
                 foreach (InvPayload payload in InvPayload.CreateGroup(InventoryType.TX, context.TransactionHashes))
                     localNode.Tell(Message.Create(MessageCommand.Inv, payload));
             }
-            TimeSpan blockTime = GetBlockGenTime();
-            ChangeTimer(TimeSpan.FromMilliseconds(((int)blockTime.TotalMilliseconds << (context.ViewNumber + 1)) - (context.ViewNumber == 0 ? blockTime.TotalMilliseconds : 0)));
+            ChangeTimer(blockGenTime);
         }
 
         private void RequestRecovery()
@@ -240,8 +231,7 @@ namespace Neo.Plugins.DBFTPlugin.Consensus
             // The latter may happen by nodes in higher views with, at least, `M` proofs
             byte expectedView = context.ViewNumber;
             expectedView++;
-            TimeSpan blockTime = GetBlockGenTime();
-            ChangeTimer(TimeSpan.FromMilliseconds((int)blockTime.TotalMilliseconds << (expectedView + 1)));
+            ChangeTimer(blockGenTime);
             if ((context.CountCommitted + context.CountFailed) > context.F)
             {
                 RequestRecovery();
@@ -334,7 +324,7 @@ namespace Neo.Plugins.DBFTPlugin.Consensus
         // this function increases existing timer (never decreases) with a value proportional to `maxDelayInBlockTimes`*`Blockchain.MillisecondsPerBlock`
         private void ExtendTimerByFactor(int maxDelayInBlockTimes)
         {
-            TimeSpan nextDelay = expected_delay - (TimeProvider.Current.UtcNow - clock_started) + TimeSpan.FromMilliseconds(maxDelayInBlockTimes * GetBlockGenTime().TotalMilliseconds / (double)context.M);
+            TimeSpan nextDelay = expected_delay - (TimeProvider.Current.UtcNow - clock_started) + TimeSpan.FromMilliseconds(maxDelayInBlockTimes * blockGenTime.TotalMilliseconds / (double)context.M);
             if (!context.WatchOnly && !context.ViewChanging && !context.CommitSent && (nextDelay > TimeSpan.Zero))
                 ChangeTimer(nextDelay);
         }

src/Neo/SmartContract/Native/PolicyContract.cs
@@ -68,7 +68,7 @@ namespace Neo.SmartContract.Native
         /// <summary>
         /// The minimum block generation time that the committee can set in milliseconds.
         /// </summary>
-        public const uint MinBlockGenTime = 1000;
+        public const uint MinBlockGenTime = 100;
 
         /// <summary>
         /// The maximum block generation time that the committee can set in milliseconds.
@@ -179,7 +179,6 @@ namespace Neo.SmartContract.Native
         }
 
         /// <summary>
-        /// Gets the fee for attribute.
         /// Gets the fee for attribute before Echidna hardfork.
         /// </summary>
         /// <param name="snapshot">The snapshot used to read data.</param>
@@ -244,8 +243,8 @@ namespace Neo.SmartContract.Native
         [ContractMethod(Hardfork.HF_Echidna, CpuFee = 1 << 15, RequiredCallFlags = CallFlags.States | CallFlags.AllowNotify)]
         public void SetMSPerBlock(ApplicationEngine engine, uint milliseconds)
         {
-            if (milliseconds is <= 0 or > MaxBlockGenTime)
-                throw new ArgumentOutOfRangeException(nameof(milliseconds), $"MSPerBlock should be positive and not greater than {MaxBlockGenTime}, got {milliseconds}");
+            if (milliseconds is <= MinBlockGenTime or > MaxBlockGenTime)
+                throw new ArgumentOutOfRangeException(nameof(milliseconds), $"MSPerBlock should be between {MinBlockGenTime} and {MaxBlockGenTime}, got {milliseconds}");
             if (!CheckCommittee(engine)) throw new InvalidOperationException("invalid committee signature");
 
             // Since this method is only available after the HF_Echidna,

src/Plugins/DBFTPlugin/Consensus/ConsensusService.Check.cs
@@ -95,8 +95,7 @@ namespace Neo.Plugins.DBFTPlugin.Consensus
                 context.Save();
                 localNode.Tell(new LocalNode.SendDirectly { Inventory = payload });
                 // Set timer, so we will resend the commit in case of a networking issue
-                TimeSpan blockTime = GetBlockGenTime();
-                ChangeTimer(blockTime);
+                ChangeTimer(blockGenTime);
                 CheckCommits();
             }
         }

src/Plugins/DBFTPlugin/Consensus/ConsensusService.OnMessage.cs
@@ -82,8 +82,7 @@ namespace Neo.Plugins.DBFTPlugin.Consensus
             if (message.Version != context.Block.Version || message.PrevHash != context.Block.PrevHash) return;
             if (message.TransactionHashes.Length > neoSystem.Settings.MaxTransactionsPerBlock) return;
             Log($"{nameof(OnPrepareRequestReceived)}: height={message.BlockIndex} view={message.ViewNumber} index={message.ValidatorIndex} tx={message.TransactionHashes.Length}");
-            TimeSpan blockTime = GetBlockGenTime();
-            if (message.Timestamp <= context.PrevHeader.Timestamp || message.Timestamp > TimeProvider.Current.UtcNow.AddMilliseconds(8 * blockTime.TotalMilliseconds).ToTimestampMS())
+            if (message.Timestamp <= context.PrevHeader.Timestamp || message.Timestamp > TimeProvider.Current.UtcNow.AddMilliseconds(8 * blockGenTime.TotalMilliseconds).ToTimestampMS())
             {
                 Log($"Timestamp incorrect: {message.Timestamp}", LogLevel.Warning);
                 return;

commit acf691a1361b2533cb30405ec35102013d6b65dc

Consensus: revert logic changes of timer reset
Timer reset is broken by
https://github.com/neo-project/neo/pull/3622/commits/0284512ae9ccb695863934df7cbe47af09d880b7,
revert these changes.

Signed-off-by: Anna Shaleva <shaleva.ann@nspcc.ru>

src/Plugins/DBFTPlugin/Consensus/ConsensusService.cs
@@ -184,7 +184,7 @@ namespace Neo.Plugins.DBFTPlugin.Consensus
                     // Re-send commit periodically by sending recover message in case of a network issue.
                     Log($"Sending {nameof(RecoveryMessage)} to resend {nameof(Commit)}");
                     localNode.Tell(new LocalNode.SendDirectly { Inventory = context.MakeRecoveryMessage() });
-                    ChangeTimer(context.BlockGenTime);
+                    ChangeTimer(TimeSpan.FromMilliseconds((int)context.BlockGenTime.TotalMilliseconds << 1));
                 }
                 else
                 {
@@ -213,7 +213,7 @@ namespace Neo.Plugins.DBFTPlugin.Consensus
                 foreach (InvPayload payload in InvPayload.CreateGroup(InventoryType.TX, context.TransactionHashes))
                     localNode.Tell(Message.Create(MessageCommand.Inv, payload));
             }
-            ChangeTimer(context.BlockGenTime);
+            ChangeTimer(TimeSpan.FromMilliseconds(((int)context.BlockGenTime.TotalMilliseconds << (context.ViewNumber + 1)) - (context.ViewNumber == 0 ? context.BlockGenTime.TotalMilliseconds : 0)));
         }
 
         private void RequestRecovery()
@@ -230,7 +230,7 @@ namespace Neo.Plugins.DBFTPlugin.Consensus
             // The latter may happen by nodes in higher views with, at least, `M` proofs
             byte expectedView = context.ViewNumber;
             expectedView++;
-            ChangeTimer(context.BlockGenTime);
+            ChangeTimer(TimeSpan.FromMilliseconds((int)context.BlockGenTime.TotalMilliseconds << (expectedView + 1)));
             if ((context.CountCommitted + context.CountFailed) > context.F)
             {
                 RequestRecovery();