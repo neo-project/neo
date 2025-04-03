abcb87790bdc33eabf004e0b6203f3ec4b19e6d6
Fernando Diaz Toledano <shargon@gmail.com>
2025-04-03 17:34:04 +0200

    Split files

--- fuzzers/Neo.VM.Fuzzer/Runners/StepEventArgs.cs
+++ fuzzers/Neo.VM.Fuzzer/Runners/StepEventArgs.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2025 The Neo Project.
 //
-// EventArgs.cs file belongs to the neo project and is free
+// StepEventArgs.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
 // repository or http://www.opensource.org/licenses/mit-license.php
@@ -8,8 +8,6 @@
 //
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
-
-using System;
 
 namespace Neo.VM.Fuzzer.Runners
 {
@@ -44,40 +42,6 @@ namespace Neo.VM.Fuzzer.Runners
             OpCode = opCode;
             InstructionPointer = instructionPointer;
             StackSize = stackSize;
-        }
-    }
-
-    /// <summary>
-    /// Provides data for the fault event in the execution engine
-    /// </summary>
-    public class FaultEventArgs : EventArgs
-    {
-        /// <summary>
-        /// Gets the type of exception that occurred
-        /// </summary>
-        public string ExceptionType { get; }
-
-        /// <summary>
-        /// Gets the detailed message of the exception
-        /// </summary>
-        public string ExceptionMessage { get; }
-
-        /// <summary>
-        /// Gets the instruction pointer position where the fault occurred
-        /// </summary>
-        public int InstructionPointer { get; }
-
-        /// <summary>
-        /// Initializes a new instance of the FaultEventArgs class
-        /// </summary>
-        /// <param name="exceptionType">The type of exception that occurred</param>
-        /// <param name="exceptionMessage">The detailed message of the exception</param>
-        /// <param name="instructionPointer">The instruction pointer position where the fault occurred</param>
-        public FaultEventArgs(string exceptionType, string exceptionMessage, int instructionPointer)
-        {
-            ExceptionType = exceptionType;
-            ExceptionMessage = exceptionMessage;
-            InstructionPointer = instructionPointer;
         }
     }
 }
