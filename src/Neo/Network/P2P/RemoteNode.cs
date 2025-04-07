008c130178266a03fc2019e647d3f20e1aa6a38a
Fernando Diaz Toledano <shargon@gmail.com>
2025-04-07 14:50:56 +0200

    divide by bucket

--- src/Neo/Network/P2P/RemoteNode.cs
+++ src/Neo/Network/P2P/RemoteNode.cs
@@ -82,8 +82,8 @@ namespace Neo.Network.P2P
         {
             this.system = system;
             this.localNode = localNode;
-            _knownHashes = new HashSetCache<UInt256>(config.MaxKnownHashes);
-            _sentHashes = new HashSetCache<UInt256>(config.MaxKnownHashes);
+            _knownHashes = new HashSetCache<UInt256>(config.MaxKnownHashes / 10, 10);
+            _sentHashes = new HashSetCache<UInt256>(config.MaxKnownHashes / 10, 10);
             localNode.RemoteNodes.TryAdd(Self, this);
         }
 
