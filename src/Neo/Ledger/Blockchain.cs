24085ba422ef763cf5ec3b1905273f8050803504
Shargon <shargon@gmail.com>
2025-04-14 20:36:49 +0200

    Update src/Neo/Ledger/Blockchain.cs

--- src/Neo/Ledger/Blockchain.cs
+++ src/Neo/Ledger/Blockchain.cs
@@ -23,7 +23,7 @@ using Neo.VM;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Diagnostics
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
