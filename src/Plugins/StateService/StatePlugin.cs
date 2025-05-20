a1e5761772ff0cc635d4b77e8fffebc6fb068fe4
Will <201105916+Wi1l-B0t@users.noreply.github.com>
2025-05-20 22:13:48 +0800

    Update src/Plugins/StateService/StatePlugin.cs
    Co-authored-by: Shargon <shargon@gmail.com>

--- src/Plugins/StateService/StatePlugin.cs
+++ src/Plugins/StateService/StatePlugin.cs
@@ -104,8 +104,7 @@ namespace Neo.Plugins.StateService
             if (system.Settings.Network != Settings.Default.Network) return;
             StateStore.Singleton.UpdateLocalStateRootSnapshot(block.Index,
                 snapshot.GetChangeSet()
-                    .Where(p => p.Value.State != TrackState.None)
-                    .Where(p => p.Key.Id != NativeContract.Ledger.Id)
+                    .Where(p => p.Value.State != TrackState.None && p.Key.Id != NativeContract.Ledger.Id)
                     .ToList());
         }
 
