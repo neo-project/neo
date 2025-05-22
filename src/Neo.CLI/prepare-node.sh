ed611790d918483f46e3c7a5d9f9af434f6a69ef
superboyiii <573504781@qq.com>
2025-05-22 11:08:38 +0800

    some improvement

--- src/Neo.CLI/prepare-node.sh
+++ src/Neo.CLI/prepare-node.sh
@@ -5,7 +5,7 @@ mkdir neo-cli-linux-x64
 tar -zxvf neo-cli.$1-linux-x64.tar.gz -C neo-cli-linux-x64/
 mv neo-cli-linux-x64 neo-cli
 
-
+# Allow CLI and Plugins in different versions in case only CLI is released or for any other test usage
 if [ -z "$2" ]; then
     echo "Downloading plugins $1"
     wget https://github.com/neo-project/neo/releases/download/$1/ApplicationLogs.zip
@@ -22,4 +22,4 @@ unzip -n ApplicationLogs.zip -d ./neo-cli/
 unzip -n RpcServer.zip -d ./neo-cli/
 unzip -n TokensTracker.zip -d ./neo-cli/
 
-echo "Node Ready!"
+echo "Node Is Ready!"
