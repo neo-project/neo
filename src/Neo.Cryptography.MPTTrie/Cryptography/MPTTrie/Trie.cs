67e49755222f345eca49d357c9819d6f16f8bc50
NGD Admin <154295625+NGDAdmin@users.noreply.github.com>
2025-05-22 14:05:35 +0800

    [`ut`] 100% Coverage Trie.Get (#3952) (#3957)
    * 100% Coverage Trie.Get
    
    * fix ut
    
    Co-authored-by: Shargon <shargon@gmail.com>

--- src/Neo.Cryptography.MPTTrie/Cryptography/MPTTrie/Trie.cs
+++ src/Neo.Cryptography.MPTTrie/Cryptography/MPTTrie/Trie.cs
@@ -17,24 +17,24 @@ namespace Neo.Cryptography.MPTTrie
     public partial class Trie
     {
         private const byte Prefix = 0xf0;
-        private readonly bool full;
-        private readonly IStoreSnapshot store;
-        private Node root;
-        private readonly Cache cache;
-        public Node Root => root;
+        private readonly bool _full;
+        private readonly IStoreSnapshot _store;
+        private Node _root;
+        private readonly Cache _cache;
+        public Node Root => _root;
 
         public Trie(IStoreSnapshot store, UInt256 root, bool full_state = false)
         {
-            this.store = store ?? throw new ArgumentNullException(nameof(store));
-            cache = new Cache(store, Prefix);
-            this.root = root is null ? new Node() : Node.NewHash(root);
-            full = full_state;
+            _store = store ?? throw new ArgumentNullException(nameof(store));
+            _cache = new Cache(store, Prefix);
+            _root = root is null ? new Node() : Node.NewHash(root);
+            _full = full_state;
         }
 
         private static byte[] ToNibbles(ReadOnlySpan<byte> path)
         {
             var result = new byte[path.Length * 2];
-            for (int i = 0; i < path.Length; i++)
+            for (var i = 0; i < path.Length; i++)
             {
                 result[i * 2] = (byte)(path[i] >> 4);
                 result[i * 2 + 1] = (byte)(path[i] & 0x0F);
@@ -46,7 +46,7 @@ namespace Neo.Cryptography.MPTTrie
         {
             if (path.Length % 2 != 0) throw new FormatException($"MPTTrie.FromNibbles invalid path.");
             var key = new byte[path.Length / 2];
-            for (int i = 0; i < key.Length; i++)
+            for (var i = 0; i < key.Length; i++)
             {
                 key[i] = (byte)(path[i * 2] << 4);
                 key[i] |= path[i * 2 + 1];
@@ -56,7 +56,7 @@ namespace Neo.Cryptography.MPTTrie
 
         public void Commit()
         {
-            cache.Commit();
+            _cache.Commit();
         }
     }
 }
