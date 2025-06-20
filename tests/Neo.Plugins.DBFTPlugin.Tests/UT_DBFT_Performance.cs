e4440964a7d2ed374e2cdd3d38bf001621fa38e4
jimmy <jimmy@r3e.network>
2025-06-20 08:59:35 +0800

    Fix copyright header filenames in DBFT tests
    - Correct UT_DBFT_Performance.cs header (was UT_DBFT_Robustness.cs)
    - Correct UT_DBFT_Failures.cs header (was UT_DBFT_AbnormalScenarios.cs)
    - Fix other copyright headers to match actual filenames
    - Applied via dotnet format to ensure consistency

--- tests/Neo.Plugins.DBFTPlugin.Tests/UT_DBFT_Performance.cs
+++ tests/Neo.Plugins.DBFTPlugin.Tests/UT_DBFT_Performance.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2025 The Neo Project.
 //
-// UT_DBFT_Robustness.cs file belongs to the neo project and is free
+// UT_DBFT_Performance.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
 // repository or http://www.opensource.org/licenses/mit-license.php
