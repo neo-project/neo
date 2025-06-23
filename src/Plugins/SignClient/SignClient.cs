ed932bbebcb47b824b4eeebb3be1469686aea177
Will <201105916+Wi1l-B0t@users.noreply.github.com>
2025-06-23 12:45:29 +0800

    Add: SignClient Vsock support (#4002)
    * Add: SignClient vsock support
    
    * optimize: add const string
    
    * optimize: use Uri parse endpoint
    
    ---------
    
    Co-authored-by: Shargon <shargon@gmail.com>
    Co-authored-by: NGD Admin <154295625+NGDAdmin@users.noreply.github.com>
    Co-authored-by: Christopher Schuchardt <cschuchardt88@gmail.com>

--- src/Plugins/SignClient/SignClient.cs
+++ src/Plugins/SignClient/SignClient.cs
@@ -46,7 +46,7 @@ namespace Neo.Plugins.SignClient
 
         public SignClient() { }
 
-        public SignClient(SignSettings settings)
+        public SignClient(Settings settings)
         {
             Reset(settings);
         }
@@ -66,7 +66,7 @@ namespace Neo.Plugins.SignClient
             if (!string.IsNullOrEmpty(_name)) SignerManager.RegisterSigner(_name, this);
         }
 
-        private ServiceConfig GetServiceConfig(SignSettings settings)
+        private ServiceConfig GetServiceConfig(Settings settings)
         {
             var methodConfig = new MethodConfig
             {
@@ -93,7 +93,7 @@ namespace Neo.Plugins.SignClient
             return new ServiceConfig { MethodConfigs = { methodConfig } };
         }
 
-        private void Reset(SignSettings settings)
+        private void Reset(Settings settings)
         {
             // _settings = settings;
             var serviceConfig = GetServiceConfig(settings);
@@ -332,7 +332,7 @@ namespace Neo.Plugins.SignClient
         protected override void Configure()
         {
             var config = GetConfiguration();
-            if (config is not null) Reset(new SignSettings(config));
+            if (config is not null) Reset(new Settings(config));
         }
 
         /// <inheritdoc/>
