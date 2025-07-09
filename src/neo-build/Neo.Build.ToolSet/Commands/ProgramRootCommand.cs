060cac0d3c3a2a5df9aa1f7e99c70cca237517f1
Christopher R. Schuchardt <cschuchardt88@gmail.com>
2025-07-09 17:24:21 -0400

    Added dbft configuation

--- src/neo-build/Neo.Build.ToolSet/Commands/ProgramRootCommand.cs
+++ src/neo-build/Neo.Build.ToolSet/Commands/ProgramRootCommand.cs
@@ -28,18 +28,22 @@ namespace Neo.Build.ToolSet.Commands
             IHostEnvironment env,
             NeoSystemOptions neoSystemOptions,
             AppEngineOptions appEngineOptions,
-            NeoProtocolOptions protocolOptions) : ICommandHandler
+            ProtocolOptions protocolOptions,
+            DBFTOptions dBFTOptions) : ICommandHandler
         {
             private readonly IHostEnvironment _env = env;
             private readonly NeoSystemOptions _neoSystemOptions = neoSystemOptions;
             private readonly AppEngineOptions _appEngineOptions = appEngineOptions;
-            private readonly NeoProtocolOptions _protocolOptions = protocolOptions;
+            private readonly ProtocolOptions _protocolOptions = protocolOptions;
+            private readonly DBFTOptions _dBFTOptions = dBFTOptions;
 
             public int Invoke(InvocationContext context) =>
                 InvokeAsync(context).ConfigureAwait(false).GetAwaiter().GetResult();
 
             public Task<int> InvokeAsync(InvocationContext context)
             {
+                context.Console.WriteLine("------- Global Settings -------");
+                context.Console.WriteLine();
                 context.Console.WriteLine("        Environment: {0}", _env.EnvironmentName);
                 context.Console.WriteLine("       Content Root: {0}", _env.ContentRootPath);
                 context.Console.WriteLine("         Store Root: {0}", _neoSystemOptions.Storage.StoreRoot);
@@ -57,6 +61,14 @@ namespace Neo.Build.ToolSet.Commands
                 context.Console.WriteLine("        Max MemPool: {0}", _protocolOptions.MemoryPoolMaxTransactions);
                 context.Console.WriteLine("      Max Traceable: {0}", _protocolOptions.MaxTraceableBlocks);
                 context.Console.WriteLine("        Initial Gas: {0}", _protocolOptions.InitialGasDistribution);
+                context.Console.WriteLine();
+                context.Console.WriteLine("--------- DBFT Plugin ---------");
+                context.Console.WriteLine();
+                context.Console.WriteLine("         Store Root: {0}", _dBFTOptions.StoreRoot);
+                context.Console.WriteLine("     Max Block Size: {0}", _dBFTOptions.MaxBlockSize);
+                context.Console.WriteLine("     Max System Fee: {0}", _dBFTOptions.MaxBlockSystemFee);
+                context.Console.WriteLine("       Recover Logs: {0}", _dBFTOptions.IgnoreRecoveryLogs);
+                context.Console.WriteLine("   Exception Policy: {0}", _dBFTOptions.ExceptionPolicy);
                 context.Console.WriteLine();
 
                 return Task.FromResult(0);
