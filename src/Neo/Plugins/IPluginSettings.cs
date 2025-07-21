74a2617364fd6503c17376f0a5f2d10e1cfab7e8
Christopher R. Schuchardt <cschuchardt88@gmail.com>
2025-07-21 16:05:28 -0400

    Fixed `neo-build.dev` branch for the changes in `dev`

--- src/Neo/Plugins/IPluginSettings.cs
+++ src/Neo/Plugins/IPluginSettings.cs
@@ -1,10 +1,6 @@
 // Copyright (C) 2015-2025 The Neo Project.
 //
-<<<<<<<< HEAD:tests/Neo.Build.Core.Tests/MSTestSettings.cs
-// MSTestSettings.cs file belongs to the neo project and is free
-========
 // IPluginSettings.cs file belongs to the neo project and is free
->>>>>>>> neo/dev:src/Neo/Plugins/IPluginSettings.cs
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
 // repository or http://www.opensource.org/licenses/mit-license.php
@@ -13,9 +9,6 @@
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
 
-<<<<<<<< HEAD:tests/Neo.Build.Core.Tests/MSTestSettings.cs
-[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
-========
 namespace Neo.Plugins
 {
     public interface IPluginSettings
@@ -23,4 +16,3 @@ namespace Neo.Plugins
         public UnhandledExceptionPolicy ExceptionPolicy { get; }
     }
 }
->>>>>>>> neo/dev:src/Neo/Plugins/IPluginSettings.cs
