c46d5308794ec23d31b2b6b206bc986d7a57aeae
Shargon <shargon@gmail.com>
2025-07-30 01:26:04 -0700

    Fix ContractMethodParametersJsonConverter

--- src/Plugins/RestServer/Newtonsoft/Json/ContractMethodParametersJsonConverter.cs
+++ src/Plugins/RestServer/Newtonsoft/Json/ContractMethodParametersJsonConverter.cs
@@ -17,11 +17,12 @@ namespace Neo.Plugins.RestServer.Newtonsoft.Json
 {
     public class ContractMethodParametersJsonConverter : JsonConverter<ContractParameterDefinition>
     {
-        public override bool CanRead => base.CanRead;
+        public override bool CanRead => false;
+        public override bool CanWrite => true;
 
-        public override bool CanWrite => base.CanWrite;
+        public override ContractParameterDefinition ReadJson(JsonReader reader, Type objectType, ContractParameterDefinition? existingValue, bool hasExistingValue, JsonSerializer serializer)
+            => throw new NotImplementedException();
 
-        public override ContractParameterDefinition ReadJson(JsonReader reader, Type objectType, ContractParameterDefinition? existingValue, bool hasExistingValue, JsonSerializer serializer) => throw new NotImplementedException();
         public override void WriteJson(JsonWriter writer, ContractParameterDefinition? value, JsonSerializer serializer)
         {
             if (value is null) throw new ArgumentNullException(nameof(value));
