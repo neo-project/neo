1e0b774ad3f6ea893c6341ff1d89e14da9f1bd63
Alvaro <amjarag@gmail.com>
2025-08-16 19:38:12 +0200

    Clean up unused using directives

--- tests/Neo.Plugins.RpcServer.Tests/UT_RpcException.cs
+++ tests/Neo.Plugins.RpcServer.Tests/UT_RpcException.cs
@@ -10,11 +10,6 @@
 // modifications are permitted.
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Neo.Plugins.RpcServer.Tests
 {
