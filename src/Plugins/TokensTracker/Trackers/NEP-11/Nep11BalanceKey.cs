09144cf4054568fa8fc5683188143a4946d4e557
Will <201105916+Wi1l-B0t@users.noreply.github.com>
2025-09-15 17:02:27 +0800

    Optimize: enable nullable for TokensTracker (#4167)
    Co-authored-by: NGD Admin <154295625+NGDAdmin@users.noreply.github.com>

--- src/Plugins/TokensTracker/Trackers/NEP-11/Nep11BalanceKey.cs
+++ src/Plugins/TokensTracker/Trackers/NEP-11/Nep11BalanceKey.cs
@@ -24,9 +24,7 @@ namespace Neo.Plugins.Trackers.NEP_11
         public ByteString Token;
         public int Size => UInt160.Length + UInt160.Length + Token.GetVarSize();
 
-        public Nep11BalanceKey() : this(new UInt160(), new UInt160(), ByteString.Empty)
-        {
-        }
+        public Nep11BalanceKey() : this(UInt160.Zero, UInt160.Zero, ByteString.Empty) { }
 
         public Nep11BalanceKey(UInt160 userScriptHash, UInt160 assetScriptHash, ByteString tokenId)
         {
@@ -39,7 +37,7 @@ namespace Neo.Plugins.Trackers.NEP_11
             Token = tokenId;
         }
 
-        public int CompareTo(Nep11BalanceKey other)
+        public int CompareTo(Nep11BalanceKey? other)
         {
             if (other is null) return 1;
             if (ReferenceEquals(this, other)) return 0;
@@ -50,14 +48,14 @@ namespace Neo.Plugins.Trackers.NEP_11
             return (Token.GetInteger() - other.Token.GetInteger()).Sign;
         }
 
-        public bool Equals(Nep11BalanceKey other)
+        public bool Equals(Nep11BalanceKey? other)
         {
             if (other is null) return false;
             if (ReferenceEquals(this, other)) return true;
             return UserScriptHash.Equals(other.UserScriptHash) && AssetScriptHash.Equals(AssetScriptHash) && Token.Equals(other.Token);
         }
 
-        public override bool Equals(Object other)
+        public override bool Equals(object? other)
         {
             return other is Nep11BalanceKey otherKey && Equals(otherKey);
         }
