6b6d5c4656bc247b0a7771c2b90e0e938afd9d46
Fernando Diaz Toledano <shargon@gmail.com>
2025-10-16 10:28:06 +0200

    Fix gas

--- src/Neo/SmartContract/Native/ContractManagement.cs
+++ src/Neo/SmartContract/Native/ContractManagement.cs
@@ -267,8 +267,8 @@ namespace Neo.SmartContract.Native
             if (manifest.Length == 0)
                 throw new ArgumentException($"Manifest length cannot be zero.");
 
-            engine.AddFee(Math.Max(
-                engine.StoragePrice * (nefFile.Length + manifest.Length),
+            engine.AddFee(BigInteger.Max(
+                engine.StoragePicoPrice * (nefFile.Length + manifest.Length),
                 GetMinimumDeploymentFee(engine.SnapshotCache) * ApplicationEngine.FeeFactor
                 ));
 
@@ -336,7 +336,7 @@ namespace Neo.SmartContract.Native
             if (nefFile is null && manifest is null)
                 throw new ArgumentException("NEF file and manifest cannot both be null.");
 
-            engine.AddFee(engine.StoragePrice * ((nefFile?.Length ?? 0) + (manifest?.Length ?? 0)));
+            engine.AddFee(engine.StoragePicoPrice * ((nefFile?.Length ?? 0) + (manifest?.Length ?? 0)));
 
             var contractState = engine.SnapshotCache.GetAndChange(CreateStorageKey(Prefix_Contract, engine.CallingScriptHash))
                 ?? throw new InvalidOperationException($"Updating Contract Does Not Exist: {engine.CallingScriptHash}");
