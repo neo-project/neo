3620e1f35c5f2dc553760c7648a47cb689270525
Shargon <shargon@gmail.com>
2025-11-17 12:57:22 +0100

    Check nullability info for arguments of native contracts and syscalls

--- src/Neo/SmartContract/Native/FungibleToken.cs
+++ src/Neo/SmartContract/Native/FungibleToken.cs
@@ -131,8 +131,6 @@ namespace Neo.SmartContract.Native
         [ContractMethod(CpuFee = 1 << 17, StorageFee = 50, RequiredCallFlags = CallFlags.States | CallFlags.AllowCall | CallFlags.AllowNotify)]
         private protected async ContractTask<bool> Transfer(ApplicationEngine engine, UInt160 from, UInt160 to, BigInteger amount, StackItem data)
         {
-            ArgumentNullException.ThrowIfNull(from);
-            ArgumentNullException.ThrowIfNull(to);
             if (amount.Sign < 0) throw new ArgumentOutOfRangeException(nameof(amount), "cannot be negative");
             if (!from.Equals(engine.CallingScriptHash) && !engine.CheckWitnessInternal(from))
                 return false;
