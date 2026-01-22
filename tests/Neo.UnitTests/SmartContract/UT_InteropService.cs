3c9d983d56882aa65fa4e370e285b3a548680699
Alvaro <amjarag@gmail.com>
2026-01-22 22:53:03 +0100

    rename

--- tests/Neo.UnitTests/SmartContract/UT_InteropService.cs
+++ tests/Neo.UnitTests/SmartContract/UT_InteropService.cs
@@ -813,7 +813,7 @@ namespace Neo.UnitTests.SmartContract
         }
 
         [TestMethod]
-        public void TestVerifyWithECDsaV0()
+        public void TestVerifyWithECDsa()
         {
             var privateKey = new byte[32];
             using var rng = RandomNumberGenerator.Create();
@@ -824,11 +824,11 @@ namespace Neo.UnitTests.SmartContract
             var signatureR1 = Crypto.Sign(hexMessage, privateKey, ECCurve.Secp256r1);
             var signatureK1 = Crypto.Sign(hexMessage, privateKey, ECCurve.Secp256k1);
 
-            var result = CryptoLib.VerifyWithECDsaV0(hexMessage, publicKeyR1, signatureR1, NamedCurveHash.secp256r1SHA256);
+            var result = CryptoLib.VerifyWithECDsa(hexMessage, publicKeyR1, signatureR1, NamedCurveHash.secp256r1SHA256);
             Assert.IsTrue(result);
-            result = CryptoLib.VerifyWithECDsaV0(hexMessage, publicKeyK1, signatureK1, NamedCurveHash.secp256k1SHA256);
+            result = CryptoLib.VerifyWithECDsa(hexMessage, publicKeyK1, signatureK1, NamedCurveHash.secp256k1SHA256);
             Assert.IsTrue(result);
-            Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => _ = CryptoLib.VerifyWithECDsaV0(hexMessage, publicKeyK1, new byte[64], NamedCurveHash.secp256r1Keccak256));
+            Assert.ThrowsExactly<FormatException>(() => CryptoLib.VerifyWithECDsa(hexMessage, publicKeyK1, [], NamedCurveHash.secp256k1SHA256));
         }
 
         [TestMethod]
