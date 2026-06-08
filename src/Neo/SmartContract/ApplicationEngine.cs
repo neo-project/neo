c4e53d2a1a0dde5fe2bd1aaa25703b602b279c71
Owen <38493437+superboyiii@users.noreply.github.com>
2026-06-08 16:43:10 +0800

    Revert "Fix fee consumed (#4559)"
    This reverts commit 8fff58554986e55e7e6260580907527044e62a52.

--- src/Neo/SmartContract/ApplicationEngine.cs
+++ src/Neo/SmartContract/ApplicationEngine.cs
@@ -532,10 +532,7 @@ namespace Neo.SmartContract
 
             _feeConsumed = _feeConsumed + picoGas;
             if (_feeConsumed > _feeAmount)
-            {
-                _feeConsumed = _feeAmount;
                 throw new InvalidOperationException("Insufficient GAS.");
-            }
         }
 
         protected override void OnFault(Exception ex)
