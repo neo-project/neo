3fef8c7b2dd09a05a8997e1c6438c149804d76ae
Christopher Schuchardt <8141309+cschuchardt88@users.noreply.github.com>
2026-07-25 11:18:43 -0400

    style: fix whitespace formatting

--- tests/Neo.UnitTests/SmartContract/UT_ApplicationEngine_Residual.cs
+++ tests/Neo.UnitTests/SmartContract/UT_ApplicationEngine_Residual.cs
@@ -180,35 +180,35 @@ namespace Neo.UnitTests.SmartContract
                 engine.ValidateCallFlags(CallFlags.WriteStates));
         }
 
-		[TestMethod]
-		public void Create_WithPersistingBlock_PreservesBlockAndTrigger()
-		{
-			var block = new Block
-			{
-				Header = new Header
-				{
-					Index = 0,
-					Timestamp = 0,
-					Nonce = 0,
-					NextConsensus = UInt160.Zero,
-					PrevHash = UInt256.Zero,
-					MerkleRoot = UInt256.Zero,
-					Witness = Witness.Empty
-				},
-				Transactions = []
-			};
+        [TestMethod]
+        public void Create_WithPersistingBlock_PreservesBlockAndTrigger()
+        {
+            var block = new Block
+            {
+                Header = new Header
+                {
+                    Index = 0,
+                    Timestamp = 0,
+                    Nonce = 0,
+                    NextConsensus = UInt160.Zero,
+                    PrevHash = UInt256.Zero,
+                    MerkleRoot = UInt256.Zero,
+                    Witness = Witness.Empty
+                },
+                Transactions = []
+            };
 
-			using var engine = ApplicationEngine.Create(
-				TriggerType.Application,
-				null,
-				_snapshot,
-				persistingBlock: block,
-				settings: TestProtocolSettings.Default,
-				gas: 100_0000_0000);
+            using var engine = ApplicationEngine.Create(
+                TriggerType.Application,
+                null,
+                _snapshot,
+                persistingBlock: block,
+                settings: TestProtocolSettings.Default,
+                gas: 100_0000_0000);
 
-			Assert.AreSame(block, engine.PersistingBlock);
-			Assert.AreEqual(TriggerType.Application, engine.Trigger);
-		}
+            Assert.AreSame(block, engine.PersistingBlock);
+            Assert.AreEqual(TriggerType.Application, engine.Trigger);
+        }
 
         [TestMethod]
         public void Services_Dictionary_IsPopulated()
