06fb0f32f64347121d90bb2d01c3226eefa13b6d
Christopher Schuchardt <8141309+cschuchardt88@users.noreply.github.com>
2026-08-09 09:23:10 -0400

    test(LocalNode): isolate actor and assert unconnected peer membership
    Address ajara87 review on #4707: create a dedicated LocalNode actor instead of reconfiguring NeoSystem.LocalNode, and assert the added endpoint is present in GetUnconnectedPeers() rather than UnconnectedCount >= 0.

--- tests/Neo.UnitTests/Network/P2P/UT_LocalNode_Coverage.cs
+++ tests/Neo.UnitTests/Network/P2P/UT_LocalNode_Coverage.cs
@@ -14,6 +14,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.Network.P2P;
 using Neo.Network.P2P.Payloads;
 using System;
+using System.Linq;
 using System.Net;
 using System.Threading;
 
@@ -22,49 +23,53 @@ namespace Neo.UnitTests.Network.P2P
     [TestClass]
     public class UT_LocalNode_Coverage : TestKit
     {
-        private static NeoSystem s_system;
-
-        [ClassInitialize]
-        public static void ClassSetup(TestContext _)
-        {
-            s_system = TestBlockchain.GetSystem();
-        }
-
         [TestMethod]
         public void Peers_Message_AddsUnconnectedPeers()
         {
+            // Isolated actor so we do not reconfigure the shared NeoSystem.LocalNode.
+            var system = TestBlockchain.GetSystem();
+            var localNode = ActorOf(LocalNode.Props(system));
             var sender = CreateTestProbe();
-            sender.Send(s_system.LocalNode, new ChannelsConfig
+
+            // MinDesiredConnections = 0 keeps OnTimer from draining UnconnectedPeers.
+            sender.Send(localNode, new ChannelsConfig
             {
                 Tcp = new IPEndPoint(IPAddress.Loopback, 0),
-                MinDesiredConnections = 1,
+                MinDesiredConnections = 0,
                 MaxConnections = 10,
                 MaxConnectionsPerAddress = 3
             });
 
             var ep = new IPEndPoint(IPAddress.Parse("203.0.113.10"), 20333);
-            sender.Send(s_system.LocalNode, new Peer.Peers([ep]));
+            sender.Send(localNode, new Peer.Peers([ep]));
 
-            sender.Send(s_system.LocalNode, new LocalNode.GetInstance());
+            sender.Send(localNode, new LocalNode.GetInstance());
             var local = sender.ExpectMsg<LocalNode>(TimeSpan.FromSeconds(3), cancellationToken: CancellationToken.None);
-            Assert.IsTrue(local.UnconnectedCount >= 0);
+            Assert.IsTrue(local.GetUnconnectedPeers().Any(p => p.Equals(ep)));
         }
 
         [TestMethod]
         public void Relay_Directly_Message_DoesNotFault()
         {
+            var system = TestBlockchain.GetSystem();
+            var localNode = ActorOf(LocalNode.Props(system));
             var sender = CreateTestProbe();
+            sender.Send(localNode, new ChannelsConfig());
+
             var inv = InvPayload.Create(InventoryType.TX,
                 UInt256.Parse("0x3333333333333333333333333333333333333333333333333333333333333333"));
-            sender.Send(s_system.LocalNode, Message.Create(MessageCommand.Inv, inv));
+            sender.Send(localNode, Message.Create(MessageCommand.Inv, inv));
             sender.ExpectNoMsg(TimeSpan.FromMilliseconds(300), cancellationToken: CancellationToken.None);
         }
 
         [TestMethod]
         public void GetInstance_ReturnsLocalNode()
         {
+            var system = TestBlockchain.GetSystem();
+            var localNode = ActorOf(LocalNode.Props(system));
             var sender = CreateTestProbe();
-            sender.Send(s_system.LocalNode, new LocalNode.GetInstance());
+            sender.Send(localNode, new ChannelsConfig());
+            sender.Send(localNode, new LocalNode.GetInstance());
             var local = sender.ExpectMsg<LocalNode>(TimeSpan.FromSeconds(3), cancellationToken: CancellationToken.None);
             Assert.IsNotNull(local);
             Assert.IsNotNull(local.Config);
